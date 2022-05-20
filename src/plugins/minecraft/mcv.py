98a034bc81ae51a2da529be36d8ca580311a9a2b
HornCopper <zgu3316@gmail.com>
2022-05-20 23:31:30 +0800

    Fixed a bug

--- src/plugins/minecraft/mcv.py
+++ src/plugins/minecraft/mcv.py
@@ -3,13 +3,13 @@ TOOLS = nonebot.get_driver().config.tools_path
 sys.path.append(TOOLS)
 from http_ import http
 
-async def mcjv():
+async def mcjavav():
     info = json.loads(await http.get_url("https://api.codethink.top/mcv?platform=j"))
     rl = info["release"]
     ss = info["snapshot"]
-    return f"找到Java版本信息啦~\n最新快照：{ss}，最新正式版：{rl}"
-async def mcbv():
+    return f"找到Java版本信息啦~\n最新快照：{ss}\n最新正式版：{rl}"
+async def mcbedrockv():
     info = json.loads(await http.get_url("https://api.codethink.top/mcv?platform=b"))
     rl = info["release"]
     bt = info["beta"]
-    return f"找到基岩版版本信息啦~\n最新测试版：{bt}，最新正式版：{rl}"
+    return f"找到基岩版版本信息啦~\n最新测试版：{bt}\n最新正式版：{rl}"
