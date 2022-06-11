09e3f8618b2a6d550982cd13869a01484dd2bd59
HornCopper <zgu3316@gmail.com>
2022-06-11 10:35:41 +0800

    Add some new code.

--- src/plugins/register/__init__.py
+++ src/plugins/register/__init__.py
@@ -1,15 +1,35 @@
-import json, sys, nonebot
+import json, sys, nonebot, os
 from nonebot import on_command, on_message
 from nonebot.adapters import Message
-from nonebot.adapters.onebot.v11 import Event, Bot
+from nonebot.adapters.onebot.v11 import Event, Bot, GroupMessageEvent
 from nonebot.matcher import Matcher
 from nonebot.params import CommandArg
+from nonebot.log import logger
 TOOLS = nonebot.get_driver().config.tools_path
 sys.path.append(str(TOOLS))
 from permission import checker, error
 from file import read, write
 
-unregistered = on_message(block=False)
+unregistered = on_message(block=False,priority=1)
 @unregistered.handle()
 async def _(matcher: Matcher, event: GroupMessageEvent):
-    pass
+    directorys=os.listdir("./src/data")
+    if str(event.group_id) not in directorys:
+        matcher.stop_propagation()
+    else:
+        return
+    
+register = on_command("register",block=False,priority=0)
+@register.handle()
+async def _(event: GroupMessageEvent):
+    group = event.group_id
+    directorys=os.listdir("./src/data")
+    if group in directorys:
+        await register.finish("已注册，无需再次注册哦~")
+    else:
+        new_path = "./src/data/"+str(group)
+        os.mkdir(new_path)
+        write(new_path+"/webhook.json","[]")
+        write(new_path+"/marry.json","[]")
+        write(new_path+"/welcome.txt","欢迎入群！")
+        await register.finish("注册成功！")
