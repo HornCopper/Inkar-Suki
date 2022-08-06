51a60a4cde57b8d47c9366f11cec1548cfc7cbe6
HornCopper <zgu3316@gmail.com>
2022-08-06 18:56:54 +0800

    Update info.json for module `jx3`.

--- src/plugins/jx3/skilldatalib.py
+++ src/plugins/jx3/skilldatalib.py
@@ -126,6 +126,8 @@ async def getAllSkillsInfo(Kungfu: str) -> str:
         for x in i["forceSkills"]:
             image = await get_icon(x["skillName"], x["icon"]["FileName"])
             releaseType = x["releaseType"] # 释放类型
+            if releaseType != "瞬间释放":
+                releaseType = releaseType + "释放"
             cd = x["cd"] # 调息时间
             skillName = x["skillName"] # 技能名
             specialDesc = x["specialDesc"] # 简单描述
