f0c36f5182fde90cb1e26b593cd1e48690e4f73d
HornCopper <zgu3316@gmail.com>
2022-09-19 01:38:55 +0800

    .

--- src/plugins/jx3/__init__.py
+++ src/plugins/jx3/__init__.py
@@ -1,4 +1,3 @@
-from re import L
 import sys
 import nonebot
 import json
@@ -27,6 +26,7 @@ from .task import getTask, getTaskChain
 from .jx3apiws import ws_client
 from .jx3_event import RecvEvent
 from .record import adventure____, firework
+from .buff import get_buff
 
 horse = on_command("jx3_horse",aliases={"马"},priority=5)
 @horse.handle()
@@ -384,6 +384,41 @@ async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
             if status == "more":
                 await recruit.send("团队招募超过100条啦！已截取前100条~")
             await bot.call_api("send_group_forward_msg", group_id = event.group_id, messages = data)
+
+buff_ = on_command("jx3_buff", aliases={"debuff","buff"}, priority=5)
+@buff_.handle()
+async def _(event: GroupMessageEvent, state: T_State, args: Message = CommandArg()):
+    buff = args.extract_plain_text()
+    data = await get_buff(buff)
+    if type(data) != type("sb"):
+        state["icon"] = data["icon"]
+        state["remark"] = data["remark"]
+        state["desc"] = data["desc"]
+        state["name"] = data["name"]
+        state["id"] = data["id"]
+        msg = ""
+        for i in range(len(data["icon"])):
+            msg = "\n" + str(i) + "." + data["name"][i] + "（技能ID：" + id[i] + "）"
+        await buff_.send(msg[1:])
+        return
+    else:
+        await buff_.finish(data)
+
+@buff_.got("num", prompt="输入数字搜索状态效果，输入其他内容则无视。")
+async def _(event: GroupMessageEvent, state: T_State, num: Message = Arg()):
+    num = num.extract_plain_text()
+    if checknumber(num):
+        icon = state["icon"]
+        remark = state["remark"]
+        desc = state["desc"]
+        name = state["name"]
+        id = state["id"]
+        if int(num) not in list(range(len(icon))):
+            await buff_.finish("唔，输入的数字不对哦，取消搜索~")
+        else:
+            msg = ms.image(icon[num]) + f"\nBUFF名称：{name[num]}\n{desc[num]}\n特殊描述：{remark[num]}"
+    else:
+        return
 
 driver = get_driver()
 
