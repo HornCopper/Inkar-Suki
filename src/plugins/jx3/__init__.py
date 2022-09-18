567f882a0a77cbb6e7571fe65647ea68ddaaa76c
HornCopper <zgu3316@gmail.com>
2022-09-19 01:50:55 +0800

    .

--- src/plugins/jx3/__init__.py
+++ src/plugins/jx3/__init__.py
@@ -399,6 +399,7 @@ async def _(event: GroupMessageEvent, state: T_State, args: Message = CommandArg
         msg = ""
         for i in range(len(data["icon"])):
             msg = "\n" + str(i) + "." + data["name"][i] + "（技能ID：" + data["id"][i] + "）"
+            logger.info(msg)
         await buff_.send(msg[1:])
         return
     else:
