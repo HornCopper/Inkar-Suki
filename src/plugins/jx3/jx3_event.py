a8b301f3255ee768e04b1db541e79c6d0d08b660
HornCopper <zgu3316@gmail.com>
2022-09-27 21:57:45 +0800

    已删除其他服务器的支持

--- src/plugins/jx3/jx3_event.py
+++ src/plugins/jx3/jx3_event.py
@@ -154,10 +154,12 @@ class ServerStatusEvent(RecvEvent):
     @overrides(RecvEvent)
     def get_message(self) -> Message:
         time_now = datetime.now().strftime("%H时%M分")
-        if self.status:
+        if self.status and str(self.server) == "幽月轮":
             return Message(f"现在是{time_now}，并且{self.server}已开服~")
-        else:
+        elif self.status == False and str(self.server) == "幽月轮":
             return Message(f"现在是{time_now}，但是{self.server}已维护~")
+        else:
+            return False
 
 
 @EventRister.rister(action=2002)
