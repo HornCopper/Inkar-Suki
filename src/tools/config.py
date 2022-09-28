92e6a568ff5fc4585352aa249136af44b1da4fae
HornCopper <zgu3316@gmail.com>
2022-09-29 00:16:25 +0800

    pul

--- src/tools/config.py
+++ src/tools/config.py
@@ -22,6 +22,7 @@ class Config:
     welcome_file = global_path+"/tools/welcome.txt"
     version = ikv
     nonebot = nbv
+    coppertoken = configs["coppertoken"]
     auaurl = configs["aua_api"]
     auatok = configs["aua_token"]
     jx3api_wslink = configs["jx3api_wslink"]
