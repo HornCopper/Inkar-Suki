5c84b11858d20eb3179b3d7bb3ac8446f3caa58e
HornCopper <zgu3316@gmail.com>
2022-10-01 18:45:25 +0800

    修复可能的错误

--- src/plugins/music/music.py
+++ src/plugins/music/music.py
@@ -5,17 +5,6 @@ TOOLS = nonebot.get_driver().config.tools_path
 sys.path.append(TOOLS)
 
 from utils import get_api
-from config import Config
-
-proxy = Config.proxy
-
-proxies = {
-    "http://": proxy,
-    "https://": proxy
-}
-
-if proxy == None:
-    proxies = None
 
 async def search(platform_: str, song: str):
     '''
@@ -31,23 +20,11 @@ async def search(platform_: str, song: str):
         platform = 2
     keyword = song
     if platform == 1:
-        api = "https://c.y.qq.com/splcloud/fcgi-bin/smartbox_new.fcg"
-        params = {
-            "format": "json",
-            "inCharset": "utf-8",
-            "outCharset": "utf-8",
-            "notice": 0,
-            "platform": "yqq.json",
-            "needNewCode": 0,
-            "uin": 0,
-            "hostUin": 0,
-            "is_xml": 0,
-            "key": keyword
-        }
-        data = await get_api(url = api, args = params, proxy = proxies)
+        api = "https://c.y.qq.com/splcloud/fcgi-bin/smartbox_new.fcg?format=json&key=" + keyword
+        data = await get_api(url = api)
         id = []
         song_ = []
-        for i in data["data"]["song"]:
+        for i in data["data"]["song"]["itemlist"]:
             song_name = i["name"] + " - " + i["singer"]
             song_.append(song_name)
             id.append(i["id"])
@@ -55,13 +32,8 @@ async def search(platform_: str, song: str):
             return "404"
         return [song_, id, platform]
     elif platform == 2:
-        api = "https://music.163.com/api/cloudsearch/pc"
-        params = {
-            "s": keyword,
-            "type": 1,
-            "offset": 0
-        }
-        data = await get_api(url = api, args = params, proxy = proxies)
+        api = "https://music.163.com/api/cloudsearch/pc?type=1&offset=1&s=" + keyword
+        data = await get_api(url = api)
         id = []
         song_ = []
         for i in data["result"]["songs"]:
@@ -82,45 +54,30 @@ async def get(platform_: str, song: str, singer: str = None):
     '''
     if platform_ in ["QQ","QQ音乐","qq","q","Q","tx","tc","tencent","腾讯","腾讯音乐","qq音乐","Qq","Qq音乐","qQ","qQ音乐"]:
         platform = 1 # 1 QQ 2 网易
-    elif platform_ in ["网易","163","网抑云","网抑","网","netease","n","网易云音乐","网抑云音乐","网易云"]:
+    elif platform_ in ["网易","163","网抑云","网抑","网","netease","n","网易云音乐","网抑云音乐","网易云","w","wy"]:
         platform = 2
+    else:
+        platform = 1
     keyword = song
     if platform == 1:
-        api = "https://c.y.qq.com/splcloud/fcgi-bin/smartbox_new.fcg"
-        params = {
-            "format": "json",
-            "inCharset": "utf-8",
-            "outCharset": "utf-8",
-            "notice": 0,
-            "platform": "yqq.json",
-            "needNewCode": 0,
-            "uin": 0,
-            "hostUin": 0,
-            "is_xml": 0,
-            "key": keyword
-        }
-        data = await get_api(url = api, args = params, proxy = proxies)
+        api = "https://c.y.qq.com/splcloud/fcgi-bin/smartbox_new.fcg?format=json&key=" + keyword
+        data = await get_api(url = api)
         if len(data["data"]["song"]) == 0:
             return "404"
         if singer == None:
-            song_name = data["data"]["song"][0]["name"] + " - " + data["data"]["song"][0]["singer"]
-            id = data["data"]["song"][0]["id"]
+            song_name = data["data"]["song"]["itemlist"][0]["name"] + " - " + data["data"]["song"]["itemlist"][0]["singer"]
+            id = data["data"]["song"]["itemlist"][0]["id"]
             return [song_name, id, platform]
         else:
-            for i in data["data"]["song"]:
+            for i in data["data"]["song"]["itemlist"]:
                 if i["singer"] == singer:
                     song_name = i["name"] + " - " + i["singer"]
                     id = i["id"]
                     return [song_name, id, platform]
             return "404"
     elif platform == 2:
-        api = "https://music.163.com/api/cloudsearch/pc"
-        params = {
-            "s": keyword,
-            "type": 1,
-            "offset": 0
-        }
-        data = await get_api(url = api, args = params, proxy = proxies)
+        api = "https://music.163.com/api/cloudsearch/pc?type=1&offset=1&s=" + keyword
+        data = await get_api(url = api)
         if len(data["result"]["songs"]) == 0:
             return "404"
         if singer == None:
