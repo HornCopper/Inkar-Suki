674a4705ce2cde7950da949a6ad80acd06eae52e
HornCopper <zgu3316@gmail.com>
2022-11-27 01:29:03 +0800

    /

--- src/plugins/jx3/skilldatalib.py
+++ src/plugins/jx3/skilldatalib.py
@@ -85,7 +85,7 @@ async def getTalents():
 
     数据来源：`JX3BOX` & `JX3API`
     '''
-    force_list = await get_api("https://www.inkar-suki.xyz/api")
+    force_list = await get_api("https://www.inkar-suki.xyz/jx3boxdata")
     data_list = []
     for i in force_list:
         data_list.append(i)
