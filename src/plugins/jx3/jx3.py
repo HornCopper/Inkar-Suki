8962ae5f4845f7c909ad4a293c009a685b3f8424
HornCopper <zgu3316@gmail.com>
2023-01-09 02:21:41 +0800

    NEW

--- src/plugins/jx3/jx3.py
+++ src/plugins/jx3/jx3.py
@@ -419,6 +419,30 @@ async def trials_(server: str = None, school: str = None):
         return ["唔……门派名称输入错误。"]
     return data["data"]["url"]
 
+async def rank_(type_1: str, type_2: str, server: str):
+    if token == None:
+        return ["Bot尚未填写Token，请联系Bot主人~"]
+    if type_1 == "个人":
+        if type_2 not in ["名士五十强","老江湖五十强","兵甲藏家五十强","名师五十强","阵营英雄五十强","薪火相传五十强","庐园广记一百强"]:
+            return ["唔……类型不正确，请检查后重试~"]
+        final_url = f"https://www.jx3api.com/view/rank/various?token={token}&robot={bot}&server={server}&type={type_2}"
+    elif type_1 == "帮会":
+        if type_2 not in ["浩气神兵宝甲五十强","恶人神兵宝甲五十强","浩气爱心帮会五十强","恶人爱心帮会五十强"]:
+            return ["唔……类型不正确，请检查后重试~"]
+        final_url = f"https://www.jx3api.com/view/rank/tribe?token={token}&robot={bot}&server={server}&type={type_2}"
+    elif type_1 == "战功":
+        if type_2 not in ["赛季恶人五十强","赛季浩气五十强","上周恶人五十强","上周浩气五十强","本周恶人五十强","本周浩气五十强"]:
+            return ["唔……类型不正确，请检查后重试~"]
+        final_url = f"https://www.jx3api.com/view/rank/excellent?token={token}&robot={bot}&server={server}&type={type_2}"
+    else:
+        return ["未知类型，只能是个人/帮会/战功哦！"]
+    data = await get_api(final_url, proxy = proxies)
+    if data["code"] == 401:
+        return ["唔……参数有误！"]
+    if data["code"] == 404:
+        return ["唔……未收录！"]
+    return data["data"]["url"]
+
 def server_mapping(server: str):
     if server in ["二合一","四合一","六合一","七合一","千岛湖","圣墓山","执子之手","平步青云","笑傲江湖","幽月轮","山雨欲来"]:
         return "幽月轮"
