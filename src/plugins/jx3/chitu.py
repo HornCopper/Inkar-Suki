9c717cc361eb41d2391f42d2f10bb19194e79116
HornCopper <zgu3316@gmail.com>
2023-04-08 01:21:51 +0800

    Add a new application

--- src/plugins/jx3/chitu.py
+++ src/plugins/jx3/chitu.py
@@ -5,7 +5,7 @@ import sys
 TOOLS = nonebot.get_driver().config.tools_path
 sys.path.append(TOOLS)
 
-from utils import get_url
+from utils import get_url, get_api
 from .jx3 import server_mapping
 
 async def get_chitu(server: str, group: str): # 数据来源@jw3cx.com
@@ -20,4 +20,19 @@ async def get_chitu(server: str, group: str): # 数据来源@jw3cx.com
         if i.get_text().find(server) != -1:
             return f"{i.get_text()}：" + i["value"]
     return "唔……服务器名输入有误，请检查后重试~"
-        
+
+async def get_horse_reporter(server: str, group: str): #数据来源@JX3BOX
+    server = server_mapping(server, group)
+    if server == False:
+        return "没有找到该服务器哦，请检查后重试~"
+    final_url = f"https://next2.jx3box.com/api/game/reporter/horse?type=horse&server={server}"
+    data = await get_api(final_url)
+    if data["data"]["page"]["total"] == 0:
+        return "没有找到该服务器信息哦，请检查后重试~"
+    for i in data["data"]["list"]:
+        if i["subtype"] == "npc_chat":
+            time = time.strftime("%Y年%m月%d日 %H:%M:%S", time.localtime(i["time"]))
+            content = i["content"]
+            map = i["map_name"]
+            msg = f"{content}\n刷新时间：{time}\n地图：{map}"
+            return msg
