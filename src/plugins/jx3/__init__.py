0ef5da21ad87f8ea1306c924a96798c70486264c
HornCopper <zgu3316@gmail.com>
2023-04-14 18:28:39 +0800

    New application about wanbaolou of JX3

--- src/plugins/jx3/__init__.py
+++ src/plugins/jx3/__init__.py
@@ -32,6 +32,7 @@ from .top100 import get_top100
 from .dh import get_dh
 from .macro import get_macro
 from .chitu import get_chitu, get_horse_reporter
+from .wanbaolou import get_wanbaolou
 
 news = on_command("jx3_news", aliases={"新闻"}, priority=5)
 @news.handle()
@@ -940,6 +941,25 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     group = str(event.group_id)
     msg = await get_horse_reporter(server, group)
     await horse.finish(msg)
+
+wbl = on_command("jx3_wbl", aliases={"万宝楼"}, priority=5)
+@wbl.handle()
+async def _(event: GroupMessageEvent, args: Message = CommandArg()):
+    arg = args.extract_plain_text().split(" ")
+    if len(arg) != 2:
+        await wbl.finish("唔……参数有误，请检查后重试~")
+    product = arg[0]
+    product_num = arg[1]
+    if checknumber(product_num) == False:
+        await wbl.finish("唔……检测到商品编号出现了非数字的内容！\n请检查后重试~")
+    if product not in ["外观","角色"]:
+        await wbl.finish("唔……第二个参数请填写「外观」或「角色」。")
+    if product == "外观":
+        product_flag = True
+    else:
+        product_flag = False
+    msg = await get_wanbaolou(product_num, product_flag)
+    await wbl.finish(msg)
 
 driver = get_driver()
 
