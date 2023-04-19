3935b8f23ce653f50654fc098db70977701d4428
HornCopper <zgu3316@gmail.com>
2023-04-20 00:44:54 +0800

    优化数据结构“

--- src/tools/local_version.py
+++ src/tools/local_version.py
@@ -1,8 +1,9 @@
+from nonebot import __version__ as nbv
 import nonebot
 import sys
-import json
 TOOLS = nonebot.get_driver().config.tools_path
 sys.path.append(TOOLS)
+
 from utils import get_api
 
 async def ikv():
@@ -11,5 +12,3 @@ async def ikv():
     all_cmt = await get_api("https://api.github.com/repos/codethink-cn/Inkar-Suki/commits")
     lst_cmt = all_cmt[0]["sha"][0:7]
     return f"{lst_rls}({lst_cmt})"
-
-nbv = nonebot.__version__
