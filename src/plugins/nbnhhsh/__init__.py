67fa5b0d19630d7647307f5c08b7414cc1aaef2c
HornCopper <zgu3316@gmail.com>
2023-05-02 21:15:53 +0800

    Fixed

--- src/plugins/nbnhhsh/__init__.py
+++ src/plugins/nbnhhsh/__init__.py
@@ -1,5 +1,7 @@
 import nonebot
 import sys
+import json
+import httpx
 
 from nonebot import on_command
 from nonebot.adapters import Message
@@ -19,10 +21,13 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     data = {
         "text": content
     }
-    resp = await post_url(url = url, data = data)
-    if "trans" not in list(resp):
-        await nbnhhsh.finish("没有任何可能的结果哦~")
-    else:
-        results = resp["trans"]
-        ans = "可能的结果有：\n" + "、".join(results)
-        await nbnhhsh.finish(ans)
+    async with httpx.AsyncClient(follow_redirects = True) as client:
+        resp = await client.post(url, data = data)
+        result = resp.text
+        resp = json.loads(result)
+        if "trans" not in list(resp):
+            await nbnhhsh.finish("没有任何可能的结果哦~")
+        else:
+            results = resp["trans"]
+            ans = "可能的结果有：\n" + "、".join(results)
+            await nbnhhsh.finish(ans)
