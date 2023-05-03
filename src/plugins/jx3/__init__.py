51ba620c00e353e100e05150ba7c74d036542b1f
HornCopper <zgu3316@gmail.com>
2023-05-03 16:39:17 +0800

    更新检测(Beta)

--- src/plugins/jx3/__init__.py
+++ src/plugins/jx3/__init__.py
@@ -34,6 +34,7 @@ from .macro import get_macro
 from .chitu import get_chitu, get_horse_reporter
 from .wanbaolou import get_wanbaolou
 from .firework import get_data as firework_
+from .update import *
 
 news = on_command("jx3_news", aliases={"新闻"}, priority=5)
 @news.handle()
@@ -320,7 +321,7 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     '''
     订阅内容，可选择订阅的内容：
 
-    目前支持：玄晶、新闻、开服。
+    目前支持：玄晶、新闻、开服、更新。
 
     Example：-订阅 玄晶
 
@@ -329,7 +330,7 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     path = DATA + "/" + str(event.group_id) + "/subscribe.json"
     now = json.loads(read(path))
     obj = args.extract_plain_text()
-    if obj not in ["玄晶","公告","开服"]:
+    if obj not in ["玄晶","公告","开服","更新"]:
         await subscribe.finish("请不要订阅一些很奇怪的东西，我可是无法理解的哦~")
     if obj in now:
         await subscribe.finish("已经订阅了哦，请不要重复订阅~")
@@ -350,7 +351,7 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     path = DATA + "/" + str(event.group_id) + "/subscribe.json"
     now = json.loads(read(path))
     obj = args.extract_plain_text()
-    if obj not in ["玄晶","公告","开服"]:
+    if obj not in ["玄晶","公告","开服","更新"]:
         await subscribe.finish("请不要取消订阅一些很奇怪的东西，我可是无法理解的哦~")
     if obj not in now:
         await subscribe.finish("尚未订阅，无法取消订阅哦~")
