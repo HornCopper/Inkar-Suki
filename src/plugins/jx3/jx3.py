102900ac2b1c3b0ea1fc077faef0724a77e25db3
HornCopper <zgu3316@gmail.com>
2023-05-22 21:59:11 +0800

    .

--- src/plugins/jx3/jx3.py
+++ src/plugins/jx3/jx3.py
@@ -325,6 +325,20 @@ async def zone(server, id, group):
         return ["玩家不存在或尚未在世界频道发言哦~"]
     return data["data"]["url"]
 
+async def pendant(name: str):
+    final_url = f"https://www.jx3api.com/data/other/pendant?name={name}&token={token}"
+    data = await get_api(final_url)
+    if data["code"] == 404:
+        return "没有找到该挂件哦~"
+    else:
+        data = data["data"][0]
+        pendant_type = data["class"]
+        pendant_name = data["name"]
+        desc = data["desc"]
+        source = data["source"]
+        msg = f"{pendant_name} - {pendant_type}\n{desc}\n获取线索：{source}"
+        return msg
+
 def server_mapping(server: str, group_id: str):
     if server in ["二合一","四合一","六合一","七合一","千岛湖","圣墓山","执子之手","平步青云","笑傲江湖","幽月轮","山雨欲来"]:
         return "幽月轮"
