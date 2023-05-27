4761a20bd41eb1c89be9cc15ff5905d382044a13
汉广 <serfend@foxmail.com>
2023-05-27 14:37:05 +0800

    refactor[sign-in]split entity logic

--- src/plugins/sign/__init__.py
+++ src/plugins/sign/__init__.py
@@ -25,28 +25,10 @@ sign_ = on_command("-签到", aliases={"-打卡"}, priority=5)
 async def sign(event: Event):
     if sign_main.wsigned(event.user_id):
         await sign_.finish(ms.at(event.user_id) + "\n你已经签到过了哦，不能重复签到。")
-    data = sign_main.generate_everyday_reward()
-    coin = data["coin"]
-    luck = data["luck"]
-    if luck == 0:
-        luck = "末吉签（1x）"
-    elif luck == 1:
-        luck = "中吉签（2x）"
-    elif luck == 2:
-        luck = "上吉签（3x）"
-    else:
-        luck = "上上签（4x）"
-    s = data["signed"] + 1
-    wlottery = data["wlottery"]
-    msg = ms.at(event.user_id) + f"\n签到成功！\n金币：+{coin}\n今日运势：{luck}"
-    if wlottery:
-        msg = msg + "\n触发额外奖励！已帮你额外添加了100枚金币！"
+    data = sign_main.generate_everyday_reward(event.user_id)
+
     sign_main.save_data(data, event.user_id)
-    continuity_day = sign_main.get_continuity(event.user_id)
-    if continuity_day != False:
-        msg = msg + f"\n已连续签到{continuity_day}天！"
-    msg = msg + f"\n在所有群内，您是第{s}位签到的哦~"
-    await sign_.finish(msg)
+    await sign_.finish(data.msg)
 
 coin = on_command("金币", aliases={"余额"}, priority=5)
 @coin.handle()
