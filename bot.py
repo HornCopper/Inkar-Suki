3373584637796b79d91767d9005afc6b48eb5df9
HornCopper <zgu3316@gmail.com>
2023-06-01 00:50:26 +0800

    Fixed a bug.

--- bot.py
+++ bot.py
@@ -7,23 +7,16 @@ import os
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 from sgtpyutils import logger
 
-tools_path: str = None
+botpy = os.getcwd()
+tools_path = botpy + "/src/tools"
 
+print(tools_path)
 
-def init_env():
-    logger.logger.info('检查和初始化环境')
-    global tools_path
-    tools_path = os.path.dirname(__file__)
-    # sys.path.append(os.path.join(tools_path, f'src'))
-    tools_path = os.path.join(tools_path, f'src{os.sep}tools')
-    sys.path.append(tools_path)
-    # src.db.init_database(Config.database)
-init_env()
 def check_folder(path: str, can_retry: bool = True):
     if os.path.isdir(path):
         return True
     if os.path.exists(path):
-        logger.logger.warn(f'{path}被文件占用，将其强行移除。')
+        logger.logger.warn(f"{path}被文件占用，将其强行移除。")
         os.remove(path)
         if not can_retry:
             return False
@@ -31,48 +24,48 @@ def check_folder(path: str, can_retry: bool = True):
     os.mkdir(path)
     return True
 
-
 def check_folders(folder_nest: dict, parent_path: str = None):
     
     if not parent_path:
-        parent_path = ''
+        parent_path = ""
     else:
-        parent_path = f'{parent_path}{os.sep}'
-    logger.logger.info(f'初始化系统文件夹:{parent_path}')
+        parent_path = f"{parent_path}{os.sep}"
+    logger.logger.info(f"初始化系统文件夹:{parent_path}")
     for f in folder_nest:
-        new_parent = f'{parent_path}{f}'
+        new_parent = f"{parent_path}{f}"
         check_folder(new_parent)
         children = folder_nest[f]
         if isinstance(children, dict):
             check_folders(children, new_parent)
+
 init_folders = {
-        './src': {
-            'data': None,
-            'cache': None,
-            'sign': None,
-            'assets': {
-                'jx3': {
-                    'skills': None,
-                    'icons': None,
-                    'achievement': None,
-                    'talents': None,
-                    'adventure': None,
+        "./src": {
+            "data": None,
+            "cache": None,
+            "sign": None,
+            "assets": {
+                "jx3": {
+                    "skills": None,
+                    "icons": None,
+                    "achievement": None,
+                    "talents": None,
+                    "adventure": None,
                 }
             },
-            'plugins': None
+            "plugins": None
         }
     }
+
 check_folders(init_folders)
 
-plugins = os.listdir('./src/plugins')
+plugins = os.listdir("./src/plugins")
 for i in plugins:
-    if not os.path.exists(f'./src/plugins/{i}/info.json'):
+    if not os.path.exists(f"./src/plugins/{i}/info.json"):
         raise FileNotFoundError(
             f"Plugin `{i}` required a `info.json` but not found. Please check and try again.")
         sys.exit(1)
 
-
-nonebot.init(tools_path=tools_path, log_level='DEBUG')
+nonebot.init(tools_path=tools_path, log_level="INFO")
 app = nonebot.get_asgi()
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
