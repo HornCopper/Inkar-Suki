76fc2e2b47bfc6ea6b83145ed67f14a8fe285735
汉广 <serfend@foxmail.com>
2023-06-05 15:50:10 +0800

    feat[test]add price test-case

--- tests/test_trade.py
+++ tests/test_trade.py
@@ -19,3 +19,19 @@ def test_trade_record():
     task = jx3_trade(state, event, obMessage(mc.tag))
     asyncio.run(task)
     mc.check_counter()
+
+
+def test_trade_price():
+
+    import src.plugins.jx3
+    mc = MessageCallback()
+    src.plugins.jx3.trade_ = mc
+
+    price_num_selected = src.plugins.jx3.price_num_selected
+    state = {'id': ['5_24428'], 'server': '唯我独尊'}  # 五行石六级
+    event = SFGroupMessageEvent()
+
+    task = price_num_selected(state, event, obMessage('0'))
+    asyncio.run(task)
+    mc.check_counter()
+
