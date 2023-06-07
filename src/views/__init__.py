0631b063d8c1fcb67bf38a81a0c619ee8fbcb314
汉广 <serfend@foxmail.com>
2023-06-07 22:46:02 +0800

    feat[front]add vue-echart

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import pathlib2
 import nonebot
@@ -17,16 +18,18 @@ def get_tag_content(data: str, tag: str) -> str:
         result.append(x[0])
     return '\n'.join(result)
 
-import asyncio
-def get_render_image(view_file_path: str, data: dict = None) -> bytes:
+
+def get_render_image(view_file_path: str, data: dict = None, delay: int = 0) -> bytes:
     if not data:
         data = {}
     content = get_render_content(view_file_path, data)
     file = pathlib2.Path(CACHE).joinpath(f'tpl_{get_uuid()}.html').as_posix()
     with open(file, 'w', encoding='utf-8') as f:
         f.write(content)
-    return asyncio.run(generate(file))
-def __init_data(data:dict):
+    return asyncio.run(generate(file, delay=delay))
+
+
+def __init_data(data: dict):
     '''覆盖原始data
     '''
     template = '''const _raw_setup = component.setup
@@ -36,8 +39,10 @@ def __init_data(data:dict):
       v_result = Object.assign(result, data)
       return v_result
     }
-    Vue.createApp(component).mount('#app')'''.replace('{{data}}',json.dumps(data))
+    Vue.createApp(component).mount('#app')'''.replace('{{data}}', json.dumps(data))
     return template
+
+
 def get_render_content(view_file_path: str, data: dict) -> str:
     '''
     渲染页面
@@ -51,8 +56,18 @@ def get_render_content(view_file_path: str, data: dict) -> str:
     content_template = get_tag_content(view_content, 'template')
     content = content.replace('<template />', content_template)
 
+    # 注入脚本
     content_script = get_tag_content(view_content, 'script')
     content_script += __init_data(data)
     content = content.replace('<component />', content_script)
-    
+
+    # 加载所有脚本文件
+    script_src = view_content.split('<script src="')
+    script_src = [x for index, x in enumerate(script_src) if index % 2]
+    script_src = [x.split('"') for x in script_src]
+    script_src = [x[0] for x in script_src if len(x) > 1]
+    script_src = [f'<script src="{x}" ></script>' for x in script_src]
+    content_src = "\n".join(script_src)
+    content = content.replace('<script_files />', content_src)
+
     return content
