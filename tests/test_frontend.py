b9c4af048306ddf2fbbf6bd2830464d5c7b3d04d
汉广 <serfend@foxmail.com>
2023-06-07 22:46:02 +0800

    feat[front]add vue-echart

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -2,6 +2,12 @@ import random
 from . import *
 from src.views import get_render_content, get_render_image
 test_base_path = 'src/views/test/index.html'
+test_echart_path = 'src/views/test/echart.html'
+
+
+def test_echart():
+    img = get_render_image(test_echart_path, delay=1000)
+    assert img.endswith('.png')
 
 
 def test_vue():
