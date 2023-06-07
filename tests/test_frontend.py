7225b92d92162cdbc727f83461cbb0fcda81dc01
汉广 <serfend@foxmail.com>
2023-06-07 18:28:37 +0800

    feat[frontend]init

---
+++ tests/test_frontend.py
@@ -0,0 +1,15 @@
+import random
+from . import *
+from src.views import get_render_content, get_render_image
+test_base_path = 'src/views/test/index.html'
+
+
+def test_vue():
+    img = get_render_image(test_base_path)
+    assert img.endswith('.png')
+
+
+def test_render_data():
+    rnd_id = random.randrange(int(10e7), int(10e8))
+    content = get_render_content(test_base_path, {'test': rnd_id})
+    assert rnd_id in content, 'content should be in rendered result'
