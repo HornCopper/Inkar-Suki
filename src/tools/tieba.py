667e20c1e8f95bd9750b34e4399394f74a66f379
汉广 <serfend@foxmail.com>
2023-06-08 15:26:55 +0800

    feat[tieba]add multi-page support

--- src/tools/tieba.py
+++ src/tools/tieba.py
@@ -6,13 +6,26 @@ import threading
 import time
 
 from sgtpyutils.logger import logger
+from sgtpyutils.extensions import distinct
+
+
 class SubjectStatus:
     def __init__(self, name: str) -> None:
         self.name: str = name
         self.last_thread_index: int = 0  # 上次已获取到的帖子序号
+        self.last_fetch: int = 0  # 上次加载时间
+        self._threads = []  # 存储的
+
+    @property
+    def threads(self):
+        return self._threads
+
+    @threads.setter
+    def threads(self, v: list):
+        self._threads = distinct(v, lambda x: x.tid)
 
 
-class Jx3Tieba:
+class Jx3Tieba(threading.Thread):
     Interval = 3  # 延迟
     Interval_Rnd = 1  # 随机变换
 
@@ -38,17 +51,22 @@ class Jx3Tieba:
                 self.current_subject)
         return self.subject_status[self.current_subject]
 
+    def run(self):
+        while True:
+
+            time.sleep(Jx3Tieba.get_interval())
+
     async def run_once(self) -> list:
-        threads = []
         s: SubjectStatus = self.current_status
-        pn = 0
+        pn = 1
         while True:
             async with aiotieba.Client() as client:
                 sub = self.current_subject
                 logger.debug(f'load forum[{sub}] at page {pn}')
                 _threads = await client.get_threads(sub, pn=pn)
                 t_index = _threads.page.total_page * _threads.page.page_size - pn
-                threads += [_threads[0:]]
+                s.threads += _threads[0:]
+                logger.debug(f'load forum[{sub}] at page {pn}, loaded threads:{len(s.threads)}')
                 if s.last_thread_index == 0:
                     s.last_thread_index = t_index
                     break
@@ -56,6 +74,7 @@ class Jx3Tieba:
                     break
                 pn += 1
                 time.sleep(Jx3Tieba.get_interval())
-        return threads
+        return s.threads
+
 
 client = Jx3Tieba(['唯满侠', '剑网三'])
--- tests/test_tieba.py
+++ tests/test_tieba.py
@@ -5,7 +5,8 @@ import asyncio
 def test_fetch_threads():
     from src.tools.tieba import client
     threads = asyncio.run(client.run_once())
-    assert len(threads) > 0, 'threads return should have content'
+    threads_count = len(threads)
+    assert threads_count > 0, 'threads return should have content'
     client.current_status.last_thread_index -= 1
     threads2 = asyncio.run(client.run_once())
-    assert len(threads2) > len(threads), 'thread count of 2 page should more than 1 page\'s'
+    assert len(threads2) > threads_count, 'thread count of 2 page should more than 1 page\'s'
