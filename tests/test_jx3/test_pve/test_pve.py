27c0134939eb8f4bb7a3b7016d11a5b507d78ca4
汉广 <serfend@foxmail.com>
2023-06-12 21:57:43 +0800

    feat[server]bind-server reset

--- tests/test_jx3/test_pve/test_pve.py
+++ tests/test_jx3/test_pve/test_pve.py
@@ -10,11 +10,14 @@ def test_recruit():
             return
         assert False, f'fail run:{msg}'
     mc = MessageCallback(handle_finish)
-    
+
     src.plugins.jx3.recruit.jx3_cmd_recruit = mc
     state = dict()
-    state['server'] = '双梦'
+    from src.plugins.jx3.bind import server_bind
+
     event = SFGroupMessageEvent()
+    server_bind(event.group_id, '双梦')
     task = func(state, event, obMessage('x 武狱黑牢'))
     asyncio.run(task)
     mc.check_counter()
+    server_bind(event.group_id, '')
