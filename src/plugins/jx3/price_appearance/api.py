b58048590c435b41376270f72511607d7d02dd2c
HornCopper <zgu3316@gmail.com>
2023-06-16 01:20:34 +0800

    Fixed a outdated api.

--- src/plugins/jx3/price_appearance/api.py
+++ src/plugins/jx3/price_appearance/api.py
@@ -6,6 +6,6 @@ async def item_(name: str = None): # 物价 <物品>
         return [PROMPT_NoToken]
     final_url = f"https://www.jx3api.com/view/trade/record?token={token}&robot={bot}&name={name}&scale=1"
     data = await get_api(final_url, proxy = proxies)
-    if data["code"] == 404:
+    if data["code"] == 400:
         return ["唔……尚未收录该物品。"]
     return data["data"]["url"]
