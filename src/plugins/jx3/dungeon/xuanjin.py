b435e372d52e94ee272a53036f416ed887c41abd
汉广 <serfend@foxmail.com>
2023-06-20 22:31:07 +0800

    fix[precious]use server_mapping

--- src/plugins/jx3/dungeon/xuanjin.py
+++ src/plugins/jx3/dungeon/xuanjin.py
@@ -4,7 +4,10 @@ from src.tools.file import *
 from src.tools.dep.path import *
 from src.tools.generate import *
 from src.plugins.help import css
-jx3_cmd_jx3_rare_gain = on_command("jx3_rare_gain", aliases={"cd","掉落"}, priority=5)
+jx3_cmd_jx3_rare_gain = on_command(
+    "jx3_rare_gain", aliases={"cd", "掉落"}, priority=5)
+
+
 @jx3_cmd_jx3_rare_gain.handle()
 async def jx3_rare_gain(event: GroupMessageEvent, args: Message = CommandArg()):
     '''
@@ -17,6 +20,7 @@ async def jx3_rare_gain(event: GroupMessageEvent, args: Message = CommandArg()):
     template = [Jx3Arg(Jx3ArgsType.server), Jx3Arg(Jx3ArgsType.default)]
     arg = get_args(args.extract_plain_text(), template)
     arg_server, arg_name = arg
+    arg_server = server_mapping(arg_server, event.group_id)
     if not arg_name:
         return await jx3_cmd_jx3_rare_gain.finish('没有输入物品名称哦')
     msg = await get_cd(arg_server, arg_name)
