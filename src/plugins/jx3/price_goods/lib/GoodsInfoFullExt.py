b2b49eda087c2b6512ef15694bf266634c5d4b49
汉广 <serfend@foxmail.com>
2023-06-22 19:37:25 +0800

    fix[trade]cache data should follow type

--- src/plugins/jx3/price_goods/lib/GoodsInfoFullExt.py
+++ src/plugins/jx3/price_goods/lib/GoodsInfoFullExt.py
@@ -4,12 +4,12 @@ import copy
 
 def check_cache_integrity(current_cache: dict, new_data: dict):
     # 当前缓存没有缓存品数，应为其缓存
-    cached_level = 'level' in current_cache
+    cached_level = not current_cache.get('level') is None
     should_update_cache = not cached_level
     if not should_update_cache:
         return
     current_cache['level'] = new_data.get('Level')
-    CACHE_Goods[current_cache['id']] = current_cache
+    CACHE_Goods[current_cache['id']] = dict2obj(GoodsInfo(), current_cache)
     flush_CACHE_Goods()
 
 
@@ -22,7 +22,7 @@ async def from_id(id: str) -> GoodsInfoFull:
     if not cache_data is None:
         current_cache = cache_data.to_dict()
         check_cache_integrity(current_cache, data)
-        
+
         x = copy.deepcopy(current_cache)
         x.update(data)
         data = x
