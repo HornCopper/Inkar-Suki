a793c8a85f10a81b30093415c6c4fd6ddd5c42e2
汉广 <serfend@foxmail.com>
2023-06-23 21:58:49 +0800

    feat[trade]support query favorite goods

--- src/plugins/jx3/price_goods/v2/renderer.py
+++ src/plugins/jx3/price_goods/v2/renderer.py
@@ -1,14 +1,14 @@
 from ..lib import *
 
 
-async def render_items(server: str, arg_item: str, arg_page: int, pageSize: int, totalCount: int, items: List[GoodsInfo]):
+async def render_items(server: str, arg_item: str, arg_page: int, pageSize: int, totalCount: int, items: List[GoodsInfo], template: str = 'goods_list'):
     '''
     渲染带价格概况的物品列表
     '''
     data = dict([[x.id, x.to_dict()] for x in items])
     data = json.dumps(data, cls=GoodsSerializerEncoder)
     data = json.loads(data)
-    img = await get_render_image('src/views/jx3/trade/goods_list.html', {
+    img = await get_render_image(f'src/views/jx3/trade/{template}.html', {
         'items': data,
         'server': server,
         'item_name': arg_item,
