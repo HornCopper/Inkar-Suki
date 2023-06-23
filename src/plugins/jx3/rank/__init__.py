19e6b87f8ed1ef3858cb28017d4d6c3a4a0a6b35
汉广 <serfend@foxmail.com>
2023-06-24 03:42:49 +0800

    feat[pvp-rank]add test-case for zlrank

--- src/plugins/jx3/rank/__init__.py
+++ src/plugins/jx3/rank/__init__.py
@@ -35,11 +35,11 @@ async def jx3_top100(event: GroupMessageEvent, args: Message = CommandArg()):
     data = await get_top100(server, boss, team)
     return await jx3_cmd_top100_.finish(data)
 
-rank = on_command("jx3_rank", aliases={"榜单"}, priority=5)
+jx3_cmd_rank = on_command("jx3_rank", aliases={"榜单"}, priority=5)
 
 
-@rank.handle()
-async def _(event: GroupMessageEvent, args: Message = CommandArg()):
+@jx3_cmd_rank.handle()
+async def jx3_rank(event: GroupMessageEvent, args: Message = CommandArg()):
     '''
     获取风云榜单：
 
@@ -50,7 +50,7 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     '''
     arg = args.extract_plain_text().split(" ")
     if len(arg) not in [2, 3]:
-        await rank.finish("唔……参数不正确哦，请检查后重试~")
+        return await jx3_cmd_rank.finish("唔……参数不正确哦，请检查后重试~")
     if len(arg) == 2:
         type1 = arg[0]
         server = None
@@ -61,16 +61,16 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
         type2 = arg[2]
     data = await rank_(type_1=type1, server=server, type_2=type2, group_id=event.group_id)
     if type(data) == type([]):
-        await rank.finish(data[0])
+        return await jx3_cmd_rank.finish(data[0])
     else:
-        await rank.finish(ms.image(data))
+        return await jx3_cmd_rank.finish(ms.image(data))
 
-zlrank_ = on_command("jx3_zlrank", aliases={"资历排行"}, priority=5)
-@zlrank_.handle()
-async def _(event: GroupMessageEvent, args: Message = CommandArg()):
+jx3_cmd_zlrank = on_command("jx3_zlrank", aliases={"资历排行"}, priority=5)
+@jx3_cmd_zlrank.handle()
+async def jx3_zlrank(event: GroupMessageEvent, args: Message = CommandArg()):
     arg = args.extract_plain_text().split(" ")
     if len(arg) not in [1, 2]:
-        return await zlrank_.finish("唔……参数不正确哦，请检查后重试~")
+        return await jx3_cmd_zlrank.finish("唔……参数不正确哦，请检查后重试~")
     if len(arg) == 1:
         server = None
         school = arg[0]
@@ -79,6 +79,6 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
         school = arg[1]
     data = await zlrank(server, school, str(event.group_id))
     if type(data) == type([]):
-        return await zlrank_.finish(data[0])
+        return await jx3_cmd_zlrank.finish(data[0])
     else:
-        return await zlrank_.finish(ms.image(Path(data).as_uri()))
+        return await jx3_cmd_zlrank.finish(ms.image(Path(data).as_uri()))
