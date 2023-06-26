5108ae28a5310d2435ff36e1067d07c6eab94362
汉广 <serfend@foxmail.com>
2023-06-26 18:38:54 +0800

    feat[jx-wiki]support select by tips

--- tests/test_jx3/test_wiki/__init__.py
+++ tests/test_jx3/test_wiki/__init__.py
@@ -3,6 +3,7 @@ from ... import *
 import src.plugins.jx3
 from src.plugins.jx3 import wiki
 
+
 def test_question():
     mc = MessageCallback()
     wiki.jx3_cmd_wiki = mc
@@ -11,7 +12,18 @@ def test_question():
     state = {}
     event = SFGroupMessageEvent()
 
-    mc.tag = '五行石'
+    mc.tag = '端午节活动'
+    task = func(state, event, obMessage(mc.tag))
+    asyncio.run(task)
+    mc.check_counter()
+
+    func = src.plugins.jx3.jx3_next_ques
+    mc.tag = 'xg0'  # 相关项
+    task = func(state, event, obMessage(mc.tag))
+    asyncio.run(task)
+    mc.check_counter()
+
+    mc.tag = 'yy0'  # 引用项
     task = func(state, event, obMessage(mc.tag))
     asyncio.run(task)
     mc.check_counter()
