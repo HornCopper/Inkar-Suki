3a20ee770f1d53a3f922b553fac385c25cb97d4a
汉广 <serfend@foxmail.com>
2023-06-29 09:50:38 +0800

    Revert "refactor[event]split logic and fix #149"
    Revert "Merge pull request #264 from serfend/EventSubscript"
    
    This reverts commit 4b5c0af4b4df763781321cccc902f582caeb73bd, reversing
    changes made to 4eab1aba2e2109fbcfe574d53bb84f1512328b21.

--- src/plugins/jx3/__init__.py
+++ src/plugins/jx3/__init__.py
@@ -15,18 +15,25 @@ async def _():
 
 ws_recev = on(type="WsRecv", priority=5, block=False)
 @ws_recev.handle()
-async def on_ws_recev(bot: Bot, event: RecvEvent):
+async def _(bot: Bot, event: RecvEvent):
     message = event.get_message()
-    if not message:
+    if message == "False":
         return
     groups = await bot.call_api("get_group_list")
     for i in groups:
         group = i["group_id"]
         subscribe = json.loads(read(DATA + "/" + str(group) + "/subscribe.json"))
         if message["type"] in subscribe:
-            to_send_messagee = event.render_message(group)
+            if message["type"] == "玄晶":
+                group_info = json.loads(read(DATA + "/" + str(group) + "/jx3group.json"))
+                if group_info["server"] != message["server"] and group_info["server"] != "":
+                    continue
+            elif message["type"] == "818":
+                group_info = json.loads(read(DATA + "/" + str(group) + "/jx3group.json"))
+                if group_info["server"] != "" and group_info["server"] != message["server"] and message["name"] != "剑网3":
+                    continue
             try:
-                await bot.call_api("send_group_msg", group_id = group, message = to_send_messagee)
+                await bot.call_api("send_group_msg", group_id = group, message = message["msg"])
             except:
                 logger.info(f"向群({i})推送失败，可能是因为风控、禁言或者未加入该群。")
 
