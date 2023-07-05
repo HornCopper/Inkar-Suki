0196923a20a015102e123827b86d486eaa110b73
汉广 <serfend@foxmail.com>
2023-07-06 07:39:03 +0800

    feat[subscribe]init base-callback

--- src/plugins/jx3/subscribe/api.py
+++ src/plugins/jx3/subscribe/api.py
@@ -1,9 +1,2 @@
 from src.tools.dep import *
 from .SubscribeRegister import *
-# 订阅 主题 订阅等级
-
-
-__subjects: list[SubscribeSubject] = []
-VALID_Subjects: dict[str, SubscribeSubject] = {}
-
-load_subjects(__subjects, VALID_Subjects)
