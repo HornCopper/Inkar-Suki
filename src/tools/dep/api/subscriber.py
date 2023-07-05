f4e0a185918787e3598e7207d9777cc47c336ed5
汉广 <serfend@foxmail.com>
2023-07-06 07:03:32 +0800

    feat[subscribe]use common subject-loader

---
+++ src/tools/dep/api/subscriber.py
@@ -0,0 +1,18 @@
+from src.tools.utils import *
+from ..data_server import *
+
+
+def load_or_write_subscribe(group_id: str, data: dict = None) -> dict:
+    '''
+    加载或写入群的订阅信息
+    @return {subject:data}
+    '''
+    path = f"{DATA}/{group_id}/subscribe.json"
+    if not data is None:
+        write(path, json.dumps(data, ensure_ascii=False))
+        return data
+    now = json.loads(read(path))
+    if not isinstance(now, dict):
+        # 用于记录更多信息
+        now = dict([[x, {}] for x in now])
+    return now
