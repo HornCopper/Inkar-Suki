1a4aafa7dbfaef510b8fca44f46cae94f58d03f0
汉广 <serfend@foxmail.com>
2023-07-06 07:52:02 +0800

    feat[subscribe]use lower-case name

--- tests/test_jx3/test_subscribe/test_schedule.py
+++ tests/test_jx3/test_subscribe/test_schedule.py
@@ -12,3 +12,15 @@ def test_subscribe_schedule():
     task = func(event, obMessage(mc.tag))
     asyncio.run(task)
     mc.check_counter()
+
+def test_subscribe_lower():
+    def check_sub(raw: str):
+        assert '已开启' in raw
+    mc = MessageCallback(cb_finish=check_sub)
+    src.plugins.jx3.subscribe.jx3_cmd_subscribe = mc
+    func = src.plugins.jx3.subscribe.jx3_subscribe
+    event = SFGroupMessageEvent(group_id=test_group_id)
+    mc.tag = '世界BoSs'
+    task = func(event, obMessage(mc.tag))
+    asyncio.run(task)
+    mc.check_counter()
