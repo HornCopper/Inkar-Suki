d4b3f88a7b8806ed7c4345477d35252f8fad2fde
汉广 <serfend@foxmail.com>
2023-07-08 09:10:38 +0800

    fix[subscribe]use data for test

--- src/plugins/jx3/subscribe/__init__.py
+++ src/plugins/jx3/subscribe/__init__.py
@@ -3,14 +3,7 @@ from .renderer import *
 jx3_cmd_subscribe = on_command("jx3_subscribe", aliases={"订阅"}, priority=5)
 
 
-@jx3_cmd_subscribe.handle()
-async def jx3_subscribe(event: GroupMessageEvent, args: Message = CommandArg()):
-    '''
-    订阅内容，可选择订阅的内容：
-    Example：订阅 玄晶 [级别]
-
-    Notice：一次只可订阅一个。
-    '''
+async def get_jx3_subscribe(event: GroupMessageEvent, args: Message = CommandArg()):
     now = load_or_write_subscribe(event.group_id)
     template = [Jx3Arg(Jx3ArgsType.default), Jx3Arg(Jx3ArgsType.default)]
     arg_sub, arg_info = get_args(args.extract_plain_text(), template)
@@ -22,22 +15,26 @@ async def jx3_subscribe(event: GroupMessageEvent, args: Message = CommandArg()):
         now[arg_sub] = {'arg': arg_info} if arg_info else {}
         load_or_write_subscribe(event.group_id, now)
         msg = f'已开启本群的[{arg_sub}(级别{arg_info or 0})]订阅！\n当收到事件时会自动推送，如需取消推送，请发送：退订 {arg_sub}'
-        
+    return now, subject, msg
+
+
+@jx3_cmd_subscribe.handle()
+async def jx3_subscribe(event: GroupMessageEvent, args: Message = CommandArg()):
+    '''
+    订阅内容，可选择订阅的内容：
+    Example：订阅 玄晶 [级别]
+
+    Notice：一次只可订阅一个。
+    '''
+    now, subject, msg = get_jx3_subscribe(event, args)
+
     result = await render_subscribe(VALID_Subjects, now, subject, msg)
     return await jx3_cmd_subscribe.finish(ms.image(Path(result).as_uri()))
 
 jx3_cmd_unsubscribe = on_command("jx3_unsubscribe", aliases={"退订"}, priority=5)
 
 
-@jx3_cmd_unsubscribe.handle()
-async def jx3_unsubscribe(event: GroupMessageEvent, args: Message = CommandArg()):
-    '''
-    退订某内容，可选择：
-
-    同上。
-
-    Example：退订 开服
-    '''
+async def get_jx3_unsubscribe(event: GroupMessageEvent, args: Message = CommandArg()):
     now = load_or_write_subscribe(event.group_id)
     template = [Jx3Arg(Jx3ArgsType.default), Jx3Arg(Jx3ArgsType.default)]
     arg_sub, arg_info = get_args(args.extract_plain_text(), template)
@@ -51,6 +48,18 @@ async def jx3_unsubscribe(event: GroupMessageEvent, args: Message = CommandArg()
         del now[arg_sub]
         load_or_write_subscribe(event.group_id, now)
         msg = f'已关闭本群的{arg_sub}订阅！\n如需再次开启，请发送：订阅 {arg_sub}'
+    return now, subject, msg
 
+
+@jx3_cmd_unsubscribe.handle()
+async def jx3_unsubscribe(event: GroupMessageEvent, args: Message = CommandArg()):
+    '''
+    退订某内容，可选择：
+
+    同上。
+
+    Example：退订 开服
+    '''
+    now, subject, msg = await get_jx3_unsubscribe(event, args)
     result = await render_subscribe(VALID_Subjects, now, subject, msg)
     return await jx3_cmd_unsubscribe.finish(ms.image(Path(result).as_uri()))
--- tests/test_jx3/test_subscribe/__init__.py
+++ tests/test_jx3/test_subscribe/__init__.py
@@ -6,62 +6,52 @@ import src.plugins.jx3.subscribe
 def test_subscribe():
     def check_sub(raw: str):
         assert '已开启' in raw
-    mc = MessageCallback(cb_finish=check_sub)
-    src.plugins.jx3.subscribe.jx3_cmd_subscribe = mc
-    func = src.plugins.jx3.subscribe.jx3_subscribe
+    func = src.plugins.jx3.subscribe.get_jx3_subscribe
     event = SFGroupMessageEvent(group_id=rnd_group)
-    mc.tag = '大攻防'
-    task = func(event, obMessage(mc.tag))
-    asyncio.run(task)
-    mc.check_counter()
+    task = func(event, obMessage('大攻防'))
+    result = asyncio.run(task)
+    msg = result[2]
+    check_sub(msg)
 
 
 def test_duplicate_subscripe():
-    def check_unsub(raw: str):
+    def check_sub(raw: str):
       assert '已经订阅了' in raw
     
-    mc = MessageCallback(cb_finish=check_unsub)
-    src.plugins.jx3.subscribe.jx3_cmd_subscribe = mc
-    mc.tag = '玄晶'
-    func = src.plugins.jx3.subscribe.jx3_subscribe
+    func = src.plugins.jx3.subscribe.get_jx3_subscribe
     event = SFGroupMessageEvent(group_id=rnd_group)
-    task = func(event, obMessage(mc.tag))
-    asyncio.run(task)
-    mc.check_counter()
+    task = func(event, obMessage('玄晶'))
+    result = asyncio.run(task)
+    msg = result[2]
+    check_sub(msg)
 
 def test_notexist_subscripe():
     def check_unsub(raw: str):
       assert '很奇怪' in raw
     
-    mc = MessageCallback(cb_finish=check_unsub)
-    src.plugins.jx3.subscribe.jx3_cmd_subscribe = mc
-    mc.tag = '不存在'
-    func = src.plugins.jx3.subscribe.jx3_subscribe
+    func = src.plugins.jx3.subscribe.get_jx3_unsubscribe
     event = SFGroupMessageEvent(group_id=rnd_group)
-    task = func(event, obMessage(mc.tag))
-    asyncio.run(task)
-    mc.check_counter()
+    task = func(event, obMessage('不存在'))
+    result = asyncio.run(task)
+    msg = result[2]
+    check_unsub(msg)
 
 def test_unsubscribe():
-    def check_sub(raw: str):
+    def check_unsub(raw: str):
         assert '已开启' in raw
-    mc = MessageCallback(cb_finish=check_sub)
-    src.plugins.jx3.subscribe.jx3_cmd_unsubscribe = mc
-    func = src.plugins.jx3.subscribe.jx3_unsubscribe
+    func = src.plugins.jx3.subscribe.get_jx3_unsubscribe
     event = SFGroupMessageEvent(group_id=rnd_group)
-    mc.tag = '玄晶'
-    task = func(event, obMessage(mc.tag))
-    asyncio.run(task)
-    mc.check_counter()
+    task = func(event, obMessage('玄晶'))
+    result = asyncio.run(task)
+    msg = result[2]
+    check_unsub(msg)
 
 
-    def check_sub(raw: str):
+    def check_unsub(raw: str):
         assert '尚未订阅' in raw
-    mc = MessageCallback(cb_finish=check_sub)
-    src.plugins.jx3.subscribe.jx3_cmd_unsubscribe = mc
-    func = src.plugins.jx3.subscribe.jx3_unsubscribe
+    func = src.plugins.jx3.subscribe.get_jx3_unsubscribe
     event = SFGroupMessageEvent(group_id=rnd_group)
-    mc.tag = '玄晶'
-    task = func(event, obMessage(mc.tag))
-    asyncio.run(task)
-    mc.check_counter()
+    task = func(event, obMessage('玄晶'))
+    result = asyncio.run(task)
+    msg = result[2]
+    check_unsub(msg)
