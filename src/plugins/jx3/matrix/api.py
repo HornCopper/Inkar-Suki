8a3ac475728b09d21f630373a9d3988bdd1cf2c4
HornCopper <zgu3316@gmail.com>
2023-07-14 11:26:11 +0800

    feat[equip]public-beta

--- src/plugins/jx3/matrix/api.py
+++ src/plugins/jx3/matrix/api.py
@@ -4,7 +4,7 @@ async def matrix_(name):
     name = aliases(name)
     if name == False:
         return "此心法不存在哦~请检查后重试。"
-    full_link = "{Config.jx3api_link}/data/school/matrix?name=" + name
+    full_link = f"{Config.jx3api_link}/data/school/matrix?name=" + name
     info = await get_api(full_link, proxy = proxies)
     if info["code"] == 400:
         return "此心法不存在哦~请检查后重试。"
