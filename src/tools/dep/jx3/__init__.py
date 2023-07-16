5f492203179bf01420e03102c802b59cd7fdf59c
汉广 <serfend@foxmail.com>
2023-07-16 16:54:22 +0800

    fix[renderer]import all-available

--- src/tools/dep/jx3/__init__.py
+++ src/tools/dep/jx3/__init__.py
@@ -3,7 +3,7 @@ try:
     logger.debug(f'load dependence:{__name__}')
 except:
     pass
-from src.tools.generate import generate, get_uuid, generate_by_url, stop_playwright
+from src.tools.generate import *
 from src.constant.jx3 import *
 from .jx3apiws import *
 from .sf_apiws import *
