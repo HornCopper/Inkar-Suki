a47e487480e2578452850e437463e7cbaf6b1c70
汉广 <serfend@foxmail.com>
2023-07-22 19:18:10 +0800

    feat[pvx-flower]support map&species args

--- src/plugins/jx3/flower/renderer.py
+++ src/plugins/jx3/flower/renderer.py
@@ -1,7 +1,11 @@
 from src.tools.dep import *
-async def renderer(server: str, data: list) -> str:
+
+
+async def renderer(server: str, arg_map: str, arg_species: str, data: list) -> str:
     img = await get_render_image('src/views/jx3/pvx/flower/index.html', {
         'data': data,
         'server': server,
-    }, delay=200)
+        'map': arg_map,
+        'species': arg_species,
+    })
     return img
