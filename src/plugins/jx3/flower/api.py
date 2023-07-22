1047de941b27a7b218e849d058cbd5e3e9d9b440
汉广 <serfend@foxmail.com>
2023-07-22 19:18:10 +0800

    feat[pvx-flower]support map&species args

--- src/plugins/jx3/flower/api.py
+++ src/plugins/jx3/flower/api.py
@@ -20,7 +20,7 @@ async def get_flower_by_tuilan(server: str, map: str = None, species: str = None
     if not result.get("code") == 0:
         return f'获取花价失败了,{result.get("msg")}'
     result = result.get("data")
-    logger.debug(f'flower-result[server{server}:map{map}:species{species}]\n{len(data)}')
+    logger.debug(f'flower-result[server={server},map={map},species={species}]count={len(result)}')
     return convert_data(result)
 
 def convert_data(raw: dict):
@@ -34,9 +34,9 @@ def convert_data(raw: dict):
         x_map = x.get('map')
         x_name = x.get('name').split('(')
         x_color = None
-        if len(x_name) > 1: # 无色花则直接返回
+        if len(x_name) > 1: # 有色花则变更花色
             x_color = x_name[1][:-1]  # .split('，')
-            x_name = x_name[0]
+        x_name = x_name[0] # 无色花则直接返回
         x_species = x.get('species')
         x_line = [line.get('number') for line in x.get('branch')]
         x_price = 1.5
