33bcd7769711fa3751b1b3efb1efb6e4fa9f7797
汉广 <serfend@foxmail.com>
2023-07-23 21:46:08 +0800

    fix[server-status]skip when no token

--- tests/test_jx3/test_server/test_server.py
+++ tests/test_jx3/test_server/test_server.py
@@ -11,6 +11,7 @@ def test_server_map():
     pass
 
 
+@pytest.mark.skipif(not token, reason="无token时不测试")
 def test_server_status():
     def default_cb_finish(msg: str):
         if '开服状态：' in msg:
