e60d1b71126c968f3f122750f1271eadddb2f1cd
汉广 <serfend@foxmail.com>
2023-07-23 20:27:38 +0800

    feat[ci]use arguments parser

---
+++ tests/test_jx3/test_pvp/test_user/test_user_info.py
@@ -0,0 +1,16 @@
+from .... import *
+
+
+@pytest.mark.skipif(not token)
+def test_user():
+    import src.plugins.jx3.user
+    func = src.plugins.jx3.user.jx3_player
+
+    def finish(msg: str):
+        assert 'file://' in msg, f'image generate fail:{msg}.'
+    mc = MessageCallback(cb_finish=finish)
+    src.plugins.jx3.user.jx3_cmd_roleInfo = mc
+    event = SFGroupMessageEvent()
+    task = func(event, obMessage('唯满侠 步龄'))
+    asyncio.run(task)
+    mc.check_counter()
