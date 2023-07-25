242f61376294f02541b0801a81dd55828f88c30c
汉广 <serfend@foxmail.com>
2023-07-25 23:02:02 +0800

    fix[common]图片引用歧义

--- src/plugins/jx3/achievement/adventure.py
+++ src/plugins/jx3/achievement/adventure.py
@@ -50,10 +50,10 @@ async def getAdventure(adventure: str):
 async def getAchievementsIcon(IconID: str):
     final_path = ASSETS + "/jx3/adventure/" + IconID + ".png"
     if os.path.exists(final_path):
-        return MessageSegment.image(Path(final_path).as_uri())
+        return ms.image(Path(final_path).as_uri())
     else:
         image_url = f"https://icon.jx3box.com/icon/{IconID}.png"
         cache = open(ASSETS + "/jx3/adventure/" + IconID + ".png", mode="wb")
         cache.write(await get_content(image_url))
         cache.close()
-        return MessageSegment.image(Path(final_path).as_uri())
+        return ms.image(Path(final_path).as_uri())
