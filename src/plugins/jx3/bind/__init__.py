204e094886c97633d0ebafbe1b072c4b393e7848
汉广 <serfend@foxmail.com>
2023-07-27 01:01:01 +0800

    fix[jx-manage]server bind invalid

--- src/plugins/jx3/bind/__init__.py
+++ src/plugins/jx3/bind/__init__.py
@@ -3,9 +3,14 @@ from src.tools.permission import checker
 from .api import *
 
 
-def server_bind(group_id: str, server: str):
+def server_bind(group_id: str, server: str) -> str:
+    """
+    绑定输入项为有效服务器，并返回该服务器名称
+    @group_id:str:群号
+    @server:str:服务器名
+    """
+    # 当server为None表示要清空，否则检查服务器名称有效性
     if not server == "":
-        # 当server为空表示要清空
         server = server_mapping(server)
         if not server:
             return [PROMPT_ServerNotExist]
@@ -13,6 +18,7 @@ def server_bind(group_id: str, server: str):
     now = json.loads(read(path))
     now["server"] = server
     write(path, json.dumps(now, ensure_ascii=False))
+    return server
 
 
 jx3_cmd_server_bind = on_command("jx3_bind", aliases={"绑定"}, priority=5)
@@ -20,17 +26,16 @@ jx3_cmd_server_bind = on_command("jx3_bind", aliases={"绑定"}, priority=5)
 
 @jx3_cmd_server_bind.handle()
 async def jx3_server_bind(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
+    # TODO 判别权限，应提取为专有函数
     personal_data = await bot.call_api("get_group_member_info", group_id=event.group_id, user_id=event.user_id, no_cache=True)
     group_admin = personal_data["role"] in ["owner", "admin"]
     robot_admin = checker(str(event.user_id), 8)
     if not group_admin and not robot_admin:
         return await jx3_cmd_server_bind.finish("唔……只有群主或管理员才可以修改哦！")
-    server = args.extract_plain_text()
-    group_id = str(event.group_id)
-    exact_server = server_mapping(server)
-    if not exact_server:
-        return await jx3_cmd_server_bind.finish("唔……服务器名称输入有误，绑定失败！")
-    server_bind(group_id=group_id, server=exact_server if server else "")
-    if server == "":
-        return await jx3_cmd_server_bind.finish("已清除本群的绑定信息！\n注意：推送将推送全服内容！")
+
+    template = [Jx3Arg(Jx3ArgsType.server)]
+    server, = get_args(args.extract_plain_text(), template)
+    server = server_bind(group_id=event.group_id, server=server)
+    if isinstance(server, list):
+        return await jx3_cmd_server_bind.finish(f"绑定失败：{server}")
     return await jx3_cmd_server_bind.finish("绑定成功！\n当前区服为：" + server)
