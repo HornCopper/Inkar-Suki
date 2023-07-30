0cba198cc58639a72c618c3dfb959338cbc359dc
HornCopper <zgu3316@gmail.com>
2023-07-31 03:09:28 +0800

    [upd]进度v2

--- src/plugins/jx3/achievement/__init__.py
+++ src/plugins/jx3/achievement/__init__.py
@@ -1,7 +1,8 @@
+from src.plugins.jx3.dungeon.api import mode_mapping
+
 from .api import *
 
 adventure_ = on_command("jx3_adventure", aliases={"成就"}, priority=5)
-
 
 @adventure_.handle()
 async def _(state: T_State, args: Message = CommandArg()):
@@ -38,7 +39,6 @@ async def _(state: T_State, args: Message = CommandArg()):
         await adventure_.send(msg)
         return
 
-
 @adventure_.got("num", prompt="发送序号以搜索，发送其他内容则取消搜索。")
 async def _(state: T_State, num: Message = Arg()):
     num = num.extract_plain_text()
@@ -58,7 +58,6 @@ async def _(state: T_State, num: Message = Arg()):
         await adventure_.finish("唔……输入的不是数字哦，取消搜索。")
 
 achievements = on_command("jx3_machi", aliases={"进度"}, priority=5)
-
 
 @achievements.handle()
 async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
@@ -86,3 +85,59 @@ async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
         await achievements.finish(data[0])
     else:
         await achievements.finish(ms.image(data))
+
+achievement_v2 = on_command("jx3_achievement_v2", aliases={"进度v2"}, priority=5)
+
+@achievement_v2.handle()
+async def _(event: GroupMessageEvent, args: Message = CommandArg()):
+    achievement = args.extract_plain_text().split(" ")
+    if len(achievement) not in [2, 3]:
+        await achievement_v2.finish(PROMPT_ArgumentInvalid)
+    if len(achievement) == 2:
+        server = None
+        id = achievement[0]
+        achi = achievement[1]
+    elif len(achievement) == 3:
+        server = achievement[0]
+        id = achievement[1]
+        achi = achievement[2]
+    data = await achi_v2(server, id, achi, event.group_id)
+    if type(data) == type([]):
+        await achievement_v2.finish(data[0])
+    else:
+        await achievement_v2.finish(ms.image(data))
+
+zone_achievement = on_command("jx3_zoneachi", aliases={"团本成就"}, priority=5)
+
+@zone_achievement.handle()
+async def _(event: GroupMessageEvent, args: Message = CommandArg()):
+    return # 施工中
+    arg = args.extract_plain_text().split(" ")
+    if len(arg) not in [2,3,4]:
+        await zone_achievement.finish(PROMPT_ArgumentInvalid)
+    group = str(event.group_id)
+    if len(arg) == 2:
+        server = None
+        id = arg[0]
+        zone = arg[1]
+        mode = "10人普通"
+    elif len(arg) == 3:
+        server = arg[0] if server_mapping(arg[0], group) != None else ""
+        if server != "":
+            id = arg[1]
+            zone = arg[2]
+            mode = "10人普通"
+        else:
+            id = arg[0]
+            zone = arg[1]
+            mode = mode_mapping(arg[2])
+    elif len(arg) == 4:
+        server = arg[0]
+        id = arg[1]
+        zone = arg[2]
+        mode = mode_mapping(arg[3])
+    data = await zone_achi(server ,id, zone, mode, group)
+    if type(data) == type([]):
+        await zone_achievement.finish(data[0])
+    else:
+        await zone_achievement.finish(ms.image(data))
