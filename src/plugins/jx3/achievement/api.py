2bef7d10e9f6e44a5cfad079a4dbf1800ffd003a
HornCopper <zgu3316@gmail.com>
2023-07-31 03:15:11 +0800

    [fix]VIEW未定义

--- src/plugins/jx3/achievement/api.py
+++ src/plugins/jx3/achievement/api.py
@@ -2,6 +2,8 @@ from src.tools.dep import *
 
 from .adventure import *
 
+VIEWS = TOOLS[:-5] + "views"
+
 try:
     from src.tools.dep.jx3.tuilan import gen_ts, gen_xsk, format_body # 收到热心网友举报，我们已对推栏的算法进行了隐藏。
 except:
