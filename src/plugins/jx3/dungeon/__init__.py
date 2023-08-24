584bc29af9ee98f221bc38a8146ae7c44a557fc8
HornCopper <zgu3316@gmail.com>
2023-08-24 14:23:11 +0800

    [upd]重写掉落列表(Part1)

--- src/plugins/jx3/dungeon/__init__.py
+++ src/plugins/jx3/dungeon/__init__.py
@@ -1,14 +1,14 @@
 from .api import *
 from .xuanjing import *
 
-zones = on_command("jx3_zones", aliases={"副本"}, priority=5)
+zones = on_command("jx3_zones_v1", aliases={"副本v1"}, priority=5)
 
 @zones.handle()
 async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
     """
     获取玩家副本通关记录：
 
-    Example：-副本 幽月轮 哭包猫@唯我独尊
+    Example：-副本v1 幽月轮 哭包猫@唯我独尊
     """
     group_server = getGroupServer(str(event.group_id))
     arg = args.extract_plain_text().split(" ")
@@ -28,7 +28,7 @@ async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
     else:
         await zones.finish(ms.image(data))
 
-zonesv2 = on_command("jx3_zones_v2", aliases={"副本v2"}, priority=5)
+zonesv2 = on_command("jx3_zones", aliases={"副本"}, priority=5)
 
 @zonesv2.handle()
 async def _(event: GroupMessageEvent, args: Message = CommandArg()):
@@ -50,18 +50,20 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     else:
         await zonesv2.finish(ms.image(data))
 
-drops = on_command("jx3_drops", aliases={"掉落列表"}, priority=5)
+# drops = on_command("jx3_drops", aliases={"掉落列表"}, priority=5)
 
-@drops.handle()
-async def _(event: GroupMessageEvent, args: Message = CommandArg()):
-    arg = args.extract_plain_text().split(" ")
-    if len(arg) != 3:
-        await drops.finish("唔……参数不正确哦~")
-    map = arg[0]
-    mode = arg[1]
-    boss = arg[2]
-    data = await genderater(map, mode, boss)
-    if type(data) != type([]):
-        await drops.finish(ms.image(Path(data).as_uri()))
-    else:
-        await drops.finish(data[0])
+# @drops.handle()
+# async def _(event: GroupMessageEvent, args: Message = CommandArg()):
+#     arg = args.extract_plain_text().split(" ")
+#     if len(arg) != 3:
+#         await drops.finish("唔……参数不正确哦~")
+#     map = arg[0]
+#     mode = arg[1]
+#     boss = arg[2]
+#     data = await generate(map, mode, boss)
+#     if type(data) != type([]):
+#         await drops.finish(ms.image(Path(data).as_uri()))
+#     else:
+#         await drops.finish(data[0])
+
+# Working
