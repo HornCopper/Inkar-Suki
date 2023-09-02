0ed68529af31730e2f7c38c90eec4b758e19d0e3
HornCopper <zgu3316@gmail.com>
2023-09-03 02:35:25 +0800

    [upd]台风相关功能

---
+++ src/plugins/geography/__init__.py
@@ -0,0 +1,23 @@
+from .typhoon import *
+
+tyList = on_command("typhoon_list", aliases={"台风列表"}, priority=5)
+
+@tyList.handle()
+async def _(event: GroupMessageEvent):
+    data = await get_typhoon_list()
+    if not data:
+        msg = "、".join(data)
+        await tyList.finish(f"当前西北太平洋有以下台风：\n{msg}")
+    else:
+        await tyList.finish("当前西北太平洋洋面并无台风。")
+
+tyPath = on_command("typhoon_path", aliases={"台风路径"}, priority=5)
+
+@tyPath.handle()
+async def _(event: GroupMessageEvent, args: Message = CommandArg()):
+    tyName = args.extract_plain_text()
+    data = await get_typhoon_path(tyName)
+    if type(data) == type([]):
+        await tyPath.finish(data[0])
+    else:
+        await tyPath.finish(ms.image(data))
