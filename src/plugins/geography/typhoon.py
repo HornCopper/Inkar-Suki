8d00b2f9bbcfca68f4f9ce89a4a0afd852258f95
HornCopper <zgu3316@gmail.com>
2023-09-03 02:35:25 +0800

    [upd]台风相关功能

---
+++ src/plugins/geography/typhoon.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup as bs
+
+from src.tools.dep import *
+
+async def get_typhoon_list():
+    api = "http://typhoon.nmc.cn/weatherservice/typhoon/jsons/list_default"
+    typhoon_list = []
+    data = await get_url(api)
+    for i in json.loads(data[28:-2])["typhoonList"]:
+        if "start" in i:
+            typhoon_list.append(i[2])
+    return typhoon_list if len(typhoon_list) != 0 else False
+
+async def get_typhoon_path(name):
+    api = "http://nmc.cn/publish/typhoon/probability.html"
+    data = await get_url(api)
+    bs_obj = bs(data, "html.parser")
+    typhoon_list = bs_obj.find_all(class_="p-wrap")[0].div.ul.find_all("li")
+    flag = False
+    for i in typhoon_list:
+        if i.get_text() == name:
+            if i["class"] == "actived":
+                return ["该台风近期已被中央气象台停止编号，搜索失败。"]
+            url = "http://nmc.cn" + i["href"]
+            flag = True
+    if not flag:
+        return ["未找到您要搜索的台风，请检查名称后重试，或是是否处在近期。"]
+    obj_data = await get_api(url)
+    new_bs_obj = bs(obj_data, "html.parser")
+    img = new_bs_obj.find_all(class_="imgblock")[0].div.img["src"]
+    return img 
