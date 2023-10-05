6caae7cd258a6c4cadc7541840d6eb42e91c299d
HornCopper <zgu3316@gmail.com>
2023-10-06 02:20:27 +0800

    [fix]修改变量名

--- src/plugins/jx3/firework/api.py
+++ src/plugins/jx3/firework/api.py
@@ -40,14 +40,14 @@ async def get_firework_image(server, name):
     data = data["data"]
     for i in range(len(data)):
         tablecontent.append(template.replace("$count", str(i)).replace("$map", data[i]["map_name"]).replace("$sed", data[i]["sender"]).replace("$rec", data[i]["recipient"]).replace("$time", convert_time(data[i]["time"])).replace("$firework", data[i]["name"]))
-    tablecontent = "\n".join(tablecontent)
+    tablecontents = "\n".join(tablecontent)
     html = read(VIEWS + "/jx3/firework/firework.html")
     saohua = await get_api(f"https://www.jx3api.com/data/saohua/random?token={token}")
     saohua = saohua["data"]["text"]
     appinfo_time = time.strftime("%H:%M:%S",time.localtime(time.time()))
     appinfo = f"烟花记录 · {server} · {name} · 当前时间：{appinfo_time}"
     font = ASSETS + "/font/custom.ttf"
-    html = html.replace("$customfont", font).replace("$appinfo", appinfo).replace("$tablecontent", tablecontent).replace("$randomsaohua", saohua)
+    html = html.replace("$customfont", font).replace("$appinfo", appinfo).replace("$tablecontent", tablecontents).replace("$randomsaohua", saohua)
     final_html = CACHE + "/" + get_uuid() + ".html"
     write(final_html, html)
     final_path = await generate(final_html, False, "table", False)
