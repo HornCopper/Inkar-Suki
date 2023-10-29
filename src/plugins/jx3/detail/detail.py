09f1f0b2d413af46dd5e5e124ea71ef018dccc67
HornCopper <zgu3316@gmail.com>
2023-10-29 23:19:40 +0800

    [fix]移除register，副本总览移入内测

--- src/plugins/jx3/detail/detail.py
+++ src/plugins/jx3/detail/detail.py
@@ -42,6 +42,7 @@ async def get_guid(server: str, name: str):
 
 async def get_menu(): # 获取总览分类
     menu_data = await get_tuilan_data("https://m.pvp.xoyo.com/achievement/list/menu")
+    logger.info(menu_data)
     for i in menu_data["data"]:
         if i["name"] == "江湖行":
             for x in i["subClass"]:
