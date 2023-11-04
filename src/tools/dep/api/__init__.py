fd4dff68c0851118b23c8b27295345bf7e3efc41
汉广 <serfend@foxmail.com>
2023-11-04 20:20:00 +0800

    fix[server-status]empty-data handler

--- src/tools/dep/api/__init__.py
+++ src/tools/dep/api/__init__.py
@@ -15,11 +15,10 @@ proxies = None
 # initilize jx3-ticket
 ticket = Config.jx3_token
 device_id = ticket and ticket.split("::")
-device_id = device_id[1] if len(device_id) > 1 else None
+device_id = device_id[1] if device_id and len(device_id) > 1 else None
 
 # initilize private api
 try:
-    from src.tools.dep.jx3.tuilan import gen_ts, gen_xsk, format_body # 收到热心网友举报，我们已对推栏的算法进行了隐藏。
+    from src.tools.dep.jx3.tuilan import gen_ts, gen_xsk, format_body  # 收到热心网友举报，我们已对推栏的算法进行了隐藏。
 except:
     pass
-
