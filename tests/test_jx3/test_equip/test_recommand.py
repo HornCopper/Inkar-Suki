56d099c629ef2d91a5f4d1f6d333c97e04dec42f
汉广 <serfend@foxmail.com>
2023-11-12 14:05:31 +0800

    feat[unit-test]recommend item init

--- tests/test_jx3/test_equip/test_recommand.py
+++ tests/test_jx3/test_equip/test_recommand.py
@@ -17,6 +17,24 @@ def test_recommend_menu():
     asyncio.run(task)
     mc.check_counter()
 
+    return state
+
+
+@pytest.mark.skipif(True, reason="当前配置文件页面无法访问到")
+@pytest.mark.skipif(not Config.jx3_token, reason="无token时不测试")
+def test_recommend():
+    mc = MessageCallback()
+    equip_recommend.jx3_cmd_equip_recommend = mc
+
+    func = equip_recommend.equip_recmded
+    state = test_recommend_menu()
+    event = SFGroupMessageEvent()
+
+    mc.tag = '0'
+    task = func(event, state, obMessage(mc.tag))
+    asyncio.run(task)
+    mc.check_counter()
+
 
 def test_kunfu():
     assert aliases('策T') == '铁牢律'
