7f15dde50f59c9da345f87cd1d00c420a72619fc
汉广 <serfend@foxmail.com>
2023-12-30 23:42:35 +0800

    feat[bot]document init

--- src/tools/dep/bot/bot_env/command.py
+++ src/tools/dep/bot/bot_env/command.py
@@ -2,10 +2,24 @@
 import nonebot
 from nonebot import on_command
 from sgtpyutils.logger import logger
+from .document import DocumentGenerator
+
+import nonebot.matcher
+
+
+def __start_hook_command_handler():
+    logger.debug('hooked func on_command.handle')
+    @DocumentGenerator.counter
+    def __hook_on_command_handle(cls, parameterless):
+        return nonebot.matcher.Matcher._handle(cls, parameterless)
+    nonebot.matcher.Matcher._handle = nonebot.matcher.Matcher.handle  # 初始化
+    nonebot.matcher.Matcher.handle = __hook_on_command_handle
 
 
 def __start_hook_on_command():
     logger.debug('hooked func on_command')
+
+    @DocumentGenerator.register
     def __hook_on_command(
         cmd,
         rule=None,
@@ -21,3 +35,4 @@ def __start_hook_on_command():
 
 
 __start_hook_on_command()
+__start_hook_command_handler()
