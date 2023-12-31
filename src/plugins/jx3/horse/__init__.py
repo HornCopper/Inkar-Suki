e4e4c4e740c41cf224989021bb6b6df9e408a660
汉广 <serfend@foxmail.com>
2023-12-31 13:09:43 +0800

    feat[horse]document init

--- src/plugins/jx3/horse/__init__.py
+++ src/plugins/jx3/horse/__init__.py
@@ -1,6 +1,16 @@
 from .api import *
 
-ct = on_command("jx3_ct", aliases={"赤兔"}, priority=5)
+ct = on_command(
+    "jx3_ct",
+    aliases={"赤兔"},
+    priority=5,
+    description='获取赤兔刷新信息',
+    catalog='jx3.pvx.property.horse.chitu',
+    example=[Jx3ArgsType.server],
+    document='''数据来源于剑三盒子
+    获取当前各个地图马场的数据并整合
+    得到马儿所在的地图'''
+)
 
 
 @ct.handle()
