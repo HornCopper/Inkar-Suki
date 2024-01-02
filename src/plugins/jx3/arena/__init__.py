1b65c0b639a876f503869de1907072136c2faa77
汉广 <serfend@foxmail.com>
2024-01-02 08:56:00 +0800

    refactor[arena]split logic

--- src/plugins/jx3/arena/__init__.py
+++ src/plugins/jx3/arena/__init__.py
@@ -1,45 +1,48 @@
 from .api import *
 
-arena = on_command("jx3_arena", aliases={"名剑"}, priority=5)
+jx3_cmd_arena_records = on_command("jx3_arena_records", aliases={"战绩", "名剑战绩"}, priority=5)
 
 
-@arena.handle()
-async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
-    arg = args.extract_plain_text().split(" ")
-    if len(arg) not in [2, 3, 4]:
-        return await arena.finish(PROMPT_ArgumentCountInvalid)
-    if arg[0] == "战绩":
-        if len(arg) not in [2, 3, 4]:
-            return await arena.finish(PROMPT_ArgumentCountInvalid)
-        mode = "22"
-        if len(arg) == 2:
-            server = None
-            name = arg[1]
-        elif len(arg) == 3:
-            server = arg[1]
-            name = arg[2]
-        else:
-            server = arg[1]
-            name = arg[2]
-            mode = arg[3]
-        data = await arena_(object="战绩", server=server, name=name, group_id=event.group_id, mode=mode)
-        if type(data) == type([]):
-            return await arena.finish(data[0])
-        else:
-            return await arena.finish(ms.image(data))
-    elif arg[0] == "排行":
-        if len(arg) != 2:
-            return await arena.finish(PROMPT_ArgumentCountInvalid)
-        data = await arena_(object="排行", mode=arg[1], group_id=event.group_id)
-        if type(data) == type([]):
-            return await arena.finish(data[0])
-        else:
-            return await arena.finish(ms.image(data))
-    elif arg[0] == "统计":
-        if len(arg) != 2:
-            return await arena.finish(PROMPT_ArgumentCountInvalid)
-        data = await arena_(object="统计", mode=arg[1], group_id=event.group_id)
-        if type(data) == type([]):
-            return await arena.finish(data[0])
-        else:
-            return await arena.finish(ms.image(data))
+@jx3_cmd_arena_records.handle()
+async def jx3_carena_records(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
+    template = [
+        Jx3Arg(Jx3ArgsType.server),
+        Jx3Arg(Jx3ArgsType.user),
+        Jx3Arg(Jx3ArgsType.default, default='22')
+    ]
+    server, user, pvp_mode = get_args(args, template, event)
+    if server is None:
+        return await jx3_cmd_arena_records.finish(PROMPT_ServerNotExist)
+    if user is None:
+        return await jx3_cmd_arena_records.finish(PROMPT_UserNotExist)
+
+    data = await arena_records(object="战绩", server=server, name=user, mode=pvp_mode)
+    if type(data) == type([]):
+        return await jx3_cmd_arena_records.finish(data[0])
+    return await jx3_cmd_arena_records.finish(ms.image(data))
+
+
+jx3_cmd_arena_rank = on_command("jx3_arena_rank", aliases={"排行", "名剑排行"}, priority=5)
+
+
+@jx3_cmd_arena_records.handle()
+async def jx3_arena_records(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
+    template = [Jx3Arg(Jx3ArgsType.default, default='22')]
+    pvp_mode, = get_args(args, template, event)
+    data = await arena_rank(mode=pvp_mode)
+    if type(data) == type([]):
+        return await jx3_cmd_arena_records.finish(data[0])
+    return await jx3_cmd_arena_records.finish(ms.image(data))
+
+jx3_cmd_arena_statistics = on_command("jx3_arena_statistics", aliases={"统计", "名剑统计"}, priority=5)
+
+
+@jx3_cmd_arena_records.handle()
+async def jx3_arena_records(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
+    template = [Jx3Arg(Jx3ArgsType.default, default='22')]
+    pvp_mode, = get_args(args, template, event)
+    data = await arena_statistics(mode=pvp_mode)
+    if type(data) == type([]):
+        return await jx3_cmd_arena_records.finish(data[0])
+    else:
+        return await jx3_cmd_arena_records.finish(ms.image(data))
