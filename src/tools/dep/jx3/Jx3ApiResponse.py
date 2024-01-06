3f7041a41a6c21ca0553269186d781e9b771fc1b
汉广 <serfend@foxmail.com>
2024-01-06 11:03:15 +0800

    refactor[arena]merge

--- src/tools/dep/jx3/Jx3ApiResponse.py
+++ src/tools/dep/jx3/Jx3ApiResponse.py
@@ -1,3 +1,9 @@
+from __future__ import annotations
+from src.tools.dep.api.argparser import *
+import enum
+from src.tools.utils.request import *
+
+
 class Jx3ApiResponse:
     def convert_403(self):
         if '侠客隐藏了游戏信息' in self.msg:
@@ -35,3 +41,24 @@ class Jx3ApiResponse:
     def output_url(self):
         err = self.error_msg
         return [err] or self.url
+
+
+class ArgRequire(enum.Enum):
+    token = Jx3Arg.requireToken
+    ticket = Jx3Arg.requireTicket
+
+
+class Jx3ApiRequest:
+    def __init__(self, url: str, argRequest: ArgRequire = None) -> None:
+        self.url = url
+        self.argRequest = argRequest
+
+    async def output_url(self):
+        data = await get_api(self.url)
+        response = Jx3ApiResponse(data)
+        return response.output_url
+
+    @classmethod
+    def request(cls, x: Jx3ApiRequest):
+        '''TODO AOP generate'''
+        return 
