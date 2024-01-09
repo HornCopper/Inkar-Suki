d7168664eecf09ef5db466a02ec7f614fc721165
汉广 <serfend@foxmail.com>
2024-01-09 22:38:59 +0800

    fix[dev]mgr list

--- src/plugins/developer_tools/__init__.py
+++ src/plugins/developer_tools/__init__.py
@@ -174,7 +174,7 @@ util_cmd_handle_request = on_command(
     description='',
     catalog=permission.bot.group.apply,
     example=[
-        Jx3Arg(Jx3ArgsType.group_id),
+        Jx3Arg(Jx3ArgsType.group_id, is_optional=True),
         Jx3Arg(Jx3ArgsType.bool, is_optional=True),
         Jx3Arg(Jx3ArgsType.remark, is_optional=True),
     ],
