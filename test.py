b12e7d49468e0a2f798262b246d5594c10915684
汉广 <serfend@foxmail.com>
2024-01-10 20:33:17 +0800

    fix[subscribe]remove outside

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
-from tests.test_docs.test_generate import test_docs_generate as func
-# from tests.test_jx3.test_pvx.test_horse import test_horse_list_view as func
+# from tests.test_docs.test_generate import test_docs_generate_detail as func
+# from tests.test_bot.test_group_config import test_group_config as func
+from tests.test_jx3.test_pvx.test_flower import test_flower_price as func
+# from tests.test_jx3.test_price.test_trade_v2 import test_default_server as func
 func()
