c285b47b59beca92db174a924f27c8f1ae4d2829
汉广 <serfend@foxmail.com>
2024-01-10 20:10:11 +0800

    fix[flower]arg

--- tests/test_jx3/test_pvx/test_flower.py
+++ tests/test_jx3/test_pvx/test_flower.py
@@ -10,13 +10,13 @@ def test_flower_price():
     src.plugins.jx3.flower.jx3_cmd_flower = mc
     state = dict()
     event = SFGroupMessageEvent()
-    event.message = obMessage('唯满侠')
+    event.message = obMessage('花 唯满侠')
     args = Jx3Arg.arg_factory(src.plugins.jx3.jx3_cmd_flower, event)
     task = func(state, event, args)
     asyncio.run(task)
     mc.check_counter()
 
-    event.message = obMessage('唯满侠 不存在地图')
+    event.message = obMessage('花 唯满侠 不存在地图')
     args = Jx3Arg.arg_factory(src.plugins.jx3.jx3_cmd_flower, event)
     task = func(state, event, args)
     asyncio.run(task)
