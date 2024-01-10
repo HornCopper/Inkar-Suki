648cc1a32619952e884ba4c40063def3b1bde516
汉广 <serfend@foxmail.com>
2024-01-11 00:50:50 +0800

    fix[saohua]rate choice

--- src/tools/dep/common_api/none_dep_api/jx3_saohua.py
+++ src/tools/dep/common_api/none_dep_api/jx3_saohua.py
@@ -10,13 +10,14 @@ if not _saohua_db.get('items'):
     _saohua_db['items'] = ['好，这里显示一条骚话']
 
 
-async def saohua(cache_rate: float = 0.99):
+async def saohua(cache_rate: float = 0.95):
+    items = _saohua_db['items']
     if random.random() < cache_rate:
-        return random.choice(_saohua_db)
+        return random.choice(items)
     buff = await get_api(f"https://www.jx3api.com/data/saohua/random?token={_token}")
     data = buff.get('data') or {}
     _saohua = data.get('text')
     if _saohua:
-        _saohua_db['items'].append(_saohua)
+        items.append(_saohua)
         return _saohua
     return random.choice(_saohua_db)
