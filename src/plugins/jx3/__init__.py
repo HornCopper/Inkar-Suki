7bfca3f4591f607bd46f1b8e13a075f01ff6cd9e
汉广 <serfend@foxmail.com>
2024-01-11 14:47:48 +0800

    feat[dev]mgr usr config

--- src/plugins/jx3/__init__.py
+++ src/plugins/jx3/__init__.py
@@ -57,19 +57,40 @@ global_cmd_update_grp_config = on_command(
 
 @global_cmd_update_grp_config.handle()
 async def global_update_grp_config(event: GroupMessageEvent, args: list[Any] = Depends(Jx3Arg.arg_factory)):
-    x = Permission(event.user_id).judge(10, '查询和更新配置')
-    if not x.success:
-        return await global_cmd_update_grp_config.finish(x.description)
     arg_group, arg_path, arg_value = args
+    msg = await _update_grp_config(arg_path, arg_value, event.user_id, arg_group)
+    return await global_cmd_update_grp_config.send(msg)
+
+global_cmd_update_usr_config = on_command(
+    'update_usr_config',
+    example=[
+        Jx3Arg(Jx3ArgsType.group_id),
+        Jx3Arg(Jx3ArgsType.string, alias='配置路径'),
+        Jx3Arg(Jx3ArgsType.string, alias='配置值', default='VIEW'),
+    ]
+)
+
+
+@global_cmd_update_usr_config.handle()
+async def global_update_usr_config(event: GroupMessageEvent, args: list[Any] = Depends(Jx3Arg.arg_factory)):
+    arg_group, arg_path, arg_value = args
+    msg = await _update_grp_config(arg_path, arg_value, event.user_id, arg_group)
+    return await global_cmd_update_usr_config.send(msg)
+
+
+async def _update_grp_config(arg_path, arg_value, user_id, arg_group=None, arg_user=None):
+    x = Permission(user_id).judge(10, '查询和更新配置')
+    if not x.success:
+        return x.description  # TODO 全局检测返回类型并进行动作
     new_val = Ellipsis if arg_value == 'VIEW' else arg_value
 
-    gc = GroupConfig(arg_group)
+    gc = GroupConfig(arg_group) if arg_group else GroupUserConfig(arg_user)
     result = gc.mgr_property(arg_path, new_val)
     update_path = gc._db.database_filename
-    db_instance = hex(id(gc._db.data_obj))
+    db_instance = hex(id(gc._db.data_obj)) & 0xffffffff
     update_result = f'已管理更新{db_instance}@{arg_path}\nnew_value={new_val},result=\n{result}'
     msg = f'{update_path}\n{update_result}'
-    return await global_cmd_update_grp_config.send(msg)
+    return msg
 
 
 ws_recev = on(type="WsRecv", priority=5, block=False)
