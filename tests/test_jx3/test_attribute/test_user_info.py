79c56d3daebfb0edae8cf25c3e5d91a4cd56ea5b
汉广 <serfend@foxmail.com>
2024-01-14 16:54:39 +0800

    fix[tests]use module full-path

--- tests/test_jx3/test_attribute/test_user_info.py
+++ tests/test_jx3/test_attribute/test_user_info.py
@@ -1,20 +1,19 @@
-from .... import *
+from ... import *
 
 
 @pytest.mark.skipif('get_tuilan_articles' not in dir(), reason='无api可用')
 @pytest.mark.skipif(not Config.jx3api_globaltoken, reason="无apitoken时不测试")
 def test_user():
     import src.plugins.jx3.user
-    func = src.plugins.jx3.user.jx3_player
+    func = src.plugins.jx3.user.jx3_roleInfo
 
     def finish(msg: str):
         assert '25523361' in msg # uid
     mc = MessageCallback(cb_finish=finish)
-    raw_matcher = src.plugins.jx3.user.jx3_cmd_roleInfo
-    src.plugins.jx3.user.jx3_cmd_roleInfo = mc
+    src.plugins.jx3.user.v2.jx3_cmd_roleInfo = mc
     event = SFGroupMessageEvent()
     event.message = obMessage("角色信息 唯满侠 步龄")
-    args = Jx3Arg.arg_factory(raw_matcher, event)
+    args = Jx3Arg.arg_factory(src.plugins.jx3.jx3_cmd_roleInfo, event)
     task = func(event, args)
     asyncio.run(task)
     mc.check_counter()
