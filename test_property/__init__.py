851e68ed322e5b734121046f38b6880ea32d69ec
汉广 <serfend@foxmail.com>
2024-01-14 12:39:28 +0800

    refactor[attribute]publish

--- test_property/__init__.py
+++
@@ -1,9 +0,0 @@
-from .Jx3Player import *
-
-
-async def run():
-    data = await Jx3PlayerDetailInfo.from_username('破阵子', '烤冷面不加蛋')
-    data = await Jx3PlayerDetailInfo.from_username('斗转星移', '云澈')
-
-    filebase_database.Database.save_all()
-    pass
