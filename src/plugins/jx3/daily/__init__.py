2c872e4a787942c7e52efe2d9a2877c415d3e7dd
汉广 <serfend@foxmail.com>
2024-01-15 23:36:55 +0800

    feat[daily]support day-offset

--- src/plugins/jx3/daily/__init__.py
+++ src/plugins/jx3/daily/__init__.py
@@ -30,6 +30,7 @@ jx3_cmd_daily_txt = on_command(
     priority=5,
     example=[
         Jx3Arg(Jx3ArgsType.server, is_optional=True),
+        Jx3Arg(Jx3ArgsType.number, default=0, alias='x天后'),
         Jx3Arg(Jx3ArgsType.number, default=-1, alias='新话百分比'),
     ],
     document="""
@@ -46,7 +47,25 @@ jx3_cmd_daily_txt = on_command(
 
 @jx3_cmd_daily_txt.handle()
 async def jx3_daily_txt(event: GroupMessageEvent, args: list[Any] = Depends(Jx3Arg.arg_factory)):
-    arg_server,  arg_rate = args
-    content = await daily_txt(arg_server, group_id=event.group_id)
+    arg_server, arg_offset,  arg_rate = args
+    content = await daily_txt(arg_server, predict_day_num=arg_offset, group_id=event.group_id)
     suffix = (f'\n{"-"*10}\n{await saohua(arg_rate/100)}') if arg_rate > 0 else ''
     return await jx3_cmd_daily_txt.finish(f'{content.text}{suffix}')
+
+
+jx3_cmd_daily_predict_txt = on_command(
+    "jx3_daily_predict_txt",
+    aliases={"日常预测", "周常预测"},
+    priority=5,
+    example=[
+        Jx3Arg(Jx3ArgsType.server, is_optional=True),
+    ],
+    document="""
+    日常预测TODO 似乎所有日常都是按顺序循环的。则其实可以根据历史预测未来
+    """
+)
+
+@jx3_cmd_daily_txt.handle()
+async def jx3_daily_predict_txt(event: GroupMessageEvent, args: list[Any] = Depends(Jx3Arg.arg_factory)):
+    arg_server, = args
+    return await jx3_cmd_daily_predict_txt.send('预测功能施工中')
