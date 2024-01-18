8243888bb669042032d7170194c409bd388050f9
汉广 <serfend@foxmail.com>
2024-01-18 19:07:15 +0800

    fix[git]remove other opt

--- src/plugins/github/parse_v2.py
+++ src/plugins/github/parse_v2.py
@@ -9,7 +9,13 @@ class GithubHandle(main):
         return f'{Config.name}又偷偷给自己更新啦~本次更新了来自{pusher}的{len(commits)}个包'
 
     def pull_request(body):
-        action = '申请的新更新' if body["action"] == 'opened' else '已批准的更新'
+        action = body["action"]
+        if action == 'opened':
+            action = '申请的新更新'
+        elif action == 'closed':
+            action = '已批准的更新'
+        else:
+            return # 不处理其他情况
         sender = body["sender"]["login"]
         source = body["pull_request"]["head"]["label"]
         return f'{Config.name}准备要更新啦~本次是来自{sender}的{action}。更新到{source}'
