c50589833fe06ce584ed74e3a881a489e80c9d88
汉广 <serfend@foxmail.com>
2024-01-18 19:07:05 +0800

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,11 +9,22 @@ def pytest_sessionfinish(session: pytest.Session, exitstatus: int):
 
 
 def migrate_test_resources():
+    current = pathlib2.Path(__file__).parent.parent.parent  # file->res->tests
     root_path = pathlib2.Path(f'tests{os.sep}res').absolute().as_posix()
+    r_len = len(root_path)
     logger.debug('start migrating test-resources')
-    shutil.copytree(root_path, '.')
 
-            
+    for root, dirs, files in os.walk(root_path):
+        if not files:
+            continue
+        if not os.path.exists(root):
+            os.makedirs(root)
+
+        for file in files:
+            source = f'{root}{os.sep}{file}'
+            target = f'{current}{os.sep}{root[r_len:]}{os.sep}{file}'
+            logger.debug(f'testingenv migrate:{source} to {target}')
+            shutil.copy(source, target)
 
 
 migrate_test_resources()
