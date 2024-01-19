3132bebb834b63e37d09232e7e975929c9f42dd0
汉广 <serfend@foxmail.com>
2024-01-19 14:07:39 +0800

    feat[notice]v2 init

--- src/plugins/jx3/notice/v1/api.py
+++ src/plugins/jx3/notice/v1/api.py
@@ -2,10 +2,6 @@ from __future__ import annotations
 from src.tools.dep import *
 
 
-
-
-
-
 async def announce_by_jx3api():
     final_url = f"{Config.jx3api_link}/view/web/news/announce?robot={bot}&token={token}"
     data = await get_api(final_url, proxy=proxies)
