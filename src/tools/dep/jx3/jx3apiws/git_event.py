4eda93df8f2c69721ac43ec12b4f2c7a33338379
汉广 <serfend@foxmail.com>
2024-01-21 01:02:27 +0800

    feat[git]update-event init

--- src/tools/dep/jx3/jx3apiws/git_event.py
+++ src/tools/dep/jx3/jx3apiws/git_event.py
@@ -1,3 +1,4 @@
+from jx3_event import *
 from fastapi import Request, FastAPI
 from src.tools.config import *
 from src.tools.utils import *
@@ -5,6 +6,19 @@ from src.tools.dep.bot.bot_env.github import *
 import nonebot
 app: FastAPI = nonebot.get_app()
 
+
+class GithubUpdateEvent(RecvEvent):
+    """更新推送事件"""
+
+    __event__ = "WsRecv.GithubUpdate"
+    message_type = "GithubUpdate"
+
+    @overrides(RecvEvent)
+    def get_message(self) -> dict:
+        return {
+            "type": "机器人更新",
+            "msg": self.server
+        }
 
 @app.post(Config.web_path)
 async def on_git_update(req: Request):
@@ -15,3 +29,7 @@ async def on_git_update(req: Request):
     # current_handler = GithubBaseParser
     message = getattr(current_handler, event)(body)
     logger.debug(f'on_git_update new message:{message}')
+
+    event = GithubUpdateEvent()
+    event.server = message
+    await BotEventController.handle_msg(event)
