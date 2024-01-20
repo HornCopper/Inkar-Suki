b3c05e1acfff286685c085fde3cd97852e4c0ebe
汉广 <serfend@foxmail.com>
2024-01-20 23:56:16 +0800

    feat[git]event

---
+++ src/tools/dep/jx3/jx3apiws/git_event.py
@@ -0,0 +1,17 @@
+from fastapi import Request, FastAPI
+from src.tools.config import *
+from src.tools.utils import *
+from src.tools.dep.bot.bot_env.github import *
+import nonebot
+app: FastAPI = nonebot.get_app()
+
+
+@app.post(Config.web_path)
+async def on_git_update(req: Request):
+    body = await req.json()
+    repo = body["repository"]["full_name"]
+    event = req.headers.get("X-GitHub-Event")
+    current_handler = GithubJx3Handler
+    # current_handler = GithubBaseParser
+    message = getattr(current_handler, event)(body)
+    logger.debug(f'on_git_update new message:{message}')
