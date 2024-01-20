1a62cbc14c0c18e90f2841f79222dbac7e8cf827
汉广 <serfend@foxmail.com>
2024-01-20 22:58:42 +0800

    fix[attribute]default use current

--- src/plugins/jx3/user/v3/__init__.py
+++ src/plugins/jx3/user/v3/__init__.py
@@ -77,10 +77,14 @@ async def get_jx3_attribute3(template: list[Any] = Depends(Jx3Arg.arg_factory)):
         return await jx3_cmd_attribute3.finish(f'未能找到来自[{arg_server}]的角色[{arg_user}]{err_msg}')
     user = data.user.to_dict()
     current = data.attributes[data.current_score]
-
-    attribute, filter_type = get_attribute_from_data(data, arg_page)
-    if no_DATA := not attribute:
+    
+    filter_type = AttributeType.Unknown
+    if arg_page == 0: # 为0时直接用当前
         attribute = current
+    else:
+        attribute, filter_type = get_attribute_from_data(data, arg_page)
+        if no_DATA := not attribute:
+            attribute = current
 
     # 实际结果的类型
     result_attributeType = attribute and attribute.page.attr_type
