6dfab6653cdfadc46fb868df19048ba812c6741b
汉广 <serfend@foxmail.com>
2024-01-20 19:03:50 +0800

    feat[dev]mapper support args

---
+++ src/tools/dep/common_api/none_dep_api/CommandMapper/tests.py
@@ -0,0 +1,29 @@
+from .CommandRunner import *
+
+
+def test_command_mapper_common():
+    mapper = {
+        '早': '早早早',
+        '你好-$1-小狗': '你好-小狗-$1',
+        '你好-$1-大狗': '你好-大狗-$1',
+        '你好-$pos-大猫': '你好-大大-$pos',
+        '你好2-$po-大猫-$pos-2': '你好-大大-$pos-$po-3',
+        '张2': '张3',
+        '张1': '张2',
+        '张0': '张1',
+        '张x': '张y',
+        '张y': '张x',
+        '张x1': '张x2',
+        '张x2': '张x',
+    }
+    assert CommandMapper(mapper).convert('早') == '早早早'
+    assert CommandMapper(mapper).convert('早 北京') == '早早早 北京'
+    assert CommandMapper(mapper).convert(['早', '北京']) == '早早早 北京'
+
+    assert CommandMapper(mapper).convert('张1 北京') == '张3 北京'
+
+    assert CommandMapper(mapper).convert('张x 北京') == '张y 北京'
+    assert CommandMapper(mapper).convert('张x1 北京') == '张y 北京'
+
+    assert CommandMapper(mapper).convert('你好 北京 小狗') == '你好 小狗 北京'
+    assert CommandMapper(mapper).convert('你好 山东 大狗 1') == '你好 大狗 山东 1'
--- src/tools/dep/common_api/none_dep_api/__init__.py
+++ src/tools/dep/common_api/none_dep_api/__init__.py
@@ -1,9 +1,15 @@
+# 通用
+from .common import *
+from .CommandMapper import *
+from .HtmlTagHandler import *
+
+# 机器人
+from .prompt import *
+
+# JX3
 from .jx3_daily import daily_, daily_txt, DailyResponse
 from .jx3_saohua import saohua
-from .prompt import *
 from .Jx3ApiResponse import *
-from .tuilan import *
-from .common import *
 from .Jx3Player import *
 from .WucaiAttribute import *
-from .HtmlTagHandler import *
+from .tuilan import *
