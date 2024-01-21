8bb79a8117e832981a110e97b40f178f937c79bd
汉广 <serfend@foxmail.com>
2024-01-21 10:21:18 +0800

    feat[attribute]reload if no cache

--- src/tools/dep/common_api/none_dep_api/Jx3Player/Jx3PlayerBase/Jx3PlayerDetailInfo.py
+++ src/tools/dep/common_api/none_dep_api/Jx3Player/Jx3PlayerBase/Jx3PlayerDetailInfo.py
@@ -23,22 +23,27 @@ class Jx3PlayerDetailInfo:
     def get_attributes_by_attr_type(self, attr_type: AttributeType) -> Jx3UserAttributeInfo:
         '''直接通过缓存获取最新配置'''
         key = self.key
+        result = None
         if his := BaseJx3UserAttribute.cache_latest_attr.get(key):
             if isinstance(attr_type, AttributeType):
                 attr_type = attr_type.value
             attr_score = his.get(str(attr_type.value))
-            return self.attributes.get(attr_score)
-        return None
 
-    def get_attributes_by_page(self, date: DateTime = None, page: AttributeType = None) -> dict[str, Jx3UserAttributeInfo]:
+            result = self.attributes.get(attr_score)
+        if result is None:
+            # TODO 后期数据全部完成缓存后应删除
+            result = self.get_attributes_by_filter(attr_type=attr_type)
+        return result
+
+    def get_attributes_by_filter(self, date: DateTime = None, attr_type: AttributeType = None) -> dict[str, Jx3UserAttributeInfo]:
         '''筛选指定的属性 TODO 筛选页面对PVE-DPS和HPS不准确'''
         attrs = self.attributes
         if date is not None:
             date = DateTime(date)
             xattrs = filter(lambda x: not attrs[x].is_outdated(date), attrs)
             attrs = list(xattrs)
-        if page is not None:
-            xattrs = filter(lambda x: attrs[x].page.attr_type & page == page, attrs)
+        if attr_type is not None:
+            xattrs = filter(lambda x: attrs[x].page.attr_type & attr_type == attr_type, attrs)
             attrs = list(xattrs)
         return dict([x, self.attributes[x]] for x in attrs)
 
