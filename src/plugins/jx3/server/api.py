d789650a034e55897c34d03e3ab8a7a96897aea9
汉广 <serfend@foxmail.com>
2024-01-22 13:37:10 +0800

    feat[server-status]record previous

--- src/plugins/jx3/server/api.py
+++ src/plugins/jx3/server/api.py
@@ -2,16 +2,17 @@ from src.tools.dep import *
 
 
 async def server_status(server: str = None):
-    server = server_mapping(server)
-    full_link = f"{Config.jx3api_link}/data/server/check?server={server}&token={token}"
-    info = await get_api(full_link)
-    data = info and info.get('data')
-    if isinstance(data, list):
+    server: Server = Server.from_alias(server)
+    if server:
+        full_link = f"{Config.jx3api_link}/data/server/check?server={server.name}&token={token}"
+        info = await get_api(full_link)
+        data = info and info.get('data')
+    if server is None or isinstance(data, list):
         return "没有这样的服务器"
     status = data and data.get('status')
     prefix = "开服状态："
-    if status == 1:
-        return f"{prefix}{server}已开服。"
-    elif status == 0:
-        return f"{prefix}{server}维护中。"
-    return f"{prefix}未能查询到服务状态"
+    status_desc = '已开服。' if status else '维护中。'
+
+    msg = f'{prefix} {server} {status_desc}'
+    msg = f'{msg}\n{server.record_desc}'
+    return msg
