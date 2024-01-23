f97a3863be7404e448edf662e07f14f7779a717f
汉广 <serfend@foxmail.com>
2024-01-23 21:40:41 +0800

    fix[args]tip when invalid

--- tests/test_args/test_full_factory.py
+++ tests/test_args/test_full_factory.py
@@ -14,3 +14,13 @@ def test_auto_args_number():
     args = argparser.get_args('原命令 [empty] 是', templates, event)
     pass
     assert args == ['原命令', None, None, True]
+
+
+def test_invalid_arguments():
+    templates = [
+        Jx3Arg(Jx3ArgsType.server),
+    ]
+    event = SFGroupMessageEvent()
+    event.group_id = 1211125  # 避免缓存
+    args = argparser.get_args('哈哈哈', templates, event)
+    assert type(args) is InvalidArgumentException
