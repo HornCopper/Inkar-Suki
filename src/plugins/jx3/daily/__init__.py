3120c7ac6a137ab178cbc9c6b65309c491522fd5
汉广 <serfend@foxmail.com>
2024-01-23 12:22:49 +0800

    fix[daily]error check

--- src/plugins/jx3/daily/__init__.py
+++ src/plugins/jx3/daily/__init__.py
@@ -49,8 +49,10 @@ jx3_cmd_daily_txt = on_command(
 async def jx3_daily_txt(event: GroupMessageEvent, args: list[Any] = Depends(Jx3Arg.arg_factory)):
     arg_server, arg_offset,  arg_rate = args
     content = await daily_txt(arg_server, predict_day_num=arg_offset, group_id=event.group_id)
+    if isinstance(content, list):
+        return await jx3_cmd_daily_txt.finish(content[0])
     suffix = (f'\n{"-"*10}\n{await saohua(arg_rate/100)}') if arg_rate > 0 else ''
-    return await jx3_cmd_daily_txt.finish(f'{content.text}{suffix}')
+    return await jx3_cmd_daily_txt.send(f'{content.text}{suffix}')
 
 
 jx3_cmd_daily_predict_txt = on_command(
@@ -64,6 +66,7 @@ jx3_cmd_daily_predict_txt = on_command(
     日常预测TODO 似乎所有日常都是按顺序循环的。则其实可以根据历史预测未来
     """
 )
+
 
 @jx3_cmd_daily_txt.handle()
 async def jx3_daily_predict_txt(event: GroupMessageEvent, args: list[Any] = Depends(Jx3Arg.arg_factory)):
