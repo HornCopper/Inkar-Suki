1d25cb73ba13afb6b6fe2f4ffdebc46ab99295d4
HornCopper <zgu3316@gmail.com>
2024-01-24 20:58:56 +0800

    [upd]喜报(the bulltion of glad)

---
+++ src/plugins/bulletin/api.py
@@ -0,0 +1,24 @@
+from src.tools.dep import *
+from PIL import Image, ImageDraw, ImageFont
+
+async def get_bulletinG(msg: str):
+    background = Image.open(bot_path.PLUGINS + "/bulletinG.jpg")
+    draw = ImageDraw.Draw(background)
+    width, height = background.size
+    if len(msg) <= 5:
+        fszie = 72
+    elif 5 < len(msg) <= 10:
+        fsize = 48
+    elif 10 < len(msg) <= 20:
+        fsize = 48
+        msg = msg[:9] + "\n" + msg[9:]
+    font = ImageFont.truetype(bot_path.ASSETS + "/font/syst-heavy.otf", fsize)
+    bbox = draw.textbbox((0, 0), msg, font=font)  
+    text_width = bbox[2] - bbox[0]  
+    text_height = bbox[3] - bbox[1]
+    x = (width - text_width) / 2  
+    y = (height - text_height) / 2
+    draw.text((x, y), msg, font=font, align="center", fill=(255, 0, 0), outline=(255, 255, 0), stroke_fill=(255, 255, 0), stroke_width=1)
+    new_file = bot_path.CACHE + "/" + get_uuid() + ".jpg"
+    background.save(new_file)
+    return Path(new_file).as_uri()
