781a7c5c04ddfa8d9976c6674bb104b8265fa88d
HornCopper <zgu3316@gmail.com>
2024-01-31 19:28:39 +0800

    [upd]云从订阅

--- src/tools/dep/common_api/SubscribeRegister/events/events_base.py
+++ src/tools/dep/common_api/SubscribeRegister/events/events_base.py
@@ -6,8 +6,9 @@ def run(__subjects: list):
     __subjects.append(SubscribeSubject("公告", "当游戏官方发布游戏公告时进行推送。"))
     __subjects.append(SubscribeSubject("开服", "当群聊所绑定的服务器开服的时候进行推送。"))
     __subjects.append(SubscribeSubject("更新", "当游戏有新版本时进行推送。"))
-    __subjects.append(SubscribeSubject("818", "当群聊绑定的服务器的相关的贴吧出现八卦帖子时推送。"))
+    __subjects.append(SubscribeSubject("818", "当贴吧出现八卦帖子时推送。"))
     __subjects.append(SubscribeSubject("诛恶", "【暂时关闭】当群聊绑定的服务器诛恶事件触发时进行推送。"))
+    __subjects.append(SubscribeSubject("云从", "推送云从社预告。"))
 
 
 def run_after(__subjects: list):
