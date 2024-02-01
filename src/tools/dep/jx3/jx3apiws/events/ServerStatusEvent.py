a30b9b64936218e7992d3e46807808c66d5e4489
HornCopper <zgu3316@gmail.com>
2024-02-01 17:49:48 +0800

    [upd]监狱牢头の入群&退群提示、[fix]开服&云从时间戳转换

--- src/tools/dep/jx3/jx3apiws/events/ServerStatusEvent.py
+++ src/tools/dep/jx3/jx3apiws/events/ServerStatusEvent.py
@@ -10,6 +10,8 @@ class ServerStatusEvent(RecvEvent):
     message_type = "ServerStatus"
     status: bool
     """服务器状态"""
+    server: str
+    """服务器名称"""
 
     @validator("status", pre=True)
     def check_status(cls, v):
@@ -17,7 +19,7 @@ class ServerStatusEvent(RecvEvent):
 
     @property
     def status_desc(self):
-        status = "已开服" if self.status else "已维护"
+        status = "开服" if self.status else "维护"
         return status
 
     @property
@@ -25,23 +27,12 @@ class ServerStatusEvent(RecvEvent):
         log = f"开服推送事件：[{self.server}]状态-{self.status_desc}"
         return log
 
-    def record(self):
-        s: Server = self.mapped_server
-        if s is None:
-            return
-        s_type = ServerRecordType.last_start if self.status else ServerRecordType.last_stop
-        s.get_record(s_type, DateTime().timestamp())
-
     @overrides(RecvEvent)
     def get_message(self) -> dict:
-        time_now = DateTime().tostring('%H:%M')
-        msg_base = f"{time_now} {self.mapped_server_name} {self.status_desc}"
-
-        self.record() # 先记录本次，然后再计算时间差
-        msg_prev = self.mapped_server.record_desc
-        msg = f'{msg_base}{msg_prev}'
+        time_now = DateTime().tostring("%H:%M")
+        msg = f"{time_now} {self.mapped_server_name} {self.status_desc}"
         return {
             "type": "开服",
-            "server": self.mapped_server.name,
+            "server": self.server,
             "msg": msg
         }
