6fdfaac9df5402bc5a8d019f83f8228d0dd8fe64
HornCopper <zgu3316@gmail.com>
2024-02-04 10:46:52 +0800

    [del]删除`single_session`，unhelpful

--- src/plugins/jx3/trade/api.py
+++ src/plugins/jx3/trade/api.py
@@ -3,6 +3,22 @@ from src.tools.dep import *
 filters = ["无封","无皇","封头","封护","封裤","封项","封鞋","封囊"]
 banned = ["囊","头饰","裤","护臂","腰坠","项链","鞋"]
 
+template_msgbox = """
+<div class="message-box">
+    <div class="element">
+        <div class="cell"><span style="color:green">当日最低价↓</span></div>
+        <div class="cell">$low</div>
+    </div>
+    <div class="element">
+        <div class="cell">当日均价</div>
+        <div class="cell">$equal</div>
+    </div>
+    <div class="element">
+        <div class="cell"><span style="color:red">当日最高价↑</span></div>
+        <div class="cell">$high</div>
+    </div>
+</div>"""
+
 async def getImg(server: str, name: str):
     for i in filters:
         if name.find(i) != -1:
@@ -33,6 +49,50 @@ async def getImg(server: str, name: str):
         current = await get_api(f"https://next2.jx3box.com/api/item-price/{itemList_searchable[0]}/detail?server={server}")
         if current["data"]["prices"] != None:
             currentStatus = 1
-        
+        if currentStatus:
+            msgbox = template_msgbox.replace("$low", toCoinImage(convert()))
+        else:
+            msgbox = ""
     else:
         # 如果有多个，则分别显示近期价格，只显示最新一条
+
+def toCoinImage(rawString: str):
+    to_replace = [["砖", brickl], ["金", goldl], ["银", silverl], ["铜", copperl]]
+    for waiting in to_replace:
+        rawString = rawString.replace(waiting[0], waiting[1])
+    processedString = rawString
+    return processedString
+
+def convert(price: int):
+    if 1 <= price <= 99: # 铜
+        return f"{price} 铜"
+    elif 100 <= price <= 9999: # 银
+        copper = price % 100
+        silver = (price - copper) / 100
+        if copper == 0:
+            return str(int(silver)) + " 银" 
+        else:
+            return str(int(silver)) + " 银" + " " + str(int(copper)) + " 铜"
+    elif 10000 <= price <= 99999999: # 金
+        copper = price % 100
+        silver = ((price - copper) % 10000) / 100
+        gold = (price - copper - silver) / 10000
+        msg = str(int(gold)) + " 金"
+        if str(int(silver)) != "0":
+            msg = msg + " " + str(int(silver)) + " 银"
+        if str(int(copper)) != "0":
+            msg = msg + " " + str(int(copper)) + " 铜"
+        return msg
+    elif 100000000 <= price: # 砖
+        copper = price % 100
+        silver: int = ((price - copper) % 10000) / 100
+        gold = ((price - copper - silver*100) % 100000000) / 10000
+        brick = (price - copper - silver*100 - gold*10000) / 100000000
+        msg = str(int(brick)) + " 砖"
+        if str(int(gold)) != "0":
+            msg = msg + " " + str(int(gold)) + " 金"
+        if str(int(silver)) != "0":
+            msg = msg + " " + str(int(silver)) + " 银"
+        if str(int(copper)) != "0":
+            msg = msg + " " + str(int(copper)) + " 铜"
+        return msg
