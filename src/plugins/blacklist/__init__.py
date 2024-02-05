3b72ce07749ea77fdef95fbdd7fc7ec3a637c18f
HornCopper <zgu3316@gmail.com>
2024-02-05 11:11:31 +0800

    [fix]优化部分代码风格

--- src/plugins/blacklist/__init__.py
+++ src/plugins/blacklist/__init__.py
@@ -1,15 +1,4 @@
 from src.tools.dep import *
-from src.tools.config import Config
-from src.tools.utils import nodetemp
-from src.tools.permission import checker
-from src.tools.file import read, write
-
-import json
-
-from nonebot import on_command
-from nonebot.adapters import Message
-from nonebot.adapters.onebot.v11 import Bot, Event, GroupMessageEvent
-from nonebot.params import CommandArg
 
 
 block = on_command("block", aliases={"避雷", "加入黑名单"}, priority=5)  # 综合避雷名单-添加
@@ -22,19 +11,19 @@ async def _(bot: Bot, event: Event, args: Message = CommandArg()):
     group_admin = personal_data["role"] in ["owner", "admin"]
     permission = checker(str(event.user_id), 5)
     if not permission and not group_admin:
-        return await block.finish("唔……只有群主或管理员才能修改哦~")
+        await block.finish("唔……只有群主或管理员才能修改哦~")
     if len(arg) != 2:
-        return await block.finish("唔……需要2个参数，第一个参数为玩家名，第二个参数是原因~\n提示：理由中请勿包含空格。")
+        await block.finish("唔……需要2个参数，第一个参数为玩家名，第二个参数是原因~\n提示：理由中请勿包含空格。")
     sb = arg[0]
     reason = arg[1]
     new = {"ban": sb, "reason": reason}
     now = json.loads(read(bot_path.DATA + "/" + str(event.group_id) + "/blacklist.json"))
     for i in now:
         if i["ban"] == sb:
-            return await block.finish("该玩家已加入黑名单。")
+            await block.finish("该玩家已加入黑名单。")
     now.append(new)
     write(bot_path.DATA + "/" + str(event.group_id) + "/blacklist.json", json.dumps(now, ensure_ascii=False))
-    return await block.finish("成功将该玩家加入黑名单！")
+    await block.finish("成功将该玩家加入黑名单！")
 
 unblock = on_command("unblock", aliases={"移出黑名单"}, priority=5)  # 解除避雷
 
@@ -45,9 +34,9 @@ async def _(bot: Bot, event: Event, args: Message = CommandArg()):
     group_admin = personal_data["role"] in ["owner", "admin"]
     permission = checker(str(event.user_id), 5)
     if not permission and not group_admin:
-        return await block.finish("唔……只有群主或管理员才能修改哦~")
+        await block.finish("唔……只有群主或管理员才能修改哦~")
     if len(arg) != 1:
-        return await unblock.finish("参数仅为玩家名，请勿附带任何信息！")
+        await unblock.finish("参数仅为玩家名，请勿附带任何信息！")
     sb = arg[0]
     now = json.loads(read(bot_path.DATA + "/" + str(event.group_id) + "/blacklist.json"))
     for i in now:
@@ -55,8 +44,8 @@ async def _(bot: Bot, event: Event, args: Message = CommandArg()):
             now.remove(i)
             write(bot_path.DATA + "/" + str(event.group_id) + "/blacklist.json",
                   json.dumps(now, ensure_ascii=False))
-            return await unblock.finish("成功移除该玩家的避雷！")
-    return await unblock.finish("移除失败！尚未避雷该玩家！")
+            await unblock.finish("成功移除该玩家的避雷！")
+    await unblock.finish("移除失败！尚未避雷该玩家！")
 
 sblock = on_command("sblock", aliases={"查找黑名单"}, priority=5)  # 查询是否在避雷名单
 
@@ -64,15 +53,15 @@ sblock = on_command("sblock", aliases={"查找黑名单"}, priority=5)  # 查询
 async def _(event: Event, args: Message = CommandArg()):
     arg = args.extract_plain_text().split(" ")
     if len(arg) != 1:
-        return await sblock.finish("参数仅为玩家名，请勿附带任何信息！")
+        await sblock.finish("参数仅为玩家名，请勿附带任何信息！")
     sb = arg[0]
     now = json.loads(read(bot_path.DATA + "/" + str(event.group_id) + "/blacklist.json"))
     for i in now:
         if i["ban"] == sb:
             reason = i["reason"]
             msg = f"玩家[{sb}]被避雷的原因为：\n{reason}"
-            return await sblock.finish(msg)
-    return await sblock.finish("该玩家没有被本群加入黑名单哦~")
+            await sblock.finish(msg)
+    await sblock.finish("该玩家没有被本群加入黑名单哦~")
 
 
 template = """
