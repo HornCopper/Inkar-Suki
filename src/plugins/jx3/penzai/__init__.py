d2491c03e5524a1c3fc9f1f05ed21013439b96ed
HornCopper <zgu3316@gmail.com>
2024-02-06 13:21:36 +0800

    [fix]完善盆栽贴吧物价

--- src/plugins/jx3/penzai/__init__.py
+++ src/plugins/jx3/penzai/__init__.py
@@ -37,4 +37,41 @@ async def _(event: GroupMessageEvent, state: T_State, num: Message = Arg()):
         floors = state["floors"]
         floor = str(floors[int(num)-1])
         await dh_.finish(links[int(num)-1] + f"\n请前往{floor}楼哦~")
-    
+
+wg_ = on_command("jx3_wg", aliases={"贴吧物价"}, priority=5)
+
+
+@wg_.handle()
+async def _(event: GroupMessageEvent, state: T_State, args: Message = CommandArg()):
+    """
+    获取盆栽蹲号信息：
+
+    Example：-蹲号 蝶金
+    """
+    details = args.extract_plain_text()
+    if details == "":
+        await wg_.finish("您没有输入条件哦，请检查后重试~\n条件以空格分割哦~")
+    details = details.split(" ")
+    if len(details) < 1:
+        await wg_.finish("您没有输入条件哦，请检查后重试~\n条件以空格分割哦~")
+    final_details = ",".join(details)
+    data = await get_wg(final_details)
+    if type(data) == type([]):
+        image = data[0]
+        state["links"] = data[1]
+        state["floors"] = data[2]
+        await wg_.send(ms.image(image))
+        return
+    else:
+        await wg_.finish(data)
+
+@wg_.got("num", prompt="回复标题前方的序号，音卡就可以给你链接啦！")
+async def _(event: GroupMessageEvent, state: T_State, num: Message = Arg()):
+    num = num.extract_plain_text()
+    if not checknumber(num):
+        await wg_.finish(PROMPT_NumberInvalid)
+    else:
+        links = state["links"]
+        floors = state["floors"]
+        floor = str(floors[int(num)-1])
+        await wg_.finish(links[int(num)-1] + f"\n请前往{floor}楼哦~")
