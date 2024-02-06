5a56f5ac1c49418d060142f44e7c8c74b20999e3
HornCopper <zgu3316@gmail.com>
2024-02-06 12:22:28 +0800

    [upd][rew]重写-蹲号

---
+++ src/plugins/jx3/penzai/dh.py
@@ -0,0 +1,53 @@
+from src.tools.dep import *
+
+
+from src.tools.config import Config
+from src.tools.utils import nodetemp, get_api
+template_dh = """
+<tr>
+    <td class="short-column">$num</td>
+    <td class="short-column">$context</td>
+    <td class="short-column">$time</td>
+</tr>
+"""
+
+async def get_dh(type_: str):
+    # 数据来源 @盆栽蹲号
+    url = f"https://www.j3dh.com/v1/h/data/hero?ifKnownDaishou=false&exterior={type_}&school=0&figure=0&page=0"
+    data = await get_api(url)
+    if data["Code"] != 0:
+        return "唔……API访问失败！"
+    else:
+        if data["Result"]["Heros"] is None:
+            return "唔……没有获取到任何信息！"
+        table = []
+        links = []
+        num = 0
+        for i in range(len(data["Result"]["Heros"])):
+            x = data["Result"]["Heros"][i]
+            post = x["PostId"]
+            time_ = x["Time"]
+            timestamp = datetime.datetime.strptime(time_, "%Y-%m-%dT%H:%M:%SZ")
+            final_time = convert_time(int(timestamp.timestamp()), "%m月%d日 %H:%M:%S")
+            title = x["Details"]
+            thread = x["BigPostId"]
+            link = f"http://c.tieba.baidu.com/p/{thread}?pid={post}0&cid=0#{post}"
+            links.append(link)
+            table.append(template_dh.replace("$num", str(i + 1).replace("$context", title).replace("$time", final_time)))
+            num = num + 1
+            if num == 10:
+                break
+        if len(table) < 1:
+            return "唔……没有获取到任何信息！"
+        final_table = "\n".join(table)
+        html = read(bot_path.VIEWS + "/jx3/trade/dunhao.html")
+        font = bot_path.ASSETS + "/font/custom.ttf"
+        saohua = await get_api(f"https://www.jx3api.com/data/saohua/random?token={token}")
+        saohua = saohua["data"]["text"]
+        type_ = type_.replace(",", "+")
+        html = html.replace("$customfont", font).replace("$tablecontent", final_table).replace("$randomsaohua", saohua).replace("$appinfo", f"蹲号 · {type_}")
+        final_html = bot_path.CACHE + "/" + get_uuid() + ".html"
+        write(final_html, html)
+        final_path = await generate(final_html, False, ".total", False)
+        return [Path(final_path).as_uri(), links]
+
--- src/plugins/wiki/__init__.py
+++ src/plugins/wiki/__init__.py
@@ -23,26 +23,27 @@ async def _(event: GroupMessageEvent, state: T_State, args: Message = CommandArg
         state["wiki"] = info["api"]
         for i in range(len(results)):
             msg = msg + "\n" + str(i) + "." + results[i]
-        return await wiki.send(msg[1:])
+        await wiki.send(msg[1:])
+        return
     elif info["status"] == 201:
         url = info["link"]
-        return await wiki.finish(f"中间Wiki出现错误，请自行重定向至「{title}」：\n{url}")
+        await wiki.finish(f"中间Wiki出现错误，请自行重定向至「{title}」：\n{url}")
     elif info["status"] == 200:
         link = info["link"]
         desc = info["desc"]
         final_msg = f"查询到「{title}」：\n{link}{desc}"
-        return await wiki.finish(final_msg)
+        await wiki.finish(final_msg)
     elif info["status"] == 301:
         link = info["link"]
         redirect = info["redirect"]
         from_ = redirect[0]
         to_ = redirect[1]
         desc = info["desc"]
-        return await wiki.finish(f"重定向「{from_}」到「{to_}」：\n{link}{desc}")
+        await wiki.finish(f"重定向「{from_}」到「{to_}」：\n{link}{desc}")
     elif info["status"] == 404:
-        return await wiki.finish(f"未找到「{title}」。")
+        await wiki.finish(f"未找到「{title}」。")
     elif info["status"] == 502:
-        return await wiki.finish(info["reason"])
+        await wiki.finish(info["reason"])
 
 setwiki = on_command("setwiki", priority=5)
 
@@ -53,13 +54,13 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
         await setwiki.finish(error(5))
     api = await wiki_.get_api(args.extract_plain_text())
     if api["status"] == 500:
-        return await setwiki.finish("唔……此站点非有效的MediaWiki，请检查后重试~")
+        await setwiki.finish("唔……此站点非有效的MediaWiki，请检查后重试~")
     else:
         link = api["data"]
         now = json.loads(read(bot_path.DATA + "/" + str(event.group_id) + "/wiki.json"))
         now["startwiki"] = link
         write(bot_path.DATA + "/" + str(event.group_id) + "/wiki.json", json.dumps(now))
-        return await setwiki.finish("初始Wiki修改成功！")
+        await setwiki.finish("初始Wiki修改成功！")
 
 
 def check_interwiki_prefix(group, prefix):
@@ -83,53 +84,53 @@ async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
     args = args.extract_plain_text().split(" ")
     if args[0] == "add":
         if len(args) != 3:
-            return await interwiki.finish(PROMPT_ArgumentInvalid)
+            await interwiki.finish(PROMPT_ArgumentInvalid)
         prefix = args[1]
         if check_interwiki_prefix(str(event.group_id), prefix) is True:
-            return await interwiki.finish("唔……该前缀已被使用，请删除或更新此前缀~")
+            await interwiki.finish("唔……该前缀已被使用，请删除或更新此前缀~")
         link = args[2]
         api = await wiki_.get_api(link)
         if api["status"] == 500:
-            return await interwiki.finish("唔……此站点非有效的MediaWiki，请检查后重试~")
+            await interwiki.finish("唔……此站点非有效的MediaWiki，请检查后重试~")
         api = api["data"]
         new = {"prefix": prefix, "link": api}
         now = json.loads(read(bot_path.DATA + "/" + str(event.group_id) + "/wiki.json"))
         now["interwiki"].append(new)
         write(bot_path.DATA + "/" + str(event.group_id) + "/wiki.json", json.dumps(now))
         site_name = await wiki_.get_site_info(api)
-        return await interwiki.finish("成功添加Interwiki：\n" + site_name)
+        await interwiki.finish("成功添加Interwiki：\n" + site_name)
     elif args[0] == "del":
         if len(args) != 2:
-            return await interwiki.finish(PROMPT_ArgumentInvalid)
+            await interwiki.finish(PROMPT_ArgumentInvalid)
         prefix = args[1]
         if check_interwiki_prefix(str(event.group_id), prefix) is False:
-            return await interwiki.finish("唔……该前缀未被使用，请检查后重试~")
+            await interwiki.finish("唔……该前缀未被使用，请检查后重试~")
         now = json.loads(read(bot_path.DATA + "/" + str(event.group_id) + "/wiki.json"))
         for i in now["interwiki"]:
             if i["prefix"] == prefix:
                 now["interwiki"].remove(i)
         write(bot_path.DATA + "/" + str(event.group_id) + "/wiki.json", json.dumps(now))
-        return await interwiki.finish("Interwiki移除成功！")
+        await interwiki.finish("Interwiki移除成功！")
     elif args[0] == "upd":
         if len(args) != 3:
-            return await interwiki.finish(PROMPT_ArgumentInvalid)
+            await interwiki.finish(PROMPT_ArgumentInvalid)
         prefix = args[1]
         link = args[2]
         api = await wiki_.get_api(link)
         if api["status"] == 500:
-            return await interwiki.finish("唔……此站点非有效的MediaWiki，请检查后重试~")
+            await interwiki.finish("唔……此站点非有效的MediaWiki，请检查后重试~")
         api = api["data"]
         if check_interwiki_prefix(str(event.group_id), prefix) is False:
-            return await interwiki.finish("唔……该前缀未被使用，请检查后重试~")
+            await interwiki.finish("唔……该前缀未被使用，请检查后重试~")
         now = json.loads(read(bot_path.DATA + "/"+str(event.group_id)+"/wiki.json"))
         for i in now["interwiki"]:
             if i["prefix"] == prefix:
                 i["link"] = api
         write(bot_path.DATA + "/" + str(event.group_id) + "/wiki.json", json.dumps(now))
         site_name = await wiki_.get_site_info(api)
-        return await interwiki.finish("成功更新Interwiki：\n" + site_name)
+        await interwiki.finish("成功更新Interwiki：\n" + site_name)
 
-    return await interwiki.finish(PROMPT_ArgumentInvalid)
+    await interwiki.finish(PROMPT_ArgumentInvalid)
 
 
 def get_local_api(group, prefix):
@@ -147,7 +148,7 @@ iwiki = on_command("iwiki", priority=5)
 async def _(state: T_State, event: GroupMessageEvent, args: Message = CommandArg()):
     search = args.extract_plain_text().split(":")
     if len(search) <= 1:
-        return await iwiki.finish("唔……没有Interwiki前缀，请检查后重试~")
+        await iwiki.finish("唔……没有Interwiki前缀，请检查后重试~")
     prefix = search[0]
     api = get_local_api(str(event.group_id), prefix)
     if len(search) == 2:
@@ -156,7 +157,7 @@ async def _(state: T_State, event: GroupMessageEvent, args: Message = CommandArg
         search.remove(search[0])
         title = ":".join(search)
     if api is False:
-        return await iwiki.finish("唔……该前缀不存在哦，请检查后重试~")
+        await iwiki.finish("唔……该前缀不存在哦，请检查后重试~")
 
     info = await wiki_.simple(api, title)
     if info["status"] == 202:
@@ -166,29 +167,29 @@ async def _(state: T_State, event: GroupMessageEvent, args: Message = CommandArg
         state["wiki"] = info["api"]
         for i in range(len(results)):
             msg = msg + "\n" + str(i) + "." + results[i]
-        return await wiki.send(msg[1:])
+        await wiki.send(msg[1:])
     elif info["status"] == 201:
         url = info["link"]
         final_title = f"{prefix}:{title}"
-        return await wiki.finish(f"中间Wiki出现错误，请自行重定向至「{final_title}」：\n{url}")
+        await wiki.finish(f"中间Wiki出现错误，请自行重定向至「{final_title}」：\n{url}")
     elif info["status"] == 200:
         link = info["link"]
         desc = info["desc"]
         full_title = prefix+":"+title
         final_msg = f"查询到「{full_title}」：\n{link}{desc}"
-        return await wiki.finish(final_msg)
+        await wiki.finish(final_msg)
     elif info["status"] == 301:
         link = info["link"]
         redirect = info["redirect"]
         from_ = redirect[0]
         to_ = redirect[1]
         desc = info["desc"]
-        return await wiki.finish(f"重定向「{from_}」到「{to_}」：\n{link}{desc}")
+        await wiki.finish(f"重定向「{from_}」到「{to_}」：\n{link}{desc}")
     elif info["status"] == 404:
         final_title = f"{prefix}:{title}"
-        return await wiki.finish(f"未找到「{final_title}」。")
+        await wiki.finish(f"未找到「{final_title}」。")
     elif info["status"] == 502:
-        return await wiki.finish(info["reason"])
+        await wiki.finish(info["reason"])
 
 
 @iwiki.got("num", prompt="发送序号以搜索，发送其他内容则取消搜索。")
@@ -196,15 +197,15 @@ async def _(state: T_State, event: GroupMessageEvent, args: Message = CommandArg
 async def __(state: T_State, num: Message = Arg()):
     num = num.extract_plain_text()
     if not checknumber(num):
-        return await wiki.finish(PROMPT_NumberInvalid)
+        await wiki.finish(PROMPT_NumberInvalid)
     api = state["wiki"]
     results = state["results"]
     if int(num) not in range(len(results)):
-        return await wiki.finish(PROMPT_NumberNotExist)
+        await wiki.finish(PROMPT_NumberNotExist)
     title = results[int(num)]
     info = await wiki_.simple(api, title)
     link = info["link"]
     desc = info["desc"]
     msg = f"查询到「{title}」：\n{link}{desc}"
-    return await wiki.finish(msg)
+    await wiki.finish(msg)
 
