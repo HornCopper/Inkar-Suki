6b128bf7364f8da429ab0c8f19d62429e8136f94
HornCopper <zgu3316@gmail.com>
2024-02-08 19:34:38 +0800

    [upd]楚天社

---
+++ src/plugins/jx3/events/chutian.py
@@ -0,0 +1,101 @@
+from src.tools.dep import *
+
+import datetime
+
+template_chutian = """
+<tr>
+    <td class="short-column">$time</td>
+    <td class="short-column">$map·$place</span></td>
+    <td class="short-column"><img src="$icon">$section</td>
+    <td class="short-column">$desc</td>
+</tr>"""
+
+async def processData():
+    api = "https://cms.jx3box.com/api/cms/game/celebrity?type=0"
+    data = await get_api(api)
+    processed = {}
+    for i in data["data"]:
+        processed[i["key"]]:list
+        processed[i["key"]].append(i)
+    return processed
+
+async def getChutianImg():
+    processData = await processData()
+    now_time = datetime.datetime.now()
+    hour = int(now_time.strftime("%H"))
+    minute = int(now_time.strftime("%M"))
+    if hour % 2 == 1 and minute >= 30:
+        t = "c11" # 单数半点
+    elif hour % 2 == 1 and 30 > minute >= 0:
+        t = "c10" # 单数整点
+    elif hour % 2 == 0 and minute >= 30:
+        t = "c01" # 双数半点
+    elif hour % 2 == 0 and 30 > minute >= 0:
+        t = "c00" # 双数整点
+    # 时间轴：双数整点 -> 双数半点 -> 单数整点 -> 单数半点
+    # 代码轴：c00 -> c01 -> c10 -> c11
+    typeList = ["c00", "c01", "c10", "c11"]
+    for i in range(len(processData[t])):
+        x = processData[t][i]
+        if x["key"] == t and x["time"] <= minute:
+            final_data = []
+            try:
+                final_data.append(processData[t][i-1])
+            except IndexError:
+                previous = typeList.index(t) - 1
+                if previous == -1:
+                    previous = 3
+                final_data.append(processData[typeList[next]][:1])
+            final_data.append(x)
+            for num in range(3)[1:]:
+                try:
+                    final_data.append(processData[t][i+num])
+                except IndexError:
+                    next = typeList.index(t) + 1
+                    if next == 4:
+                        next = 0
+                    final_data.append(processData[typeList[next]][num])
+    table = []
+    for i in final_data:
+        icon = "https://img.jx3box.com/pve/minimap/minimap_" + i["icon"] + ".png"
+        hour = processHour(t, i["key"], int(hour))
+        minute = str(i["minute"])
+        at_map = i["map"]
+        site = i["site"]
+        section = i["stage"]
+        desc = i["desc"]
+        table.append(template_chutian.replace("$time", hour + ":" + minute).replace("$map", at_map).replace("$place", site).replace("$icon", icon).replace("$section", section).replace("$desc", desc))
+    final_table = "\n".join(table)
+    html = read(bot_path.VIEWS + "/jx3/celebrations/chutian.html")
+    font = bot_path.ASSETS + "/font/custom.ttf"
+    saohua = await get_api(f"https://www.jx3api.com/data/saohua/random?token={token}")
+    saohua = saohua["data"]["text"]
+    current_time = datetime.datetime.now().strftime("%H:%M")
+    html = html.replace("$customfont", font).replace("$tablecontent", final_table).replace("$randomsaohua", saohua).replace("$appinfo", f"楚天社 · {current_time}")
+    final_html = bot_path.CACHE + "/" + get_uuid() + ".html"
+    write(final_html, html)
+    final_path = await generate(final_html, False, "table", False)
+    return Path(final_path).as_uri()
+
+def processHour(current: str, goal: str, hour: int):
+    if current == goal:
+        return hour
+    # c00双整 c01双半 c10单整 c11单半
+    elif current == "c00" and goal == "c01":
+        return hour
+    elif current == "c01" and goal == "c10":
+        return hour + 1
+    elif current == "c10" and goal == "c11":
+        return hour
+    elif current == "c11" and goal == "c00":
+        return hour + 1
+    elif current == "c11" and goal == "c10":
+        return hour
+    elif current == "c10" and goal == "c01":
+        return hour - 1
+    elif current == "c01" and goal == "c00":
+        return hour
+    elif current == "c00" and goal == "c11":
+        return hour - 1
+    else:
+        raise ValueError("The current key and the goal key must be adjacent!")
---
+++ src/plugins/jx3/events/yuncong.py
@@ -0,0 +1,6 @@
+from src.tools.dep import *
+
+async def getChutianImg():
+    # api = "https://cms.jx3box.com/api/cms/game/celebrity?type=1" # 0为楚天社 1为云从社
+    # data = await get_api(api)
+    ... # 施工中
