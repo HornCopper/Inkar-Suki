3c5ff0303f231fee2ff0c96ea32b7cc1a1d6358e
HornCopper <zgu3316@gmail.com>
2024-02-16 19:31:01 +0800

    [upd]无聊的BMI计算

---
+++ src/plugins/bmi/__init__.py
@@ -0,0 +1,25 @@
+from src.tools.dep import *
+
+bmi = on_command("bmi", aliases={"BMI", "身体质量指数"}, priority=5)
+
+@bmi.handle()
+async def _(event: Event, args: Message = CommandArg()):
+    arg = args.extract_plain_text().split(" ")
+    if len(arg) != 2:
+        await bmi.finish("唔……参数数量不正确哦，请参考以下格式，注意两个参数都是纯数字哦~\nBMI 身高(米) 体重(千克)")
+    for i in arg:
+        if not checknumber(i):
+            await bmi.finish("唔……请参考以下格式，注意两个参数都是纯数字哦~\nBMI 身高(米) 体重(千克)")
+    height = int(arg[0])
+    weight = int(arg[1])
+    bmi = height / (weight*weight)
+    final_result = round(bmi, 1)
+    if final_result <= 18.4:
+        msg = f"您的BMI计算结果是：{final_result}，属于偏瘦（0~18.4）哦~"
+    elif 18.5 <= final_result <= 23.9:
+        msg = f"您的BMI计算结果是：{final_result}，属于正常（18.5~23.9）哦~"
+    elif 24.0 <= final_result <= 27.9:
+        msg = f"您的BMI计算结果是：{final_result}，属于偏胖（24.0~27.9）哦~"
+    elif final_result >= 28.0:
+        msg = f"您的BMI计算结果是：{final_result}，属于肥胖（28.0+）哦~\n音卡建议您少吃高热量食物，多多运动保持健康身体哦！"
+    
