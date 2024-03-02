30a6737803d46f4e57a246b0326dc5215b8fe832
HornCopper <zgu3316@gmail.com>
2024-03-02 22:19:08 +0800

    [upd]来点彩色日志

--- src/tools/generate.py
+++ src/tools/generate.py
@@ -23,7 +23,7 @@ def get_uuid():
 async def generate_by_url(url: str, locate: str = None, first_element: bool = False, delay: int = 0):
     try:
         async with async_playwright() as p:
-            logger.info("Generating source: " + url)
+            logger.opt(colors=True).info("<gold>Generating source: " + url + "</gold>")
             browser = await p.chromium.launch(headless=True, slow_mo=0)
             context = await browser.new_context()
             page = await context.new_page()
@@ -39,7 +39,7 @@ async def generate_by_url(url: str, locate: str = None, first_element: bool = Fa
                     await page.locator(locate).screenshot(path=store_path)
             else:
                 await page.screenshot(path=store_path)
-            logger.info("Generated successfully: " + store_path)
+            logger.opt(colors=True).info("<green>Generated successfully: " + store_path + "</green>")
             return store_path
     except Exception as ex:
         logger.info(f"音卡的图片生成失败啦！请尝试执行`playwright install`！:{ex}")
