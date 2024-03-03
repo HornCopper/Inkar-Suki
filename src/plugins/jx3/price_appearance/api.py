868ee8d254d2e30ebef9487494a067d1f8483d68
HornCopper <zgu3316@gmail.com>
2024-03-04 07:59:39 +0800

    [fix]取消物价代理

--- src/plugins/jx3/price_appearance/api.py
+++ src/plugins/jx3/price_appearance/api.py
@@ -5,7 +5,7 @@ async def item_(name: str = None):  # 物价 <物品>
     if token is None:
         return [PROMPT_NoToken]
     final_url = f"{Config.jx3api_link}/view/trade/record?token={token}&robot={bot}&name={name}&scale=1"
-    data = await get_api(final_url, proxy=proxies)
+    data = await get_api(final_url)
     if data["code"] == 400:
         return ["唔……尚未收录该物品。"]
     return data["data"]["url"]
