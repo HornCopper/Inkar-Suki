2581a374108412fb29ae15e988a79bf0cc103f4c
HornCopper <zgu3316@gmail.com>
2024-03-04 15:22:05 +0800

    [upd]无订阅时的关于回复

--- src/plugins/jx3/subscribe/about.py
+++ src/plugins/jx3/subscribe/about.py
@@ -19,27 +19,24 @@ template_subscribe = """
 
 async def generateGroupInfo(bot: Bot, group_id: str):
     current = getGroupData(group_id, "subscribe")
-    if len(current) <= 0:
-        return ["本群聊没有开启任何内容哦~"]
-    else:
-        contents = []
-        for i in list(subscribe_options):
-            desc = subscribe_options[i][0]
-            icon = subscribe_options[i][1]
-            if i in current:
-                status = "enabled"
-                flag = "✔"
-            else:
-                status = "disabled"
-                flag = "✖"
-            contents.append(template_subscribe.replace("$image", icon).replace("$subject", i).replace("$description", desc).replace("$status", status).replace("$flag", flag))
-        final_contents = "\n".join(contents)
-        html = read(VIEWS + "/jx3/subscribe/subscribe.html")
-        font = ASSETS + "/font/custom.ttf"
-        group_info = await bot.call_api("get_group_info", group_id=int(group_id), no_cache=True)
-        group_name = group_info["group_name"]
-        html = html.replace("$customfont", font).replace("$contents", final_contents).replace("$group_id", group_id).replace("$group_name", group_name)
-        final_html = CACHE + "/" + get_uuid() + ".html"
-        write(final_html, html)
-        final_path = await generate(final_html, False, ".total", False)
-        return Path(final_path).as_uri()
+    contents = []
+    for i in list(subscribe_options):
+        desc = subscribe_options[i][0]
+        icon = subscribe_options[i][1]
+        if i in current:
+            status = "enabled"
+            flag = "✔"
+        else:
+            status = "disabled"
+            flag = "✖"
+        contents.append(template_subscribe.replace("$image", icon).replace("$subject", i).replace("$description", desc).replace("$status", status).replace("$flag", flag))
+    final_contents = "\n".join(contents)
+    html = read(VIEWS + "/jx3/subscribe/subscribe.html")
+    font = ASSETS + "/font/custom.ttf"
+    group_info = await bot.call_api("get_group_info", group_id=int(group_id), no_cache=True)
+    group_name = group_info["group_name"]
+    html = html.replace("$customfont", font).replace("$contents", final_contents).replace("$group_id", group_id).replace("$group_name", group_name)
+    final_html = CACHE + "/" + get_uuid() + ".html"
+    write(final_html, html)
+    final_path = await generate(final_html, False, ".total", False)
+    return Path(final_path).as_uri()
