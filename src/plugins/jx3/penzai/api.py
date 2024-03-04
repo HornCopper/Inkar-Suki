66123a94d65493aede0be7ab570380a462f72436
HornCopper <zgu3316@gmail.com>
2024-03-04 09:25:18 +0800

    [upd]贴吧物价显示来源

--- src/plugins/jx3/penzai/api.py
+++ src/plugins/jx3/penzai/api.py
@@ -1,2 +1,8 @@
 from .dh import *
 from .wg import *
+
+from urllib.parse import unquote
+
+async def get_from(url: str):
+    data = await get_url(url)
+    return unquote(re.findall(r"<meta furl=\".+fname", data)[0][33:-16])
