01789660335183dc670dfdcf6fe7e68c6a949602
HornCopper <zgu3316@gmail.com>
2024-03-04 15:52:04 +0800

    [upd]来点mc元素;[fix]骚话谁教你这么写的

--- src/plugins/jx3/joy/api.py
+++ src/plugins/jx3/joy/api.py
@@ -3,7 +3,7 @@ from typing import Literal, Tuple
 from src.tools.basic import *
 
 
-async def saohua_primary(subject: Literal["random", "content"]) -> Tuple[str, str]:
+async def saohua_primary(subject: Literal["random", "content"]):
     full_link = f"{Config.jx3api_link}/data/saohua/{subject}?token={token}"
     info = await get_api(full_link)
     data = info and info.get("data")
@@ -11,11 +11,3 @@ async def saohua_primary(subject: Literal["random", "content"]) -> Tuple[str, st
         return None, None
     text, id = data.get("text"), data.get("id")
     return text, id
-
-
-async def saohua_random():
-    await saohua_primary("random")
-
-
-async def saohua_tiangou():
-    await saohua_primary("content")
