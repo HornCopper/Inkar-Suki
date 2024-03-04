987a3b6ec5d0c7627c1f89a23b4319447efbc588
HornCopper <zgu3316@gmail.com>
2024-03-04 09:27:44 +0800

    [fix]导入错误

--- src/plugins/jx3/penzai/api.py
+++ src/plugins/jx3/penzai/api.py
@@ -1,8 +1,2 @@
 from .dh import *
 from .wg import *
-
-from urllib.parse import unquote
-
-async def get_from(url: str):
-    data = await get_url(url)
-    return unquote(re.findall(r"<meta furl=\".+fname", data)[0][33:-16])
