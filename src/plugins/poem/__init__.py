226d60dfc62b6c7d521f56e9b71a5d9a416c2a56
HornCopper <zgu3316@gmail.com>
2024-03-09 15:07:00 +0800

    [fix]对诗错误提示。

--- src/plugins/poem/__init__.py
+++ src/plugins/poem/__init__.py
@@ -16,6 +16,7 @@ async def _(state: T_State, event: Event):
     if guess[-1] in ["？", "！", "。"]:
         guess = guess[0:-1]
     blank = content.replace(guess, "__________")
+    state["raw"] = content
     state["author"] = author
     state["title"] = title
     state["answer"] = guess
@@ -31,4 +32,10 @@ async def __(event: Event, state: T_State, answer: Message = Arg()):
         Sign.add(str(event.user_id), 50)
         author = state["author"]
         title = state["title"]
-        await rdp.finish(f"答对啦！\n这首诗是 {author} 的《{title}》。\n您获得了50枚金币。")
+        await rdp.finish(f"答对啦！\n这句诗来自 {author} 的《{title}》。\n您获得了50枚金币。")
+    else:
+        Sign.reduce(str(event.user_id), 70)
+        right = state["raw"]
+        author = state["author"]
+        title = state["title"]
+        await rdp.finish(f"唔……答错了！\n这句诗来自 {author} 的《{title}》。\n很可惜，您失去了70枚金币。\n正确答案为：{right}")
