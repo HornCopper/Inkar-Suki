24589e0441a2d2c12ded0c1564b8e352a2ad5f76
HornCopper <zgu3316@gmail.com>
2024-03-09 18:02:32 +0800

    [fix]文字无需MessageSegment

--- src/plugins/jx3/daily/api.py
+++ src/plugins/jx3/daily/api.py
@@ -7,7 +7,7 @@ async def daily_(server: str = None, group_id: str = None, predict_day_num: int 
     获取日常图片链接
     @param predict_day_num 向后预测天数
     """
-    server = server_mapping(server, group_id)
+    server = server_mapping(server, str(group_id))
     if not server:
         return [PROMPT_ServerNotExist]
     full_link = f"{Config.jx3api_link}/data/active/current?server={server}&num={predict_day_num}"
