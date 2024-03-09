78d009d6a54dcd11e60bff532fa778ebadf4ad32
HornCopper <zgu3316@gmail.com>
2024-03-09 13:35:01 +0800

    [fix]`args`类型判断错误

--- src/plugins/jx3/events/yuncong.py
+++ src/plugins/jx3/events/yuncong.py
@@ -1,6 +1,13 @@
 from src.tools.basic import *
 
+def parity(num: int):
+    if num % 2 == 0:
+        return True
+    return False
+
 async def getYuncongImg():
-    # api = "https://cms.jx3box.com/api/cms/game/celebrity?type=1" # 0为楚天社 1为云从社
-    # data = await get_api(api)
-    ... # 施工中
+    api = "https://cms.jx3box.com/api/cms/game/celebrity?type=1" # 0为楚天社 1为云从社
+    data = await get_api(api)
+    chour = convert_time(getCurrentTime(), "%H")
+    currentFlag = "y0" if parity(int(chour)) else "y1"
+    
