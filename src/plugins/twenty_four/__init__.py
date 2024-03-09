5f2f6b83bb66a688087a5555f13f7173910642e6
HornCopper <zgu3316@gmail.com>
2024-03-09 17:22:40 +0800

    [fix]优化回答

--- src/plugins/twenty_four/__init__.py
+++ src/plugins/twenty_four/__init__.py
@@ -10,7 +10,7 @@ tf = on_command("twentyFour", aliases={"24点"}, priority=5)
 async def _(state: T_State, event: Event):
     numbers = [random.randint(1, 13) for _ in range(4)]
     state["numbers"] = numbers
-    await tf.send(f"给出的数字组合：{numbers}\n请输入表达式使其结果为24。（若无解，请输入“无解”）")
+    await tf.send(f"音卡翻出了这组数字：{numbers}\n请告诉我一个表达式，使其结果为24。（若无解，可以告诉我“无解”）")
     return
 
 @tf.receive("answer")
@@ -21,19 +21,19 @@ async def __(state: T_State, event: Event = Received("answer")):
     if expr == "无解":
         if solution:
             Sign.reduce(str(event.user_id), 50)
-            await tf.finish(f"回答错误：该组合存在解。\n其中一组解为：{solution}\n您失去了50枚金币。")
+            await tf.finish(f"唔……回答错误，并非无解哦。\n其中一组解为：{solution}\n您失去了50枚金币。")
         else:
             Sign.add(str(event.user_id), 50)
-            await tf.finish("回答正确。\n您获得了50枚金币。")
+            await tf.finish("回答正确！\n您获得了50枚金币。")
     elif check_valid(expr):
         result = calc(expr)
         if not result:
-            await tf.finish("回答错误：表达式无效。")
+            await tf.finish("唔……回答错误，您的表达式真的能算吗？")
         elif (result == 24 or 0 < 24 - result < 1e-13 ) \
             and contains_all_numbers(expr, numbers):
             Sign.add(str(event.user_id), 50)
-            await tf.finish("回答正确。\n您获得了50枚金币。")
+            await tf.finish("回答正确！\n您获得了50枚金币。")
         else:
-            await tf.finish("回答错误。")
+            await tf.finish("唔……回答错误。")
     else:
-        await tf.finish("回答错误：表达式无效。")
+        await tf.finish("唔……回答错误，您的表达式真的能算吗？")
