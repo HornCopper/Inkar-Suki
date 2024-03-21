10767a1ee77c85d1da22e84c4bdbe71549c0064a
HornCopper <zgu3316@gmail.com>
2024-03-22 00:10:52 +0800

    [del]删除违禁词检测

--- src/plugins/developer_tools/failed.py
+++ src/plugins/developer_tools/failed.py
@@ -1,13 +1,1 @@
-from nonebot.adapters import Bot
-from nonebot.exception import MockApiException
-
-from src.tools.basic import *
-
-@Bot.on_calling_api
-async def handle_api_call(bot: Bot, api: str, data: dict):
-    if api == "send_group_msg":
-        to_check = data["message"]
-        logger.info("Checking message……")
-        data = await get_api("https://inkar-suki.codethink.cn/banword?word=" + to_check)
-        if data["code"] == 200:
-            raise MockApiException("The content is not lawful!")
+...
