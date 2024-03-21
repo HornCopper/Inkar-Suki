00fcfcebf3b8680d278284e985c227bdd2760352
HornCopper <zgu3316@gmail.com>
2024-03-21 23:50:14 +0800

    [upd]log

--- src/plugins/developer_tools/failed.py
+++ src/plugins/developer_tools/failed.py
@@ -7,6 +7,7 @@ from src.tools.basic import *
 async def handle_api_call(bot: Bot, api: str, data: dict):
     if api == "send_group_msg":
         to_check = data["message"]
+        logger.info("Checking message……")
         data = await get_api("https://inkar-suki.codethink.cn/banword?word=" + to_check)
         if data["code"] == 200:
             raise MockApiException("The content is not lawful!")
