01a454cf635eb39e63e0ed0c31cd2bf2045c7bae
HornCopper <zgu3316@gmail.com>
2024-03-21 23:39:18 +0800

    [fix]奇怪的报错增加了

--- src/plugins/developer_tools/failed.py
+++ src/plugins/developer_tools/failed.py
@@ -1,13 +1,6 @@
-from nonebot.message import run_postprocessor
 from nonebot.adapters import Bot
 
 from src.tools.basic import *
-
-@run_postprocessor
-async def _(bot: Bot, event: Event, matcher: Matcher, exception: Optional[Exception]):
-    if exception:
-        for i in Config.owner:
-            await bot.send_private_msg(user_id=int(i), message=f"音卡出现了报错：\n{exception}")
 
 @Bot.on_calling_api
 async def _(event: Event, matcher: Matcher):
