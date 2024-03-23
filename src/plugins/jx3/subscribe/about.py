4432afaee8f40cd6a9b26b26b2b0aaba56dbb7f1
HornCopper <zgu3316@gmail.com>
2024-03-23 21:52:21 +0800

    [fix]过滤前判定订阅;[upd]附加功能订阅;[upd]更新关于界面

--- src/plugins/jx3/subscribe/about.py
+++ src/plugins/jx3/subscribe/about.py
@@ -17,24 +17,38 @@ template_subscribe = """
 
 async def generateGroupInfo(bot: Bot, group_id: str):
     subscribe_options = json.loads(read(PLUGINS + "/jx3/subscribe/options.json"))
-    current = getGroupData(group_id, "subscribe")
-    contents = []
+    addtions_options = json.loads(read(PLUGINS + "/jx3/subscribe/addtions.json"))
+    current_subscribe = getGroupData(group_id, "subscribe")
+    current_addtions = getGroupData(group_id, "addtions")
+    subscribe_contents = []
+    addtions_contents = []
     for i in list(subscribe_options):
         desc = subscribe_options[i][0]
         icon = subscribe_options[i][1]
-        if i in current:
+        if i in current_subscribe:
             status = "enabled"
             flag = "✔"
         else:
             status = "disabled"
             flag = "✖"
-        contents.append(template_subscribe.replace("$image", icon).replace("$subject", i).replace("$description", desc).replace("$status", status).replace("$flag", flag))
-    final_contents = "\n".join(contents)
+        subscribe_contents.append(template_subscribe.replace("$image", icon).replace("$subject", i).replace("$description", desc).replace("$status", status).replace("$flag", flag))
+    for i in list(addtions_options):
+        desc = addtions_options[i][0]
+        icon = addtions_options[i][1]
+        if i in current_addtions:
+            status = "enabled"
+            flag = "✔"
+        else:
+            status = "disabled"
+            flag = "✖"
+        addtions_contents.append(template_subscribe.replace("$image", icon).replace("$subject", i).replace("$description", desc).replace("$status", status).replace("$flag", flag))
+    final_subscribe_contents = "\n".join(subscribe_contents)
+    final_options_contents = "\n".join(subscribe_options)
     html = read(VIEWS + "/jx3/subscribe/subscribe.html")
     font = ASSETS + "/font/custom.ttf"
     group_info = await bot.call_api("get_group_info", group_id=int(group_id), no_cache=True)
     group_name = group_info["group_name"]
-    html = html.replace("$customfont", font).replace("$contents", final_contents).replace("$group_id", group_id).replace("$group_name", group_name)
+    html = html.replace("$customfont", font).replace("$subscribe_contents", final_subscribe_contents).replace("$option_contents", final_options_contents).replace("$group_id", group_id).replace("$group_name", group_name)
     final_html = CACHE + "/" + get_uuid() + ".html"
     write(final_html, html)
     final_path = await generate(final_html, False, ".total", False)
