4c8e1e7f5b002bbe2117c3c917ae929eeab270ef
HornCopper <zgu3316@gmail.com>
2024-03-23 22:02:07 +0800

    [fix]遗留废码

--- src/tools/utils/global_path_cache.py
+++ src/tools/utils/global_path_cache.py
@@ -19,10 +19,3 @@ TOOLS = tools_path
 common_data: str = "common"
 common_data_full: str = f"{DATA}/{common_data}/"
 """DATA中的数据通用存储"""
-
-
-def get_group_config(group_id: str, config_name: str = "jx3group") -> str:
-    return f"{DATA}/{group_id}/{config_name}"
-
-def get_user_config(user_id: str, config_name: str = "jx3user") -> str:
-    return f"{DATA}/users/{user_id}/{config_name}"
