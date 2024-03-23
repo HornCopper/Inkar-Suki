75b5101d5eba838f1344fbbcd48c406f997ea111
HornCopper <zgu3316@gmail.com>
2024-03-23 19:50:27 +0800

    [fix]本地支持

--- src/tools/generate.py
+++ src/tools/generate.py
@@ -20,14 +20,17 @@ def get_uuid():
     return str(uuid.uuid1()).replace("-", "")
 
 
-async def generate_by_url(url: str, locate: str = None, first_element: bool = False, delay: int = 0, css: str = ""):
+async def generate_by_url(url: str, locate: str = None, first_element: bool = False, delay: int = 0, css: str = "", web: bool=False):
     try:
         async with async_playwright() as p:
             logger.opt(colors=True).info("<green>Generating source: " + url + "</green>")
             browser = await p.chromium.launch(headless=True, slow_mo=0)
             context = await browser.new_context()
             page = await context.new_page()
-            await page.goto(url)
+            if web:
+                await page.goto(url)
+            else:
+                await page.goto({"path": url})
             if delay > 0:
                 time.sleep(delay / 1000)
             uuid_ = get_uuid()
@@ -57,11 +60,8 @@ async def generate(path: str, web: bool = False, locate: str = None, first: bool
     @param delay: 等待时间，单位为毫秒（ms）
     @addtional_css: 追加的CSS
     """
-    if not web:
-        final_path = Path(path).as_uri()
-    else:
-        final_path = path
-    result = await generate_by_url(final_path, locate, first, delay, addtional_css)
+    final_path = path
+    result = await generate_by_url(final_path, locate, first, delay, addtional_css, web)
     if result is None:
         return False
     return result
