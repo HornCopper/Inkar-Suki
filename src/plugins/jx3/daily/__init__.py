33755bea72975f39b02caf620edf4d9733f17818
HornCopper <zgu3316@gmail.com>
2024-03-27 00:36:38 +0800

    [fix]优化日常推送

--- src/plugins/jx3/daily/__init__.py
+++ src/plugins/jx3/daily/__init__.py
@@ -21,12 +21,15 @@ async def jx3_daily(event: GroupMessageEvent, args: Message = CommandArg()):
     info = await daily_(args.extract_plain_text(), group_id = event.group_id)
     await jx3_cmd_daily.finish(info)
 
-@scheduler.scheduled_job("cron", hour="7", minute="40", id="daily")
-async def run_every_2_hour():
+@scheduler.scheduled_job("cron", hour="7", minute="40")
+async def run_every_morning():
     msg = await daily_("幽月轮", Config.notice_to[0])
-    bot = get_bot(int(Config.bot[0]))
-    groups = os.listdir(DATA)
-    for i in groups:
-        subscribe = getGroupData(i, "subscribe")
-        if "日常" in subscribe:
-            await bot.call_api("send_group_msg", group_id=int(i), message=msg)
+    for i in Config.bot:
+        bot = get_bot(i)
+        groups = os.listdir(DATA)
+        for i in groups:
+            subscribe = getGroupData(i, "subscribe")
+            server = getGroupData(i, "server")
+            msg = msg.replace("幽月轮", server)
+            if "日常" in subscribe:
+                await bot.call_api("send_group_msg", group_id=int(i), message=msg)
