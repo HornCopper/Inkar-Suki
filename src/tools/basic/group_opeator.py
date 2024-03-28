afca21d4975c949de55df8179fe12e434293a708
HornCopper <zgu3316@gmail.com>
2024-03-28 23:37:54 +0800

    [upd]改变数据结构“

--- src/tools/basic/group_opeator.py
+++ src/tools/basic/group_opeator.py
@@ -3,15 +3,15 @@ from ..basic import DATA, read, write
 import json
 
 def getGroupData(group: str, key: str):
-    data = json.loads(read(DATA + "/" + str(group) + "/jx3group.json"))
+    data = json.loads(read(DATA + "/" + str(group) + "/settings.json"))
     if data == False:
         return False
     return data[key]
 
 def setGroupData(group: str, key: str, new):
-    data = json.loads(read(DATA + "/" + str(group) + "/jx3group.json"))
+    data = json.loads(read(DATA + "/" + str(group) + "/settings.json"))
     if data == False:
         return False
     data[key] = new
-    write(DATA + "/" + str(group) + "/jx3group.json", json.dumps(data, ensure_ascii=False))
+    write(DATA + "/" + str(group) + "/settings.json", json.dumps(data, ensure_ascii=False))
     return True
