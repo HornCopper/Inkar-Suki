dfff756633fb4589524087afb38821e89df31247
HornCopper <zgu3316@gmail.com>
2024-04-05 19:52:31 +0800

    [upd]adapt to firework.

--- src/tools/basic/jx3/jx3apiws/events/FuyaoEvent.py
+++ src/tools/basic/jx3/jx3apiws/events/FuyaoEvent.py
@@ -2,6 +2,28 @@ from .api_lib.EventRister import *
 from src.tools.utils import *
 
 
+@EventRister.rister(action=1004)
+class FuyaoRefreshEvent(RecvEvent):
+
+    __event__ = "WsRecv.FuyaoRefresh"
+    message_type = "FuyaoRefresh"
+    server: str
+    """服务器"""
+    time: int
+    """时间"""
+
+
+
+    @property
+    def log(self) -> str:
+        log = f"扶摇刷新事件：[{self.server}]的扶摇已刷新 。"
+        return log
+
+    @overrides(RecvEvent)
+    def get_message(self) -> dict:
+        final_time = convert_time(self.time, format="%H:%M")
+        return {"type": "扶摇", "server": self.server, "msg": f"{self.server} 的扶摇九天在 {final_time} 开启啦，请前往少林演武场参加试炼！"}
+
 
 @EventRister.rister(action=1005)
 class FuyaoNamedEvent(RecvEvent):
@@ -28,3 +50,4 @@ class FuyaoNamedEvent(RecvEvent):
         name = "、".join(self.name)
         final_time = convert_time(self.time, format="%H:%M")
         return {"type": "扶摇", "msg": f"{self.server} 的唐文羽在 {final_time} 点名了 [{name}]，请速速前往少林！", "server": f"{self.server}"}
+
