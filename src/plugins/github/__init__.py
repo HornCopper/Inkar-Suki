4a48741a47a99ccf2af3b85225f49d9f51b156c5
HornCopper <zgu3316@gmail.com>
2024-04-20 23:05:22 +0800

    <github>[feat]缺失冒号;<utils>[refactor]!=False

--- src/plugins/github/__init__.py
+++ src/plugins/github/__init__.py
@@ -87,7 +87,7 @@ async def _(bot: Bot, event: GroupMessageEvent, args: Message = CommandArg()):
     personal_data = await bot.call_api("get_group_member_info", group_id=event.group_id, user_id=event.user_id, no_cache=True)
     group_admin = personal_data["role"] in ["owner", "admin"]
     if not group_admin:
-        if not checker(str(event.user_id), 9)
+        if not checker(str(event.user_id), 9):
             await unbind.finish(error(9))
     repo = args.extract_plain_text()
     group = str(event.group_id)
