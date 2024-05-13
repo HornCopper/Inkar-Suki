52184b888e2d6fca84541d6d211a6b246c1cbf28
HornCopper <zgu3316@gmail.com>
2024-05-14 04:38:41 +0800

    <rdimg>[feat]随机龙图

--- src/plugins/grab/rdimg.py
+++ src/plugins/grab/rdimg.py
@@ -19,3 +19,29 @@ async def _(args: Message = CommandArg()):
     data = await get_api("https://api.thedogapi.com/v1/images/search?size=full")
     image = await get_content(data[0]["url"])
     await rddi.finish(ms.image(image))
+
+rdli = on_command("随机龙图", priority=5)
+
+@rdli.handle()
+async def _(args: Message = CommandArg()):
+    if args.extract_plain_text() != "":
+        return
+    async with httpx.AsyncClient(follow_redirects=True) as client:
+        batch_choice = random.choice(["batch1/", "batch2/", "batch3/"])
+        base_url = "https://git.acwing.com/Est/dragon/-/raw/main/"
+        rdnum = random.randint(
+            (
+                int(batch_choice[-2])
+                -1
+            )*500+1,
+            (
+                int(batch_choice[-2])
+                *500
+            )
+                )
+        for ext in ['.jpg', '.png', '.gif']:
+            image_url = f"{base_url}{batch_choice}dragon_{rdnum}_{ext}"
+            resp = await client.get(image_url)
+            if resp.status_code == 200:
+                image = await resp.content
+                await rdli.finish(ms.image(image))
