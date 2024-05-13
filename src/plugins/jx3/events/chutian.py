7c453e348208e4182b0dea8ca15a354683e5730e
HornCopper <zgu3316@gmail.com>
2024-05-13 22:08:21 +0800

    <jx3>[feat]sync to docs

--- src/plugins/jx3/events/chutian.py
+++ src/plugins/jx3/events/chutian.py
@@ -9,7 +9,7 @@ template_chutian = """
 </tr>"""
 
 async def getChutianImg():
-    url = f"https://www.jx3api.com/data/active/celebrities?token={token}"
+    url = f"https://www.jx3api.com/data/active/celebrity"
     data = await get_api(url)
     tables = []
     for i in data["data"]:
