71cbb1b116997b8b54cb49197dce654b5d25bac1
HornCopper <zgu3316@gmail.com>
2024-05-13 21:34:01 +0800

    <jx3>[feat]sync to docs

--- src/plugins/jx3/daily/__init__.py
+++ src/plugins/jx3/daily/__init__.py
@@ -6,9 +6,9 @@ require("nonebot_plugin_apscheduler")
 
 from nonebot_plugin_apscheduler import scheduler
 
-jx3_cmd_daily = on_command("jx3_daily", aliases={"日常", "周常"}, force_whitespace=True, priority=5)
+jx3_cmd_daily = on_command("jx3_daily", aliases={"日常"}, force_whitespace=True, priority=5)
 @jx3_cmd_daily.handle()
-async def jx3_daily(event: GroupMessageEvent, args: Message = CommandArg()):
+async def jx3_daily(event: GroupMessageEvent):
     """
     查询日常。
 
@@ -16,7 +16,6 @@ async def jx3_daily(event: GroupMessageEvent, args: Message = CommandArg()):
 
     Example：-日常
     Example：-周常
-    Example：-日常 幽月轮
     """
-    info = await daily_(args.extract_plain_text(), group_id = event.group_id)
+    info = await daily_()
     await jx3_cmd_daily.finish(info)
