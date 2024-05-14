fad367a394f782f489e461f854338406c05dfd2b
HornCopper <zgu3316@gmail.com>
2024-05-14 13:45:25 +0800

    <jx3>[feat]avg重复转换

--- src/plugins/jx3/trade/wufeng.py
+++ src/plugins/jx3/trade/wufeng.py
@@ -156,7 +156,8 @@ async def getAllServerWufengImg(raw: str):
                 table.append(table_content)
                 continue
             else:
-                toReplace_word = [["$icon", icon], ["$color", color], ["$name", name + f"（{server}）<br><span style=\"color:rgb(0, 210, 75)\">" + " ".join(getAttrs(data["attributes"])) + "</span>"], ["$time", convert_time(getCurrentTime(), "%m月%d日 %H:%M:%S")], ["$limit", "N/A"], ["$price", toCoinImage(convert(avg))]]
+                avg = convert(current["AvgPrice"])
+                toReplace_word = [["$icon", icon], ["$color", color], ["$name", name + f"（{server}）<br><span style=\"color:rgb(0, 210, 75)\">" + " ".join(getAttrs(data["attributes"])) + "</span>"], ["$time", convert_time(getCurrentTime(), "%m月%d日 %H:%M:%S")], ["$limit", "N/A"], ["$price", toCoinImage(avg)]]
                 table_content = template_table
                 for word in toReplace_word:
                     table_content = table_content.replace(word[0], word[1])
