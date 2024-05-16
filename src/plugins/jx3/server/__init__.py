e6dd31f37b7653876171f8e4e1f6eb85693793b5
HornCopper <zgu3316@gmail.com>
2024-05-16 09:46:40 +0800

    <jx3>[feat]强制空格

--- src/plugins/jx3/server/__init__.py
+++ src/plugins/jx3/server/__init__.py
@@ -1,6 +1,6 @@
 from .api import *
 
-cmd_jx3_server = on_command("jx3_server", aliases = {"开服"}, priority = 5)
+cmd_jx3_server = on_command("jx3_server", aliases={"开服"}, priority=5, force_whitespace=True)
 
 @cmd_jx3_server.handle()
 async def jx3_server(event: GroupMessageEvent, args: Message = CommandArg()):
