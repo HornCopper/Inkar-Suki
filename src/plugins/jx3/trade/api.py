8584ef90808ca1372d9992862bfcc8f839943167
HornCopper <zgu3316@gmail.com>
2024-05-20 11:22:07 +0800

    <jx3>[feat]代码段位置错误

--- src/plugins/jx3/trade/api.py
+++ src/plugins/jx3/trade/api.py
@@ -232,6 +232,12 @@ async def getSingleImg(name: str):
                         table_content = table_content.replace(word[0], word[1])
                     table.append(table_content)
                     continue
+            each_price = detailData["data"]["prices"][0]
+            table_content = template_table
+            toReplace_word = [["$icon", itemList_searchable[0]["icon"]], ["$color", color], ["$name", itemList_searchable[0]["name"] + f"（{server}）"], ["$time", convert_time(each_price["created"], "%m月%d日 %H:%M:%S")], ["$limit", str(each_price["n_count"])], ["$price", toCoinImage(convert(each_price["unit_price"]))]]
+            for word in toReplace_word:
+                table_content = table_content.replace(word[0], word[1])
+            table.append(table_content)
         else:
             return ["唔……您给出的物品名称似乎不够精准，全服交易行价格查询最好给出准确名称哦！"]
         fhighs = [x for x in highs if x != 0]
@@ -244,12 +250,6 @@ async def getSingleImg(name: str):
         msgbox = template_msgbox.replace("当日", "全服")
         for toReplace_word in toReplace:
             msgbox = msgbox.replace(toReplace_word[0], toReplace_word[1])
-        each_price = detailData["data"]["prices"][0]
-        table_content = template_table
-        toReplace_word = [["$icon", itemList_searchable[0]["icon"]], ["$color", color], ["$name", itemList_searchable[0]["name"]], ["$time", convert_time(each_price["created"], "%m月%d日 %H:%M:%S")], ["$limit", str(each_price["n_count"])], ["$price", toCoinImage(convert(each_price["unit_price"]))]]
-        for word in toReplace_word:
-            table_content = table_content.replace(word[0], word[1])
-        table.append(table_content)
         final_table = "\n".join(table)
         html = read(VIEWS + "/jx3/trade/trade.html")
         font = ASSETS + "/font/custom.ttf"
