92466cb935fdf7824e68a41ff4813b301e1c77a5
HornCopper <zgu3316@gmail.com>
2024-05-20 11:42:11 +0800

    <jx3>[feat]to high

--- src/plugins/jx3/trade/__init__.py
+++ src/plugins/jx3/trade/__init__.py
@@ -20,8 +20,8 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     if len(id_cut) <= 1:
         id_cut = []
     if server == "全服":
-        if not checker(str(event.user_id), 10):
-            await trade.finish(error(10))
+        if not checker(str(event.user_id), 5):
+            await trade.finish(error(5))
     img = await getImg(server, id, str(event.group_id), id_cut)
     if type(img) == type([]):
         await trade.finish(img[0])
