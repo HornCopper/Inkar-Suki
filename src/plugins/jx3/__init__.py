84e0d22068124bf711e0417deecb0de2100c16a0
HornCopper <zgu3316@gmail.com>
2024-05-23 09:53:54 +0800

    <jx3>[feat]缺失提示

--- src/plugins/jx3/__init__.py
+++ src/plugins/jx3/__init__.py
@@ -2,6 +2,8 @@ from nonebot import get_driver
 
 from .jx3 import *
 
+import shutil
+
 driver = get_driver()
 
 
@@ -20,6 +22,8 @@ async def on_jx3_event_recv(bot: Bot, event: RecvEvent):
     message = event.get_message()
     if not message or message["type"] == "":
         return
+    if message["type"] == "开服" and datetime.date.today().weekday() == 0:
+        shutil.rmtree(ASSETS + "/jx3/monsters.jpg")
     logger.info(message["msg"])
     record_info(message["msg"])
     groups = os.listdir(DATA)
@@ -28,7 +32,6 @@ async def on_jx3_event_recv(bot: Bot, event: RecvEvent):
     for i in bot_groups:
         if str(i["group_id"]) in groups:
             available_group.append(int(i["group_id"]))
-    
     msg_type = message["type"]
     for i in available_group:
         if msg_type in getGroupData(str(i), "subscribe"):
