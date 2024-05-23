f0d910f6cf70dfbf3e8a324908856918cbb64894
HornCopper <zgu3316@gmail.com>
2024-05-24 07:49:07 +0800

    <jx3>[feat]造装备提示

--- src/plugins/jx3/trade/wufeng.py
+++ src/plugins/jx3/trade/wufeng.py
@@ -116,7 +116,8 @@ async def getAllServerWufengImg(raw: str):
     try:
         itemId = data["id"]
     except:
-        return [f"唔……未找到该属性的无封装备。"]
+        emg = await get_content("https://inkar-suki.codethink.cn/Inkar-Suki-Docs/img/emoji.jpg")
+        return ["音卡建议您不要造无封装备了，因为没有。\n" + ms.image(emg)]
     for server in servers:
         logs = await get_api(f"https://next2.jx3box.com/api/item-price/{itemId}/logs?server={server}")
         current = logs["data"]["today"]
