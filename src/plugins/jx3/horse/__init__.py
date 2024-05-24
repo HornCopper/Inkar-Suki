9ebc86dc658b18225f04a04a5e125c62d735e086
HornCopper <zgu3316@gmail.com>
2024-05-24 15:57:39 +0800

    <jx3>[feat]粗略优化抓马

--- src/plugins/jx3/horse/__init__.py
+++ src/plugins/jx3/horse/__init__.py
@@ -2,9 +2,16 @@ from .api import *
 
 jx3_cmd_horse = on_command("jx3_horse_v1", aliases={"抓马", "马场"}, force_whitespace=True, priority=5)
 
-
 @jx3_cmd_horse.handle()
 async def jx3_horse_v1(event: GroupMessageEvent, args: Message = CommandArg()):
     server = args.extract_plain_text()
     msg = await get_horse_reporter(server, group_id=event.group_id)
     await jx3_cmd_horse.finish(msg)
+
+jx3_cmd_horse_v2 = on_command("jx3_horse_v2", aliases={"抓马v2", "马场v2"}, force_whitespace=True, priority=5)
+
+@jx3_cmd_horse_v2.handle()
+async def jx3_horse_v2(event: GroupMessageEvent, args: Message = CommandArg()):
+    server = args.extract_plain_text()
+    msg = await get_horse_next_spawn(server)
+    await jx3_cmd_horse_v2.finish(msg)
