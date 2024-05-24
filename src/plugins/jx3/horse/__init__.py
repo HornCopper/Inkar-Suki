6dd3c846706d54138fb10be45a0d47465730575b
HornCopper <zgu3316@gmail.com>
2024-05-24 16:56:50 +0800

    <jx3>[feat]抓马默认切换至v1

--- src/plugins/jx3/horse/__init__.py
+++ src/plugins/jx3/horse/__init__.py
@@ -1,6 +1,6 @@
 from .api import *
 
-jx3_cmd_horse = on_command("jx3_horse_v1", aliases={"抓马", "马场"}, force_whitespace=True, priority=5)
+jx3_cmd_horse = on_command("jx3_horse_v1", aliases={"抓马v1", "马场v1"}, force_whitespace=True, priority=5)
 
 @jx3_cmd_horse.handle()
 async def jx3_horse_v1(event: GroupMessageEvent, args: Message = CommandArg()):
@@ -8,7 +8,7 @@ async def jx3_horse_v1(event: GroupMessageEvent, args: Message = CommandArg()):
     msg = await get_horse_reporter(server, group_id=event.group_id)
     await jx3_cmd_horse.finish(msg)
 
-jx3_cmd_horse_v2 = on_command("jx3_horse_v2", aliases={"抓马v2", "马场v2"}, force_whitespace=True, priority=5)
+jx3_cmd_horse_v2 = on_command("jx3_horse_v2", aliases={"抓马", "马场"}, force_whitespace=True, priority=5)
 
 @jx3_cmd_horse_v2.handle()
 async def jx3_horse_v2(event: GroupMessageEvent, args: Message = CommandArg()):
