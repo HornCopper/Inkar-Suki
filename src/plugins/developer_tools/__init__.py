724ccefc33a889230e3ee4ed0b26389f4a61b032
HornCopper <zgu3316@gmail.com>
2024-05-25 13:19:19 +0800

    <jx3>[feat]共存而非替换

--- src/plugins/developer_tools/__init__.py
+++ src/plugins/developer_tools/__init__.py
@@ -1,6 +1,7 @@
 try:
     from .application_v2 import * # 只用于公共实例，其他实例请勿使用该代码
 except:
-    from .application import *
+    pass
+from .application import *
 from .classic import *
 from .feedback import *
