69a63453211a28d3b601911fc01de1990e70a08b
多羅狼 <Dorowolf314@gmail.com>
2024-05-29 00:09:01 +0800

    <dice>[ref]修改提示词

--- src/plugins/dice/__init__.py
+++ src/plugins/dice/__init__.py
@@ -193,7 +193,7 @@ def generate_dice_message(expr, dice_expr_list, dice_count, times, dc):
         try:
             output_line += '=' + str(result)
         except ValueError:
-            return DiceValueError("音卡似乎无法理解该骰子表达式：\n太复杂啦，不想算了啦......").message
+            return DiceValueError("音卡似乎无法理解该骰子表达式：\n太复杂啦，不想算了啦……").message
 
         try:
             if dc:
