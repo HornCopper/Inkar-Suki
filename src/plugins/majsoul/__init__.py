7600b03b337ff3358a44dbbe7d51c7e5c59daaa6
HornCopper <zgu3316@gmail.com>
2024-06-03 16:36:50 +0800

    <majsoul>[feat]find_player

---
+++ src/plugins/majsoul/__init__.py
@@ -0,0 +1,10 @@
+from .koromo import *
+
+search_player = on_command("search_player", aliases={"mssp"}, priority=5, force_whitespace=True)
+
+@search_player.handle()
+async def _(event: GroupMessageEvent, args: Message = CommandArg()):
+    if args.extract_plain_text() == "":
+        return
+    data = await find_player(args.extract_plain_text())
+    await search_player.finish(data)
