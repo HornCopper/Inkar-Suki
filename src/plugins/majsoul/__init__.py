1043a994b10cc8f693150b54a477072f8a988c09
HornCopper <zgu3316@gmail.com>
2024-06-03 19:23:48 +0800

    <majsoul>[feat]get_records

--- src/plugins/majsoul/__init__.py
+++ src/plugins/majsoul/__init__.py
@@ -8,3 +8,17 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
         return
     data = await find_player(args.extract_plain_text())
     await search_player.finish(data)
+
+
+generate_record = on_command("get_records", aliases={"msgr"}, priority=5, force_whitespace=True)
+
+@generate_record.handle()
+async def _(event: GroupMessageEvent, args: Message = CommandArg()):
+    if args.extract_plain_text() == "":
+        return
+    data = get_records(args.extract_plain_text())
+    if type(data) == type([]):
+        img = get_content_local(data[0])
+        await generate_record.finish(ms.image(img))
+    else:
+        await generate_record.finish(data)
