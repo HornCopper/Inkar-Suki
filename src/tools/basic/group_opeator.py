35fc3deb1e4d382f57bb9df06ee14f248436bbeb
HornCopper <zgu3316@gmail.com>
2024-06-14 17:51:09 +0800

    <util>[feat]print data

--- src/tools/basic/group_opeator.py
+++ src/tools/basic/group_opeator.py
@@ -1,11 +1,16 @@
-from ..basic import DATA, read, write
+from ..basic import DATA, read, write, logger
 
 import json
 
 def getGroupData(group: str, key: str):
-    data = json.loads(read(DATA + "/" + str(group) + "/settings.json"))
+    data = read(DATA + "/" + str(group) + "/settings.json")
     if not data:
         return False
+    else:
+        data = json.loads(read(DATA + "/" + str(group) + "/settings.json"))
+        logger.info(data)
+    return data[key]
+
     return data[key]
 
 def setGroupData(group: str, key: str, new):
