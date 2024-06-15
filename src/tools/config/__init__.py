9abdba2ec62a3471faf23036f59ceab7a8ce91d1
HornCopper <zgu3316@gmail.com>
2024-06-15 22:39:13 +0800

    <jx3>[feat]sync servers

--- src/tools/config/__init__.py
+++ src/tools/config/__init__.py
@@ -4,8 +4,8 @@
 # def require_config():
 # class configs(enum)
 
-from .config import Config
-from .config import Config as gloConfig
+from .config_example import Config
+from .config_example import Config as gloConfig
 token = Config.jx3api_globaltoken
 bot = "Inkar-Suki"
 proxies = None
