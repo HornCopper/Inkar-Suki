e44439f6da4a9766dcc0d5c821af88692969e7f4
HornCopper <zgu3316@gmail.com>
2024-06-18 16:40:06 +0800

    <jx3>[feat]sandbox_v2 paid required

--- src/plugins/jx3/sandbox/__init__.py
+++ src/plugins/jx3/sandbox/__init__.py
@@ -1,4 +1,5 @@
 from .api import *
+from src.plugins.sign import Sign
 
 sandbox = on_command("jx3_sandbox", aliases={"沙盘"}, force_whitespace=True, priority=5)
 
@@ -27,6 +28,11 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     获取服务器沙盘：
     Example：-沙盘v2 幽月轮
     """
+    coin = Sign.get_coin(str(event.user_id))
+    if coin < 10:
+        await sandbox_v2.finish("沙盘v2正在内测，需要10金币才能使用哦！")
+    else:
+        Sign.reduce(str(event.user_id), 10)
     server = args.extract_plain_text()
     server = server_mapping(server, str(event.group_id))
     data = await sandbox_v2_(server)
