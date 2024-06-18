d4f1176b07a4d24a54194589c8f4d6992b119bb4
HornCopper <zgu3316@gmail.com>
2024-06-18 16:12:43 +0800

    <jx3>[feat]sandbox v2

--- src/plugins/jx3/sandbox/__init__.py
+++ src/plugins/jx3/sandbox/__init__.py
@@ -17,3 +17,21 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     else:
         data = await get_content(data)
         await sandbox.finish(ms.image(data))
+
+
+sandbox_v2 = on_command("jx3_sandbox_v2", aliases={"沙盘v2"}, force_whitespace=True, priority=5)
+
+@sandbox_v2.handle()
+async def _(event: GroupMessageEvent, args: Message = CommandArg()):
+    """
+    获取服务器沙盘：
+    Example：-沙盘v2 幽月轮
+    """
+    server = args.extract_plain_text()
+    server = server_mapping(server, str(event.group_id))
+    data = await sandbox_v2_(server)
+    if type(data) == type([]):
+        await sandbox_v2.finish(data[0])
+    else:
+        data = await get_content(data)
+        await sandbox_v2.finish(ms.image(data))
