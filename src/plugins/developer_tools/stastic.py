a4478012af90aa9dd2ac60fffb22c4901488ba4d
HornCopper <zgu3316@gmail.com>
2024-06-21 02:15:43 +0800

    <stastic>[feat]xy互换

--- src/plugins/developer_tools/stastic.py
+++ src/plugins/developer_tools/stastic.py
@@ -2,20 +2,22 @@ import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 import io
 import numpy as np
-import base64
 
 from src.tools.basic import ASSETS
 
 async def generate_bar_chart(data):
+    data = dict(sorted(data.items(), key=lambda item: item[1], reverse=False))
+
     categories = list(data.keys())
     values = list(data.values())
 
-    fig, ax = plt.subplots(figsize=(16, 10))
+    fig, ax = plt.subplots(figsize=(10, 18))
+    
+    bar_height = 0.6
+    spacing_factor = 1.5
+    bar_positions = np.arange(len(categories)) * spacing_factor
 
-    bar_width = 0.6
-    bar_positions = np.arange(len(categories))
-
-    bars = ax.bar(bar_positions, values, width=bar_width, color=["blue", "green", "red", "purple"])
+    bars = ax.barh(bar_positions, values, height=bar_height, color="blue")
 
     font_path = ASSETS + "/font/custom.ttf"
     prop = fm.FontProperties(fname=font_path)
@@ -23,16 +25,18 @@ async def generate_bar_chart(data):
     plt.rcParams["font.family"] = prop.get_name()
 
     ax.set_title("Inkar Suki 命令使用统计", fontproperties=prop, fontsize=16)
-    ax.set_xlabel("类别", fontproperties=prop, fontsize=12)
-    ax.set_ylabel("对数数量", fontproperties=prop, fontsize=12)
+    ax.set_ylabel("类别", fontproperties=prop, fontsize=12)
+    ax.set_xlabel("数量", fontproperties=prop, fontsize=12)
 
-    ax.set_xticks(bar_positions)
-    ax.set_xticklabels(categories, fontproperties=prop, rotation=90, ha="center")
+    ax.set_yticks(bar_positions)
+    ax.set_yticklabels(categories, fontproperties=prop, ha="right")  # 标签居中对齐
 
     for bar in bars:
-        yval = bar.get_height()
-        ax.text(bar.get_x() + bar.get_width() / 2, yval + 0.1, round(yval, 2), ha="center", va="bottom", fontproperties=prop)
+        xval = bar.get_width()
+        ax.text(xval + 0.1, bar.get_y() + bar.get_height() / 2, round(xval, 2), 
+                ha="left", va="center", fontproperties=prop)
 
+    plt.subplots_adjust(left=0.25)
     plt.tight_layout()
 
     buf = io.BytesIO()
@@ -44,7 +48,3 @@ async def generate_bar_chart(data):
     buf.close()
 
     return image_bytes
-
-def preprocess_data(data):
-    processed_data = {key: np.log10(value) for key, value in data.items()}
-    return processed_data
