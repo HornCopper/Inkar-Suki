0808e5b9b253a5f951e53e8a7270ed9e72474dba
HornCopper <zgu3316@gmail.com>
2024-06-21 01:40:05 +0800

    <stastic>[feat]调整柱间距

--- src/plugins/developer_tools/stastic.py
+++ src/plugins/developer_tools/stastic.py
@@ -2,37 +2,44 @@ import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 import io
 import numpy as np
+import base64
 
 from src.tools.basic import ASSETS
 
 async def generate_bar_chart(data):
-    data = preprocess_data(data)
     categories = list(data.keys())
     values = list(data.values())
 
-    plt.figure(figsize=(160, 80))
-    plt.bar(categories, values, color=["blue", "green", "red", "purple"])
+    fig, ax = plt.subplots(figsize=(16, 10))
+
+    bar_width = 0.6
+    bar_positions = np.arange(len(categories))
+
+    ax.bar(bar_positions, values, width=bar_width, color="blue")
 
     font_path = ASSETS + "/font/custom.ttf"
     prop = fm.FontProperties(fname=font_path)
 
     plt.rcParams["font.family"] = prop.get_name()
 
-    plt.title("Inkar Suki 命令使用统计", fontproperties=prop, fontsize=16)
-    plt.xlabel("类别", fontproperties=prop, fontsize=12)
-    plt.ylabel("数量", fontproperties=prop, fontsize=12)
+    ax.set_title("Inkar Suki 命令使用统计", fontproperties=prop, fontsize=16)
+    ax.set_xlabel("类别", fontproperties=prop, fontsize=12)
+    ax.set_ylabel("对数数量", fontproperties=prop, fontsize=12)
 
-    plt.xticks(fontproperties=prop)
+    ax.set_xticks(bar_positions)
+    ax.set_xticklabels(categories, fontproperties=prop, rotation=90, ha="center")
+
+    plt.tight_layout()
 
     buf = io.BytesIO()
     plt.savefig(buf, format="png")
-    plt.close()
+    plt.close(fig)
     buf.seek(0)
 
-    image_bytes = buf.read()
+    image_base64 = base64.b64encode(buf.read()).decode("utf-8")
     buf.close()
 
-    return image_bytes
+    return image_base64
 
 def preprocess_data(data):
     processed_data = {key: np.log10(value) for key, value in data.items()}
