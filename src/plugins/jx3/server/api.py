9ccbad4eb678f361b431f68495b99df152b53bd1
Inkar-Suki <horncopper@outlook.com>
2024-06-27 12:20:54 +0800

    <jx3>[feat]优化属性v4

--- src/plugins/jx3/server/api.py
+++ src/plugins/jx3/server/api.py
@@ -2,10 +2,12 @@ from src.tools.basic import *
 
 
 async def server_status(server: str = None):
-    full_link = f"{Config.jx3api_link}/data/server/check?server={server}&token={token}"
+    full_link = f"{Config.jx3api_link}/data/server/check"
     info = await get_api(full_link)
     data = info["data"]
     prefix = "开服状态："
-    status_desc = "已开服。" if data["status"] else "维护中。"
+    for i in data:
+        if i["server"] == server:
+            status_desc = "已开服。" if i["status"] else "维护中。"
     msg = f"{prefix} {server} {status_desc}"
     return msg
