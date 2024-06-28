489a4ed98946bcf5742fd2000dd9f5f4ec85e7a7
Inkar-Suki <horncopper@outlook.com>
2024-06-28 23:37:41 +0800

    <jx3>[feat]解决属性v4来源未知的问题

--- src/plugins/jx3/trade/appearence.py
+++ src/plugins/jx3/trade/appearence.py
@@ -3,7 +3,7 @@ from .sl import *
 async def item_(name: str = None):  # 物价 <物品>
     if token is None:
         return [PROMPT_NoToken]
-    final_url = f"{Config.jx3api_link}/view/trade/record?token={token}&robot={bot}&name={name}&scale=1"
+    final_url = f"https://www.jx3api.com/view/trade/record?robot={bot}&name={name}&scale=1"
     data = await get_api(final_url)
     if data["code"] == 400:
         return ["唔……尚未收录该物品。"]
