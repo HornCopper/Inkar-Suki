0568415da00dc667589e166d256daeec1793a7ce
HornCopper <zgu3316@gmail.com>
2024-07-11 06:10:17 +0800

    <jx3>[feat]金币限制

--- src/plugins/jx3/detail/__init__.py
+++ src/plugins/jx3/detail/__init__.py
@@ -1,3 +1,5 @@
+from src.plugins.sign import Sign
+
 from .detail import *
 
 zone_detail = on_command("jx3_zone_detail", aliases={"副本总览"}, force_whitespace=True, priority=5)
@@ -31,6 +33,11 @@ global_dungeon_lookup = on_command("jx3_global_dungeon", aliases={"副本分览"
 async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     if args.extract_plain_text() == "":
         return
+    coin = Sign.get_coin(str(event.user_id))
+    if coin < 50:
+        await global_dungeon_lookup.finish("副本分览正在内测，需要50金币才能使用哦！")
+    else:
+        Sign.reduce(str(event.user_id), 50)
     arg = args.extract_plain_text().split(" ")
     if len(arg) not in [1, 2]:
         await global_dungeon_lookup.finish("唔……参数不正确哦，请检查后重试~")
