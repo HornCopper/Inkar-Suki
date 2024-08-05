c28b6d7fbcb650f82cc172c1125b94d55b7fab83
HornCopper <zgu3316@gmail.com>
2024-08-05 21:19:54 +0800

    <global>[feat]proxy

--- src/tools/utils/request.py
+++ src/tools/utils/request.py
@@ -1,31 +1,35 @@
+from src.tools.config import Config
+
+proxy = Config.bot_basic.proxy
+
 import httpx
 
 async def get_url(url, **kwargs):
-    async with httpx.AsyncClient(follow_redirects=True, verify=False) as client:
+    async with httpx.AsyncClient(follow_redirects=True, verify=False, proxy=proxy) as client:
         resp = await client.get(url, **kwargs)
         result = resp.text
         return result
     
 async def get_content(url, **kwargs):
-    async with httpx.AsyncClient(follow_redirects=True, verify=False) as client:
+    async with httpx.AsyncClient(follow_redirects=True, verify=False, proxy=proxy) as client:
         resp = await client.get(url, **kwargs)
         result = resp.content
         return result
 
 async def get_status(url, **kwargs):
-    async with httpx.AsyncClient(follow_redirects=True, verify=False) as client:
+    async with httpx.AsyncClient(follow_redirects=True, verify=False, proxy=proxy) as client:
         resp = await client.get(url, **kwargs)
         result = resp.status_code
         return result
     
 async def get_api(url, **kwargs):
-    async with httpx.AsyncClient(follow_redirects=True, verify=False) as client:
+    async with httpx.AsyncClient(follow_redirects=True, verify=False, proxy=proxy) as client:
         resp = await client.get(url, **kwargs)
         result = resp.json()
         return result
 
 async def post_url(url, **kwargs):
-    async with httpx.AsyncClient(follow_redirects=True, verify=False) as client:
+    async with httpx.AsyncClient(follow_redirects=True, verify=False, proxy=proxy) as client:
         resp = await client.post(url, **kwargs)
         result = resp.text
         return result
