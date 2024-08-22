91c4019a6f8446588907a3ea61d49b50eccf6fe2
HornCopper <zgu3316@gmail.com>
2024-08-22 19:04:11 +0800

    <grab>[feat]poke 判断自身

--- src/plugins/grab/poke.py
+++ src/plugins/grab/poke.py
@@ -1,11 +1,13 @@
 from nonebot.adapters.onebot.v11 import PokeNotifyEvent, MessageSegment as ms
 from nonebot import on_notice
 
+from src.tools.config import Config
+
 poke_me = on_notice(priority=5)
 
 @poke_me.handle()
 async def _(event: PokeNotifyEvent):
-    if event.group_id == None:
+    if event.group_id == None or event.self_id not in list(Config.bot_basic.notice_to.__dict__):
         return
     else:
         await poke_me.finish("音卡在呢！找音卡有什么事吗！(^ω^)" + ms.image("https://inkar-suki.codethink.cn/Inkar-Suki-Docs/img/emoji.jpg"))
