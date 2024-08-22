cadb28920f01323ec021571ad4484b177571a5bf
HornCopper <zgu3316@gmail.com>
2024-08-22 19:05:36 +0800

    <grab>[feat]poke 调用config属性错误

--- src/plugins/grab/poke.py
+++ src/plugins/grab/poke.py
@@ -7,7 +7,7 @@ poke_me = on_notice(priority=5)
 
 @poke_me.handle()
 async def _(event: PokeNotifyEvent):
-    if event.group_id == None or event.self_id not in list(Config.bot_basic.notice_to.__dict__):
+    if event.group_id == None or event.self_id not in list(Config.bot_basic.bot_notice.__dict__):
         return
     else:
         await poke_me.finish("音卡在呢！找音卡有什么事吗！(^ω^)" + ms.image("https://inkar-suki.codethink.cn/Inkar-Suki-Docs/img/emoji.jpg"))
