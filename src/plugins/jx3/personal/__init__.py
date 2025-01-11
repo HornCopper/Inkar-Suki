bba5de9df83c7ddaaf04fee0a86fe47b222bd732
HornCopper <zgu3316@gmail.com>
2025-01-11 11:28:25 +0800

    <jx3>[feat]update personal bound parser

--- src/plugins/jx3/personal/__init__.py
+++ src/plugins/jx3/personal/__init__.py
@@ -19,16 +19,36 @@ def parse_role_name(role_name: str, group_server: str) -> tuple[str, str]:
         return role_name, role_server
     else:
         return role_name, group_server
+    
+def parse_role_names(input_str: str) -> list[str]:
+    extracted_items = re.findall(r"\[(.*?)\]", input_str)
+    cleaned_str = re.sub(r"\[.*?\]", '', input_str)
+    result = re.split(r'[，、,。/.\\]', cleaned_str)
+    result = [item.strip() for item in result if item.strip()]
+    print(extracted_items + result)
+    return extracted_items + result
 
 PersonalBindMathcer = on_command("绑定角色", priority=5, force_whitespace=True)
 
 @PersonalBindMathcer.handle()
-async def _(event: GroupMessageEvent, args: Message = CommandArg()):
-    arg = args.extract_plain_text().strip()
-    roles: list[str] = re.findall(r"\[(.*?)\]", arg) if "、" not in arg else arg.split("、")
-    server = Server(None, event.group_id).server
-    if server is None:
-        await PersonalBindMathcer.finish(PROMPT.ServerNotExist)
+async def _(event: GroupMessageEvent, argument: Message = CommandArg()):
+    args = argument.extract_plain_text().strip()
+    if args == "":
+        return
+    args = args.split(" ")
+    if len(args) not in [1, 2]:
+        await PersonalBindMathcer.finish("唔……参数不正确哦，请检查后重试~")
+    if len(args) == 1:
+        roles: list[str] = parse_role_names(args[0])
+        server = Server(None, event.group_id).server
+        if server is None:
+            await PersonalBindMathcer.finish(PROMPT.ServerNotExist)
+    elif len(args) == 2:
+        server = args[0]
+        roles: list[str] = parse_role_names(args[1])
+        server = Server(server, event.group_id).server
+        if server is None:
+            await PersonalBindMathcer.finish(PROMPT.ServerNotExist)
     parsed_roles = [parse_role_name(name, server) for name in roles]
     msg = RoleBind(event.user_id, parsed_roles).bind()
     await PersonalBindMathcer.finish(msg)
@@ -36,20 +56,32 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
 PersonalUnbindMathcer = on_command("解绑角色", priority=5, force_whitespace=True)
 
 @PersonalUnbindMathcer.handle()
-async def _(event: GroupMessageEvent, args: Message = CommandArg()):
-    arg = args.extract_plain_text().strip()
+async def _(event: GroupMessageEvent, argument: Message = CommandArg()):
+    arg = argument.extract_plain_text().strip()
+    if arg == "":
+        return
     all = False
     if arg == "全部":
         parsed_roles = []
         all = True
+        msg = RoleBind(event.user_id, parsed_roles).unbind(all=all)
+        await PersonalBindMathcer.finish(msg)
     else:
-        roles: list[str] = re.findall(r"\[(.*?)\]", arg) if "、" not in arg else arg.split("、")
-        server = Server(None, event.group_id).server
-        if server is None:
-            await PersonalUnbindMathcer.finish(PROMPT.ServerNotExist)
+        args = arg.split(" ")
+        if len(args) == 1:
+            roles: list[str] = parse_role_names(args[0])
+            server = Server(None, event.group_id).server
+            if server is None:
+                await PersonalBindMathcer.finish(PROMPT.ServerNotExist)
+        elif len(args) == 2:
+            server = args[0]
+            roles: list[str] = parse_role_names(args[1])
+            server = Server(server, event.group_id).server
+            if server is None:
+                await PersonalBindMathcer.finish(PROMPT.ServerNotExist)
         parsed_roles = [parse_role_name(name, server) for name in roles]
-    msg = RoleBind(event.user_id, parsed_roles).unbind(all=all)
-    await PersonalUnbindMathcer.finish(msg)
+        msg = RoleBind(event.user_id, parsed_roles).unbind(all=all)
+        await PersonalBindMathcer.finish(msg)
 
 AllBoundRolesMatcher = on_command("角色列表", priority=5, force_whitespace=True)
 
