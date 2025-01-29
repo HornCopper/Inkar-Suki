e2093aaa80b10be3ec3acdee30658fb4817f1159
HornCopper <zgu3316@gmail.com>
2025-01-29 20:08:03 +0800

    <jx3>[feat]bingxinjue calculator

--- src/plugins/jx3/calculator/__init__.py
+++ src/plugins/jx3/calculator/__init__.py
@@ -4,9 +4,11 @@ from nonebot.adapters.onebot.v11 import Message, GroupMessageEvent
 
 from src.const.prompts import PROMPT
 from src.const.jx3.server import Server
+from src.utils.permission import check_permission, denied
 
 from .lxg import LingxueCalculator
 from .zxg import ZixiagongCalculator
+from .bxj import BingxinjueCalculator
 
 YLJCalcMatcher = on_command("jx3_calculator_lyj", aliases={"凌雪计算器"}, priority=5, force_whitespace=True)
 
@@ -50,5 +52,34 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     server = Server(server, event.group_id).server
     if server is None:
         await ZXGCalculator.finish(PROMPT.ServerNotExist)
-    data = await (await ZixiagongCalculator.with_name(name, server)).image()
+    instance = await ZixiagongCalculator.with_name(name, server)
+    if isinstance(instance, str):
+        await ZXGCalculator.finish(instance)
+    data = await instance.image()
     await ZXGCalculator.finish(data)
+
+BXJCalculator = on_command("jx3_calculator_bxj", aliases={"冰心计算器"}, priority=5, force_whitespace=True)
+
+@BXJCalculator.handle()
+async def _(event: GroupMessageEvent, args: Message = CommandArg()):
+    if not check_permission(event.user_id, 1):
+        await BXJCalculator.finish(denied(1))
+    if args.extract_plain_text() == "":
+        return
+    arg = args.extract_plain_text().split(" ")
+    if len(arg) not in [1, 2]:
+        await BXJCalculator.finish("唔……参数不正确哦，请检查后重试~")
+    if len(arg) == 1:
+        server = None
+        name = arg[0]
+    elif len(arg) == 2:
+        server = arg[0]
+        name = arg[1]
+    server = Server(server, event.group_id).server
+    if server is None:
+        await BXJCalculator.finish(PROMPT.ServerNotExist)
+    instance = await BingxinjueCalculator.with_name(name, server)
+    if isinstance(instance, str):
+        await BXJCalculator.finish(instance)
+    data = await instance.image()
+    await BXJCalculator.finish(data)
--- src/plugins/jx3/calculator/bxj.py
+++ src/plugins/jx3/calculator/bxj.py
@@ -10,6 +10,7 @@ from src.utils.analyze import sort_dict_list
 from src.utils.network import Request
 from src.utils.generate import generate
 from src.plugins.jx3.attributes.v2_remake import (
+    Qixue,
     Panel,
     EquipDataProcesser,
 )
@@ -18,61 +19,56 @@ from src.templates import SimpleHTML, get_saohua
 from ._template import template_calculator, template_attr
 from .base import BaseCalculator
 
-class Zixiagong(Kungfu):
+bx_talents = ["青梅嗅", "千里冰封", "新妆", "芳姿畅音", "枕上", "生莲", "流玉", "钗燕", "盈袖", "化冰", "明空", "凝华"]
+
+class Bingxinjue(Kungfu):
     @classmethod
     def with_internel_id(cls, internel_id) -> "Self | str":
-        if int(internel_id) not in [10014]:
+        if int(internel_id) not in [10081]:
             current_kungfu = super().with_internel_id(internel_id).name or "无法识别"
             return "该计算器与心法不符合，请检查后重试！\n当前识别的心法：" + current_kungfu
         return super().with_internel_id(internel_id)
     
-class ZixiagongCalculator(BaseCalculator):
+class BingxinjueCalculator(BaseCalculator):
+    cw_flag: bool | None = None
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.parser = EquipDataProcesser(self.data)
 
     @property
-    def kungfu(self) -> Zixiagong:
-        kungfu = Zixiagong.with_internel_id(
+    def kungfu(self) -> Bingxinjue:
+        kungfu = Bingxinjue.with_internel_id(
             self.data["data"]["Kungfu"]["KungfuID"]
         )
         if isinstance(kungfu, str):
             raise ValueError(kungfu)
         return kungfu
-
-    @property
-    def haste(self) -> int:
-        """
-        加速
-        """
-        for each_attr in self.data["data"]["PersonalPanel"]:
-            if each_attr["name"] == "加速":
-                return int(each_attr["value"])
-        raise ValueError("无法确定加速！")
     
     @property
-    def talents(self) -> list[str]:
-        talents_data = self.data["data"]["Person"]["qixueList"]
-        return [t["name"] for t in talents_data]
+    def weapon(self) -> str:
+        for each_equip in self.data["data"]["Equips"]:
+            if each_equip["Name"] == "淬血霜花剑·金莲":
+                return "飞剑"
+            if each_equip["Name"] == "飞霜绛露":
+                self.cw_flag = True
+                return "飞霜绛露"
+            if each_equip["Name"] == "龙鲤":
+                self.cw_flag = True
+                return "龙鲤"
+            if each_equip["Name"] == "月初生":
+                return "水特效"
+        return "无"
     
     @property
     def cw(self) -> bool:
+        if isinstance(self.cw_flag, bool):
+            return self.cw_flag
+        flag = False
         for each_equip in self.data["data"]["Equips"]:
-            if each_equip["Name"] in ["苍冥游", "仙灵"]:
-                return True
-        return False
-    
-    @property
-    def mode(self) -> str:
-        cw_flag = self.cw
-        haste = self.haste
-        if not cw_flag:
-            return "紫武及格档"
-        elif cw_flag and (206 <= haste < 9232):
-            return "橙武一段加速标准模式"
-        elif cw_flag and (9232 <= haste):
-            return "橙武二段加速标准模式"
-        return "紫武及格档"
+            if each_equip["Name"] in ["龙鲤", "飞霜绛露"]:
+                flag = True
+        return flag
     
     @property
     def set_effect(self) -> tuple[bool, bool]: # buff / 技能增伤
@@ -91,22 +87,9 @@ class ZixiagongCalculator(BaseCalculator):
     
     @property
     def effect_sash(self) -> str:
-        """
-        特效腰坠
-        """
         for each_equip in self.data["data"]["Equips"]:
             if each_equip["Name"] == "画青山":
-                return "25900风特效"
-        return "无"
-    
-    @property
-    def effect_weapon(self) -> str:
-        """
-        特效武器
-        """
-        for each_equip in self.data["data"]["Equips"]:
-            if each_equip["Name"] == "如云浮":
-                return "25900水特效"
+                return "画青山"
         return "无"
     
     @property
@@ -116,17 +99,16 @@ class ZixiagongCalculator(BaseCalculator):
         """
         enchants = []
         location_map = {
-            "腰带": "腰带",
-            "护臂": "护腕",
-            "鞋": "鞋子"
+            "腰带": "腰",
+            "护臂": "腕",
+            "鞋": "鞋"
         }
         for location in ["腰带", "护臂", "鞋"]:
             for each_equip in self.data["data"]["Equips"]:
                 if each_equip["Icon"]["SubKind"] == location:
                     if "WCommonEnchant" in each_equip:
                         enchants.append(
-                            ("全等级" if location == "腰带" else each_equip["Quality"]) + \
-                            location_map[location] + "大附魔"
+                            ("小" if int(each_equip["Quality"]) <= 23500 else "大") + "伤" + location_map[location]
                         )
                     else:
                         enchants.append(
@@ -138,7 +120,7 @@ class ZixiagongCalculator(BaseCalculator):
     def attr(self) -> list[Panel]:
         results = []
         personal_panel = self.data["data"]["PersonalPanel"] or []
-        for a in ["基础攻击力", "攻击力", "会心", "会心效果", "破防", "无双", "破招"]:
+        for a in ["根骨", "基础攻击力", "会心", "会心效果", "加速", "破防", "无双", "破招"]:
             for each_attr in personal_panel:
                 if each_attr["name"] == a:
                     results.append(
@@ -148,15 +130,15 @@ class ZixiagongCalculator(BaseCalculator):
                         )
                     )
         return results
-    
+
     @property
-    def spirit(self) -> int:
-        """根骨"""
+    def attack(self) -> int:
+        """攻击力"""
         for attr in (self.data["data"]["PersonalPanel"] or []):
-            if attr["name"] == "根骨":
+            if attr["name"] == "攻击力":
                 return int(attr["value"])
         return 0
-    
+
     async def calculate(self) -> dict:
         attrs = self.attr
         detailed_attrs = {
@@ -169,36 +151,18 @@ class ZixiagongCalculator(BaseCalculator):
             "enchants": self.enchants,
             "is_precious_weapon": self.cw,
             "effect_sash": self.effect_sash,
-            "effect_weapon": self.effect_weapon,
+            "effect_weapon": self.weapon,
             "is_school_effect": school_effect,
             "is_school_damage": school_damage,
             "special_effects": [],
-            "talents": self.talents,
-            "mode": self.mode
+            "talents": []
         }
         final_result = (await Request(
-            "http://10.0.10.13:17171/calculator_zxg",
+            "http://10.0.10.13:17171/calculator_bxj",
             params=params,
             headers={"token": Config.hidden.offcial_token}
         ).post()).json()
         return final_result
-    
-    def parse_attr(self, role_attr: list[Panel], income_data: list) -> list[str]:
-        result = []
-        attr_typs = ["基础攻击力", "根骨", "会心", "会心效果", "破防", "无双", "破招"]
-        for each_attr in role_attr:
-            if each_attr.name in attr_typs:
-                result.append(
-                    Template(template_attr).render(
-                        name = each_attr.name,
-                        value = each_attr.value,
-                        income = round(float(income_data[attr_typs.index(each_attr.name)]), 3)
-                    )
-                )
-        result = [Template(template_attr).render(name=role_attr[1].name, value=role_attr[1].value, income="未知")] + \
-        result + \
-        [Template(template_attr).render(name="根骨", value=str(self.spirit), income=round(float(income_data[1]), 3))]
-        return result
     
     @staticmethod
     def _to_float(percent: str) -> float:
@@ -208,6 +172,38 @@ class ZixiagongCalculator(BaseCalculator):
     def _to_percent(float_value: float) -> str:
         return str(round(float_value * 100, 2)) + "%"
     
+    @staticmethod
+    async def loop_talent() -> dict[str, str]:
+        _loop_talents = {}
+        for t in bx_talents:
+            x, y, icon = (await Qixue.create({"name": t}, "冰心诀")).location or (
+                "",
+                "",
+                "",
+            )
+            _loop_talents[t] = icon
+        loop_talents = _loop_talents
+        return loop_talents
+    
+    def parse_attr(self, role_panel: list[Panel], income_data: dict[str, str]):
+        results = []
+        for panel in role_panel:
+            attr_name = panel.name
+            if attr_name == "会心效果":
+                attr_name = "会效"
+            if attr_name == "基础攻击力":
+                attr_name = "攻击"
+            if attr_name in income_data.keys():
+                results.append(
+                    Template(template_attr).render(
+                        name = panel.name,
+                        value = panel.value,
+                        income = income_data[attr_name]
+                    )
+                )
+        results = [Template(template_attr).render(name="攻击力", value=str(self.attack), income="未知")] + results
+        return results
+
     async def image(self):
         data = await self.calculate()
         if data["code"] != 200:
@@ -219,12 +215,12 @@ class ZixiagongCalculator(BaseCalculator):
             tables.append(
                 Template(template_calculator).render(
                     **{
-                        "skill": skill_data["name"].replace("两仪", "两仪化形").replace("四象", "四象轮回").replace("六合", "六合独尊"),
+                        "skill": skill_data["name"],
                         "display": self._to_percent(
                             self._to_float(skill_data["percent"]) / self._to_float(skills_data[0]["percent"])
                         ),
                         "percent": skill_data["percent"],
-                        "count": str(skill_data["count"]),
+                        "count": "N/A",
                         "value": "{:,}".format(int(skill_data["damage"])),
                     }
                 )
@@ -238,12 +234,12 @@ class ZixiagongCalculator(BaseCalculator):
                     "color": self.kungfu.color,
                     "kungfu": self.kungfu.name,
                     "dps": str(int(data["data"]["dps"])),
-                    "desc": "计算器来源：【丝路风语】气纯DPS计算器 by 月慕青尘<br>延迟：<60ms / 战斗时长：300" + \
+                    "desc": "计算器来源：【丝路风语】冰心DPS计算器 by 诺诺诺<br>延迟：30-60ms / 战斗时长：300" + \
                         f"s<br>玩家：{name}·{server}",
                     "attrs": self.parse_attr(self.attr, data["data"]["income"]),
                     "skills": tables,
                     "talents": {t.name: t.icon for t in (await self.parser.qixue())},
-                    "loop_talents": None,
+                    "loop_talents": await self.loop_talent(),
                     "saohua": get_saohua(),
                 },
             )
