7396d9371c5ecc090f46faf5be2ce33d42afdbb3
102375470 <102375470@qq.com>
2025-04-17 11:16:35 +0800

    add精耐查询

--- src/plugins/jx3/dungeon/__init__.py
+++ src/plugins/jx3/dungeon/__init__.py
@@ -10,7 +10,6 @@ from src.utils.permission import check_permission
 from src.utils.database.operation import get_group_settings
 
 from .zone_drop import get_drop_list_image
-# from .monster import get_monsters_map
 from .record import get_item_record
 from .teamcd import get_zone_record_image, get_mulit_record_image, get_personal_roles_teamcd_image
 
@@ -79,6 +78,28 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
     from .monster import get_monsters_map
     img = await get_monsters_map()
     await MonstersMatcher.finish(img)
+
+RoleMonstersMatcher = on_command("jx3_role_monster", aliases={"精耐"}, force_whitespace=True, priority=5)
+
+@RoleMonstersMatcher.handle()
+async def _(event: GroupMessageEvent, args: Message = CommandArg()):
+    if args.extract_plain_text() == "":
+        return
+    arg = args.extract_plain_text().strip().split(" ")
+    if len(arg) not in [1, 2]:
+        await RoleMonstersMatcher.finish("唔……参数不正确哦，请检查后重试~")
+    if len(arg) == 1:
+        server = None
+        role_name = arg[0]
+    elif len(arg) == 2:
+        server = arg[0]
+        role_name = arg[1]
+    server = Server(server, event.group_id).server
+    if not server:
+        await RoleMonstersMatcher.finish(PROMPT.ServerNotExist)
+    from .role_monster import get_role_monsters_map
+    data = await get_role_monsters_map(server, role_name)
+    await RoleMonstersMatcher.finish(data)
 
 AllServerItemRecordMatcher = on_command("jx3_itemrecord_allserver", aliases={"全服掉落"}, force_whitespace=True, priority=5)
 
